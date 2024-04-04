7ded2b14a811af41f0a5aef2d444718a086d7a90
nyeroni <nir2y@naver.com>
2024-04-04 19:25:16 +0900

    Feat: main 클래스 생성

---
+++ src/main/java/car/Application.java
@@ -0,0 +1,10 @@
+package car;
+
+import car.controller.RacingGameController;
+
+public class Application {
+    public static void main(String [] args){
+        RacingGameController controller = new RacingGameController();
+    }
+
+}
