57b5680e29f69f3831270e9be00d67b4b9333cc7
mangsuyo <mangsuyo3610@gmail.com>
2024-04-06 14:55:43 +0900

    Feat : Model Cars Method to determine Winner

--- src/main/java/model/Cars.java
+++ src/main/java/model/Cars.java
@@ -2,16 +2,29 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cars {
 
     private final List<Car> cars = new ArrayList<>();
-
-    public void save(Car car){
+    private List<Car> winners = new ArrayList<>();
+    public void save(Car car) {
         cars.add(car);
     }
 
     public List<Car> getCars() {
         return cars;
     }
+
+    public void calculateWinner() {
+        final int maxScore = cars.stream().mapToInt(Car::getScore).max().orElse(0);
+        winners = cars.stream().filter(car -> car.getScore() == maxScore)
+                .collect(Collectors.toList());
+    }
+
+    public List<Car> getWinners(){
+        calculateWinner();
+        return winners;
+    }
+
 }
