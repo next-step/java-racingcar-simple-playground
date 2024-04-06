9db1b1443f47506330a64bef6bb98bfb9da191dc
mangsuyo <mangsuyo3610@gmail.com>
2024-04-06 13:54:31 +0900

    Feat: CarController Function For Moving Cars

--- src/main/java/controller/CarController.java
+++ src/main/java/controller/CarController.java
@@ -8,9 +8,15 @@ public class CarController {
     private Car car;
     private Cars carList;
     private String[] carNames = {"붕붕이", "씽씽이", "주전차"};
-    public void saveCar(){
+    public void saveCars(){
         for(String carName: carNames){
             carList.save(new Car(carName));
+        }
+    }
+
+    public void moveCars(){
+        for(Car car : carList.getCars()){
+            car.move();
         }
     }
 }
