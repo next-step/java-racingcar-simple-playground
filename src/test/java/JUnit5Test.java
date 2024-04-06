d0460f90f4e4e7dbf56072d0afa508e13cced35e
mangsuyo <mangsuyo3610@gmail.com>
2024-04-06 15:32:40 +0900

    Test: Add Unit Test For Calculate Winner

--- src/test/java/JUnit5Test.java
+++ src/test/java/JUnit5Test.java
@@ -1,3 +1,5 @@
+import model.Car;
+import model.Cars;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -9,14 +11,50 @@ public class JUnit5Test {
 
     @Nested
     @DisplayName("generate 메서드 0~9 정수 반환 테스트")
-    class GenerateMethodTest{
+    class GenerateMethodTest {
         @Test
         @DisplayName("generate 메서드는 0에서 9사이의 정수를 반환한다.")
-        void testGenerateMethodReturnZeroAndNine(){
+        void testGenerateMethodReturnZeroAndNine() {
             List<Integer> numbers = List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
             int testNumber = RandomNumberGenerator.generate();
-            if(!numbers.contains(testNumber)){
-                throw new RuntimeException("generate 메소드가 0에서 9사이의 정수를 반환하지 않습니다.");
+            if (!numbers.contains(testNumber)) {
+                throw new RuntimeException("generate 메서드가 0에서 9사이의 정수를 반환하지 않습니다.");
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("우승자를 구하는 기능 테스트")
+    class CalculateWinnerMethodTest {
+        @Test
+        @DisplayName("calculateWinner 메서드는 가장 높은 점수를 가진 자동차를 우승자로 결정한다.")
+        void testCalculateWinnersMethodReturnAccurateWinners() {
+            int raceCount = 3;
+
+            Cars cars = new Cars();
+            Car car1 = new Car("붕붕이");
+            Car car2 = new Car("따릉이");
+            Car car3 = new Car("쌩생이");
+
+            cars.save(car1);
+            cars.save(car2);
+            cars.save(car3);
+            cars.move(raceCount);
+
+            int maxScore = 0;
+
+            for (Car car : cars.getCars()) {
+                if (car.getScore() > maxScore) {
+                    maxScore = car.getScore();
+                }
+            }
+
+            final int winnerScore = maxScore;
+            List<Car> topScorer = cars.getCars().stream().filter(car -> car.getScore() == winnerScore).toList();
+
+            List<Car> winners = cars.getWinners();
+            if (!winners.equals(topScorer)) {
+                throw new RuntimeException("calculateWinner 메서드는 정확한 우승자를 반환하지 않습니다.");
             }
         }
     }
