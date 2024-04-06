aab20b608aac11919bf8d070bd15d8d270d46f31
hkjbrian <hkjbrian@gmail.com>
2024-04-06 20:32:58 +0900

    Second Version of RacingCarStep2 04062032

--- src/test/java/RacingCarStep2.java
+++ src/test/java/RacingCarStep2.java
@@ -33,20 +33,20 @@ public class RacingCarStep2 {
         }
     }
 
-    // 자동차의 carLocations을 초기화해주는 함수
+    // 자동차의 carLocations을 초기화해주는 메서드
     public void setCarLocations(){
         carLocations = new int[n];
         // int 배열을 선언하면 기본값으로 초기화 된다?
     }
 
-    // 랜덤으로 나온 값에 대해서 차가 움직이는 함수
+    // 랜덤으로 나온 값에 대해서 차가 움직이는 메서드
     public void randomCarMoving(int carIndex){
         if (random.nextInt(10) >= 4) {
             carLocations[carIndex]++;
         }
     }
 
-    // 참여한 모든 차가 1번 움직이는 함수
+    // 참여한 모든 차가 1번 움직이는 메서드
     public void randomAllCarsMoving() {
         for (int i = 0; i < n; i++) {
             randomCarMoving(i);
@@ -57,6 +57,31 @@ public class RacingCarStep2 {
     public void racingGame(){
         for (int i = 0; i < cnt; i++) {
             randomAllCarsMoving();
+        }
+    }
+
+    // carsLocation의 최댓값을 찾는 함수
+    public int findMaxLocation(){
+        int maxLo = Integer.MAX_VALUE;
+        for (int carLocation : carLocations) {
+            maxLo = Math.max(maxLo, carLocation);
+        }
+        return maxLo;
+    }
+    // 가장 멀리 간 차량의 index 반환하는 메서드
+    public void checkingIndex(int carLocation, int i) {
+        System.out.println("우승자는 다음 차량입니다.");
+        if (carLocation == findMaxLocation()) {
+            System.out.println(carNames[i]);;
+        }
+    }
+
+    // 우승자를 구하는 메서드
+    public void winnerChecking(){
+        int maxLo = findMaxLocation();
+        for (int i = 0; i < carLocations.length; i++) {
+            checkingIndex(carLocations[i],i);
+            //
         }
     }
 }
