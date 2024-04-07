0519cfbbfc9184962f87feae7ba80ef56b123144
mangsuyo <mangsuyo3610@gmail.com>
2024-04-07 14:12:45 +0900

    Feat: Input Race Count

--- src/main/java/CarRacingApplication.java
+++ src/main/java/CarRacingApplication.java
@@ -1,9 +1,8 @@
 import view.InputView;
 
-import java.util.Arrays;
-
 public class CarRacingApplication {
     public static void main(String[] args){
         final String[] carNames = InputView.getCarNames();
+        final int raceCount = InputView.getRaceCount();
     }
 }
