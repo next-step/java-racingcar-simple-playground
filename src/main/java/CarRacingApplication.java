71669fb71ad5a06fc7953f217ebf96d1dda96e65
mangsuyo <mangsuyo3610@gmail.com>
2024-04-07 14:09:19 +0900

    Feat: Input Car Names

---
+++ src/main/java/CarRacingApplication.java
@@ -0,0 +1,9 @@
+import view.InputView;
+
+import java.util.Arrays;
+
+public class CarRacingApplication {
+    public static void main(String[] args){
+        final String[] carNames = InputView.getCarNames();
+    }
+}
