9df25f845a1ceae75cf5b502a944137df7e43327
whxogus215 <whxogus215@naver.com>
2024-04-08 12:39:21 +0900

    refactor: 컨트롤러 메서드 구현 및 main 메서드 간소화

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,31 +1,12 @@
-import domain.Car;
-import domain.CarRace;
-import java.util.ArrayList;
-import java.util.List;
-import utils.Parser;
-import validator.InputValidator;
-import view.GamePrinter;
-import view.GameReader;
+import controller.CarController;
 
 public class Application {
 
     public static void main(String[] args) {
-        String carNames = GameReader.getNames();
-        List<Car> cars = new ArrayList<>();
-        List<String> parsedNames = Parser.parseStringArray(carNames);
-        for (String name : parsedNames) {
-            cars.add(new Car(name));
-        }
+        
+        CarController controller = new CarController();
+        controller.gameRun();
 
-        int gameCount;
-        gameCount = GameReader.getCount();
-        InputValidator.validateGameCount(gameCount);
-
-        CarRace carRace = new CarRace(cars);
-        GamePrinter.printResultStart();
-        carRace.raceStart(gameCount);
-        List<String> winners = carRace.selectWinners();
-        GamePrinter.printRaceWinners(winners);
     }
 
 }
--- src/main/java/controller/CarController.java
+++ src/main/java/controller/CarController.java
@@ -1,4 +1,47 @@
 package controller;
 
+import domain.Car;
+import domain.CarRace;
+import java.util.ArrayList;
+import java.util.List;
+import utils.Parser;
+import validator.InputValidator;
+import view.GamePrinter;
+import view.GameReader;
+
 public class CarController {
+
+    public void gameRun() {
+        List<Car> carNames = inputCarNames();
+        int gameCount = inputGameCount();
+        gameStart(carNames, gameCount);
+    }
+
+    // 자동차 이름 입력받기
+    private List<Car> inputCarNames() {
+        String carNames = GameReader.getNames();
+        List<String> parsedNames = Parser.parseStringArray(carNames);
+        List<Car> cars = new ArrayList<>();
+        for (String name : parsedNames) {
+            cars.add(new Car(name));
+        }
+        return cars;
+    }
+
+    // 진행 횟수 입력받기
+    private int inputGameCount() {
+        int gameCount = GameReader.getCount();
+        InputValidator.validateGameCount(gameCount);
+        return gameCount;
+    }
+
+    // 경주 시작
+    private void gameStart(List<Car> cars, int gameCount) {
+        CarRace carRace = new CarRace(cars);
+        GamePrinter.printResultStart();
+        carRace.raceStart(gameCount);
+        List<String> winners = carRace.selectWinners();
+        GamePrinter.printRaceWinners(winners);
+    }
+
 }
