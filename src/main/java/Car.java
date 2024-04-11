ff47b80c2e8a24dc66addf4126d7937756deef9d
seongjae6751 <seongjae6751@naver.com>
2024-04-11 20:42:44 +0900

    feature: 1단계 Car클래스 및 테스트 작성

---
+++ src/main/java/Car.java
@@ -0,0 +1,17 @@
+public class Car {
+    String name;
+    int position;
+
+    public Car(String name) {
+        this.name  = name;
+        this.position = 0;
+    }
+
+    int move(int value) {
+        if(value >= 4) {
+            this.position += 1;
+        }
+
+        return this.position;
+    }
+}
---
+++ src/test/java/CarTest.java
@@ -0,0 +1,29 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Random;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+    private Car car = new Car("test");
+    Random random = new Random();
+
+    @Test
+    @DisplayName("이름 테스트")
+    void checkNickName() {
+        assertEquals("test", car.name);
+    }
+
+    @Test
+    @DisplayName("전진 테스트")
+    void moveFront() {
+        assertEquals(1, car.move(4));
+    }
+
+    @Test
+    @DisplayName("정지 해있는지 테스트")
+    void moveStop() {
+        assertEquals(0, car.move(2));
+    }
+}
