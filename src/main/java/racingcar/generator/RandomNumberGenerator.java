b4dae6436c5083ac607178dca938cb300a37593e
HiiWee <wpdlzhf159@gmail.com>
2024-04-14 02:11:18 +0900

    refactor: 상수 처리

--- src/main/java/racingcar/generator/RandomNumberGenerator.java
+++ src/main/java/racingcar/generator/RandomNumberGenerator.java
@@ -4,10 +4,12 @@ import java.util.Random;
 
 public class RandomNumberGenerator implements NumberGenerator {
 
+    private static final int EXCLUSIVE_LIMIT = 10;
+
     private final Random random = new Random();
 
     @Override
     public int generateNumber() {
-        return random.nextInt(10);
+        return random.nextInt(EXCLUSIVE_LIMIT);
     }
 }
