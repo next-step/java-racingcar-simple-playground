68edf0789981f718afabf3aeee8e79a8e520be0c
HiiWee <wpdlzhf159@gmail.com>
2024-04-14 01:35:48 +0900

    refactor: scanner 자원 반환 명령 추가

--- src/main/java/CarRaceApplication.java
+++ src/main/java/CarRaceApplication.java
@@ -1,19 +1,25 @@
 import racingcar.Circuit;
 import racingcar.RacingGame;
+import racingcar.generator.NumberGenerator;
 import racingcar.generator.RandomNumberGenerator;
 import racingcar.view.ConsoleReader;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
+import racingcar.view.Reader;
 
 public class CarRaceApplication {
 
     public static void main(String[] args) {
+        Reader reader = new ConsoleReader();
+        NumberGenerator generator = new RandomNumberGenerator();
+
         RacingGame racingGame = new RacingGame(
-                new InputView(new ConsoleReader()),
+                new InputView(reader),
                 new OutputView(),
-                new Circuit(new RandomNumberGenerator())
+                new Circuit(generator)
         );
 
         racingGame.run();
+        reader.close();
     }
 }
