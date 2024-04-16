51e28281892e5e7487321d94e6c69849706b3088
Mint <audwl03071@gmail.com>
2024-04-16 11:36:02 +0900

    refactor: 테스트 코드 리팩토링
    - @NestedClass 사용하여 테스트 분류
    - mockito 버전 변경에 따른 메서드 이름 변경

--- src/test/java/org/duckstudy/game/CarRacerTest.java
+++ src/test/java/org/duckstudy/game/CarRacerTest.java
@@ -3,85 +3,92 @@ package org.duckstudy.game;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.mockito.Matchers.anyListOf;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 import org.duckstudy.movingcar.Car;
 import org.duckstudy.movingcar.CarMover;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
 @DisplayName("자동차 게임 테스트")
 class CarRacerTest {
 
-    @InjectMocks
-    private CarRacer carRacer;
-    @Mock
     private CarMover carMover;
-
     private String[] nameList;
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.initMocks(this);
+        carMover = mock(CarMover.class);
     }
 
-    @Test
-    @DisplayName("게임 종료 시 우승자를 반환한다")
-    void calculateWinnerWhenGameEnds() {
-        nameList = new String[]{"Car1", "Car2", "Car3"};
-        carRacer = new CarRacer(carMover, 3, 5, nameList);
-        doAnswer(invocation -> {
-            List<Car> carList = invocation.getArgumentAt(0, List.class);
-            carList.stream().filter(c -> c.getName().equals(nameList[0]) || c.getName().equals(nameList[1]))
-                    .forEach(Car::move);
-            return null;
-        }).when(carMover).move(anyListOf(Car.class));
+    @Nested
+    @DisplayName("입력값 검증 테스트")
+    class inputValidationTest {
+        @ParameterizedTest
+        @MethodSource("methodSourceCarNameLengthTestArguments")
+        @DisplayName("자동차 이름이 없거나 5글자 초과일 때 에러를 발생한다")
+        void gameFailWhenCarNameLengthIsEmptyOrGreaterThan5(String[] nameList) {
 
-        ArrayList<Car> winnerList = carRacer.play();
+            assertThatThrownBy(() -> new CarRacer(carMover, 1, 5, nameList))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("자동차 이름은 1글자 이상 5자 이하만 가능합니다.");
+        }
 
-        assertAll(
-                () -> assertThat(winnerList.size()).isEqualTo(2),
-                () -> assertThat(winnerList.get(0).getName()).isEqualTo(nameList[0]),
-                () -> assertThat(winnerList.get(1).getName()).isEqualTo(nameList[1])
-        );
+        private static Stream<Arguments> methodSourceCarNameLengthTestArguments() {
+            return Stream.of(
+                    Arguments.arguments((Object) new String[]{""}),
+                    Arguments.arguments((Object) new String[]{"abcdef"})
+            );
+        }
+
+        @Test
+        @DisplayName("반복 횟수가 0 이하일때 에러를 발생한다")
+        void gameFailWhenRepetitionNumIsEqualOrLessThan0() {
+            nameList = new String[]{"abc", "def"};
+            int repetitionNum = 0;
+
+            assertThatThrownBy(() -> new CarRacer(carMover, 2, repetitionNum, nameList))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("반복 횟수는 0보다 커야 합니다.");
+        }
     }
 
-    @ParameterizedTest
-    @MethodSource("methodSourceCarNameLengthTestArguments")
-    @DisplayName("자동차 이름이 없거나 5글자 초과일 때 에러를 발생한다")
-    void gameFailWhenCarNameLengthIsEmptyOrGreaterThan5(String[] nameList) {
+    @Nested
+    @DisplayName("게임 진행 테스트")
+    class gamePlayTest {
+        @Test
+        @DisplayName("경주가 완료되면 우승자를 계산한다")
+        void calculateWinnerWhenRaceIsOver() {
+            nameList = new String[]{"Car1", "Car2", "Car3"};
+            List<String> selectedWinnerNames = Arrays.asList(nameList).subList(0, 2);
+            CarRacer carRacer = new CarRacer(carMover, 3, 5, nameList);
 
-        assertThatThrownBy(() -> new CarRacer(carMover, 1, 5, nameList))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("자동차 이름은 1글자 이상 5자 이하만 가능합니다.");
-    }
+            doAnswer(invocation -> {
+                List<Car> carList = invocation.getArgument(0, List.class);
+                carList.stream().filter(car -> selectedWinnerNames.contains(car.getName()))
+                        .forEach(Car::move);
+                return null;
+            }).when(carMover).move(anyList());
 
-    private static Stream<Arguments> methodSourceCarNameLengthTestArguments() {
-        return Stream.of(
-                Arguments.arguments((Object) new String[]{""}),
-                Arguments.arguments((Object) new String[]{"abcdef"})
-        );
-    }
+            ArrayList<Car> winnerList = carRacer.play();
 
-    @Test
-    @DisplayName("반복 횟수가 0 이하일때 에러를 발생한다")
-    void gameFailWhenRepetitionNumIsEqualOrLessThan0() {
-        nameList = new String[]{"abc", "def"};
-        int repetitionNum = 0;
-
-        assertThatThrownBy(() -> new CarRacer(carMover, 2, repetitionNum, nameList))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("반복 횟수는 0보다 커야 합니다.");
+            assertAll(
+                    () -> assertThat(winnerList.size()).isEqualTo(selectedWinnerNames.size()),
+                    () -> winnerList.stream().map(Car::getName).map(selectedWinnerNames::contains)
+                            .forEach(Assertions::assertTrue)
+            );
+        }
     }
 }
