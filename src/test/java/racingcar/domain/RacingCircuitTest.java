e1c30bd3b60b0d7fd5a2df1fa150b0bdd24e3358
HiiWee <wpdlzhf159@gmail.com>
2024-04-16 22:48:37 +0900

    test: 우승자를 찾을 수 없을때 예외 발생 검증

--- src/test/java/racingcar/domain/RacingCircuitTest.java
+++ src/test/java/racingcar/domain/RacingCircuitTest.java
@@ -5,13 +5,12 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.infrastructure.RandomNumberFakeGenerator;
 
 @DisplayName("서킷 테스트")
@@ -61,7 +60,7 @@ class RacingCircuitTest {
             @Test
             @DisplayName("레이스가 진행된 모든 결과를 반환합니다.")
             void eachRace_shouldReturnResults() {
-                // given & when
+                // when
                 List<Cars> carsSet = racingCircuit.startRace(5);
                 List<Integer> eachResultCarSize = carsSet.stream()
                         .map(Cars::cars)
@@ -80,12 +79,11 @@ class RacingCircuitTest {
         @DisplayName("만약 0회 이하의 값이 입력되면")
         class ContextWithInvalidRaceTryCount {
 
-            @ParameterizedTest
-            @ValueSource(ints = {0, -1})
+            @Test
             @DisplayName("레이스를 진행할 수 없기에 예외가 발생합니다.")
-            void canNotStartRace_withInvalidRaceTryCount(int invalidRaceTryCount) {
+            void canNotStartRace_withInvalidRaceTryCount() {
                 // expect
-                assertThatThrownBy(() -> racingCircuit.startRace(invalidRaceTryCount))
+                assertThatThrownBy(() -> racingCircuit.startRace(0))
                         .isInstanceOf(IllegalArgumentException.class)
                         .hasMessage("레이스는 1회 이상 진행되어야 합니다.");
             }
@@ -96,20 +94,20 @@ class RacingCircuitTest {
     @DisplayName("findWinners 메소드는")
     class DescribeFindWinners {
 
-        @BeforeEach
-        void setUp() {
-            racingCircuit.registerCars(List.of("name1", "name2", "name3", "name4"));
-            racingCircuit.startRace(5);
-        }
-
         @Nested
         @DisplayName("만약 모든 레이스가 종료된다면")
         class ContextWithEndOfTheRace {
 
+            @BeforeEach
+            void setUp() {
+                racingCircuit.registerCars(List.of("name1", "name2", "name3", "name4"));
+                racingCircuit.startRace(5);
+            }
+
             @Test
             @DisplayName("레이스의 우승자들을 조회할 수 있습니다.")
             void canFind_winnersOfRace() {
-                // given & when
+                // when
                 Cars winners = racingCircuit.findWinners();
                 List<String> winnerNames = winners.cars()
                         .stream()
@@ -118,6 +116,26 @@ class RacingCircuitTest {
 
                 // then
                 assertThat(winnerNames).containsExactly("name1", "name2", "name3", "name4");
+            }
+        }
+
+        @Nested
+        @DisplayName("만약 아무런 차량도 등록되지 않았다면")
+        class ContextWithEmptyCars {
+
+            @BeforeEach
+            void setUp() {
+                racingCircuit.registerCars(Collections.emptyList());
+                racingCircuit.startRace(5);
+            }
+
+            @Test
+            @DisplayName("우승자를 찾을 수 없다는 예외를 발생시킵니다.")
+            void canFind_winnersOfRace() {
+                // expect
+                assertThatThrownBy(() -> racingCircuit.findWinners())
+                        .isInstanceOf(IllegalStateException.class)
+                        .hasMessage("우승자를 찾을 수 없습니다.");
             }
         }
     }
