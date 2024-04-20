377e4af8c3ccd7f6a30cf27602f82a7d7dd0fe95
Mint <audwl03071@gmail.com>
2024-04-20 23:51:06 +0900

    test: 랜덤값을 경계값으로 수정

--- src/test/java/org/duckstudy/movingcar/CarTest.java
+++ src/test/java/org/duckstudy/movingcar/CarTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
-import java.util.Random;
 import org.duckstudy.generator.Generator;
 import org.duckstudy.generator.RandomValueGenerator;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,7 +13,6 @@ import org.junit.jupiter.api.Test;
 @DisplayName("움직이는 자동차 테스트")
 class CarTest {
 
-    private final Random random = new Random();
     private Generator generator;
     private Car car;
 
@@ -27,8 +25,7 @@ class CarTest {
     @Test
     @DisplayName("random 값이 4 이상 9 이하인 경우 1만큼 전진한다")
     void moveCarWhenRandomValueIsGreaterThanOrEqual4() {
-        int randomValue = 4 + random.nextInt(5);
-        given(generator.generateValue()).willReturn(randomValue);
+        given(generator.generateValue()).willReturn(4);
 
         car.move();
 
@@ -38,8 +35,7 @@ class CarTest {
     @Test
     @DisplayName("random 값이 3 이하인 경우 멈춘다")
     void stopCarWhenRandomValueIsLessThan4() {
-        int randomValue = random.nextInt(3);
-        given(generator.generateValue()).willReturn(randomValue);
+        given(generator.generateValue()).willReturn(3);
 
         car.move();
 
