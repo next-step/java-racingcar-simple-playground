8a55789d1bab9842f4898e57c9357c0a49489300
Mint <audwl03071@gmail.com>
2024-04-21 16:17:37 +0900

    refactor: 자동차 경주(Cars) 테스트 분리 및 리팩토링

--- src/test/java/org/duckstudy/input/InputValidatorTest.java
+++ src/test/java/org/duckstudy/input/InputValidatorTest.java
@@ -1,12 +1,8 @@
 package org.duckstudy.input;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.mock;
 
 import java.util.stream.Stream;
-import org.duckstudy.generator.Generator;
-import org.duckstudy.generator.RandomValueGenerator;
-import org.duckstudy.movingcar.Cars;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -18,8 +14,6 @@ import org.junit.jupiter.params.provider.MethodSource;
 class InputValidatorTest {
 
     private final InputValidator inputValidator = new InputValidator();
-    private final Generator generator = new RandomValueGenerator();
-    private final Cars cars = mock(Cars.class);
 
     @Nested
     @DisplayName("입력값 검증 테스트")
@@ -29,7 +23,7 @@ class InputValidatorTest {
         @DisplayName("자동차 이름이 없거나 5글자 초과일 때 에러를 발생한다")
         void gameFailWhenCarNameLengthIsEmptyOrGreaterThan5(String[] carNames) {
 
-            assertThatThrownBy(() -> inputValidator.validateInput(1, carNames))
+            assertThatThrownBy(() -> inputValidator.validateInput(carNames, 1))
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessage("자동차 이름은 1글자 이상 5자 이하만 가능합니다.");
         }
@@ -47,37 +41,9 @@ class InputValidatorTest {
             String[] carNames = new String[]{"abc", "def"};
             int repetitionNum = 0;
 
-            assertThatThrownBy(() -> inputValidator.validateInput(repetitionNum, carNames))
+            assertThatThrownBy(() -> inputValidator.validateInput(carNames, repetitionNum))
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessage("반복 횟수는 0보다 커야 합니다.");
         }
     }
-
-//    @Nested
-//    @DisplayName("게임 진행 테스트")
-//    class gamePlayTest {
-//
-//        @Test
-//        @DisplayName("경주가 완료되면 우승자를 계산한다")
-//        void calculateWinnerWhenRaceIsOver() {
-//            String[] carNames = new String[]{"Car1", "Car2", "Car3"};
-//            List<String> selectedWinnerNames = Arrays.asList(carNames).subList(0, 2);
-//            Racer racer = new Racer(3, 5, carNames);
-//
-//            doAnswer(invocation -> {
-//                List<Car> carList = invocation.getArgument(0, List.class);
-//                carList.stream().filter(car -> selectedWinnerNames.contains(car.getName()))
-//                        .forEach(Car::move);
-//                return null;
-//            }).when(cars).play(anyInt());
-//
-//            ArrayList<Car> winners = cars.play(5);
-//
-//            assertAll(
-//                    () -> assertThat(winners.size()).isEqualTo(selectedWinnerNames.size()),
-//                    () -> winners.stream().map(Car::getName).map(selectedWinnerNames::contains)
-//                            .forEach(Assertions::assertTrue)
-//            );
-//        }
-//    }
 }
---
+++ src/test/java/org/duckstudy/movingcar/CarsTest.java
@@ -0,0 +1,40 @@
+package org.duckstudy.movingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
+import java.util.Arrays;
+import org.duckstudy.generator.Generator;
+import org.duckstudy.generator.RandomValueGenerator;
+import org.duckstudy.output.OutputView;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("자동차 경주 테스트")
+class CarsTest {
+
+    private final Generator generator = new RandomValueGenerator();
+    private final OutputView outputView = new OutputView();
+    private final Cars cars = mock(Cars.class);
+
+    @Test
+    @DisplayName("경주가 완료되면 우승자를 계산한다")
+    void calculateWinnerWhenRaceIsOver() {
+        String[] carNames = new String[]{"Car1", "Car2", "Car3"};
+        String[] selectedWinnerNames = Arrays.copyOfRange(carNames, 0, 2);
+        Cars selectedWinners = new Cars(selectedWinnerNames, generator);
+        doReturn(selectedWinners).when(cars).playAndGetWinners(anyInt(), any(OutputView.class));
+
+        Cars winners = cars.playAndGetWinners(5, outputView);
+
+        assertAll(
+                () -> assertThat(winners.getCars().size()).isEqualTo(selectedWinnerNames.length),
+                () -> assertThat(winners.getCars().get(0).getName()).isEqualTo(selectedWinnerNames[0]),
+                () -> assertThat(winners.getCars().get(1).getName()).isEqualTo(selectedWinnerNames[1])
+        );
+    }
+}
