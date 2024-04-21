7b789a8189c65a3b623a8364c920d95f9ec5f1f3
HiiWee <wpdlzhf159@gmail.com>
2024-04-21 21:39:25 +0900

    style: 인덴트 정렬

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -1,7 +1,6 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
