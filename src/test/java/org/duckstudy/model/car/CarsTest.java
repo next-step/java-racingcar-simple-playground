724d45c08e625ad2b3e862a13e4c5851ecbb547c
Mint <audwl03071@gmail.com>
2024-04-23 22:16:16 +0900

    test: 자동차 결과 테스트 재작성
    - mock을 제거하고 실제 경주 로직을 테스트하도록 수정한다.

--- src/test/java/org/duckstudy/model/car/CarsTest.java
+++ src/test/java/org/duckstudy/model/car/CarsTest.java
@@ -3,11 +3,7 @@ package org.duckstudy.model.car;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
 
-import java.util.Arrays;
 import org.duckstudy.model.generator.DefaultRandomValueGenerator;
 import org.duckstudy.model.generator.RandomValueGenerator;
 import org.junit.jupiter.api.DisplayName;
@@ -22,13 +18,12 @@ class CarsTest {
 
         private final String[] carNames = new String[]{"Car1", "Car2", "Car3"};
         private final RandomValueGenerator randomValueGenerator = new DefaultRandomValueGenerator();
-        private Cars cars;
+        private final Cars cars = new Cars(carNames, randomValueGenerator);
         private int repetitionNum;
 
         @Test
         @DisplayName("반복 횟수가 0보다 클때 성공한다")
         void gameSuccessWhenRepetitionNumIsGreaterThan0() {
-            cars = new Cars(carNames, randomValueGenerator);
             repetitionNum = 1;
 
             assertThatCode(() -> cars.validateRepetitionNum(repetitionNum))
@@ -38,7 +33,6 @@ class CarsTest {
         @Test
         @DisplayName("반복 횟수가 0 이하이면 에러가 발생한다")
         void gameFailWhenRepetitionNumIsEqualOrLessThan0() {
-            cars = new Cars(carNames, randomValueGenerator);
             repetitionNum = 0;
 
             assertThatThrownBy(() -> cars.validateRepetitionNum(repetitionNum))
@@ -47,27 +41,24 @@ class CarsTest {
     }
 
     @Nested
-    @DisplayName("결과 테스트")
+    @DisplayName("자동차 경주 결과 테스트")
     class CarRaceResultTest {
 
         private final String[] carNames = new String[]{"Car1", "Car2", "Car3"};
         private final RandomValueGenerator randomValueGenerator = new DefaultRandomValueGenerator();
-        private final Cars cars = mock(Cars.class);
+        private final Cars cars = new Cars(carNames, randomValueGenerator);
 
         @Test
         @DisplayName("경주가 완료되면 우승자를 계산한다")
         void calculateWinnerWhenRaceIsOver() {
-            String[] selectedWinnerNames = Arrays.copyOfRange(carNames, 0, 2);
-            Cars selectedWinners = new Cars(selectedWinnerNames, randomValueGenerator);
-            doReturn(selectedWinners).when(cars).calculateWinners();
+            int repetitionNum = 5;
 
+            for (int i = 0; i < repetitionNum; i++) {
+                cars.moveAll();
+            }
             Cars winners = cars.calculateWinners();
 
-            assertAll(
-                    () -> assertThat(winners.getCars().size()).isEqualTo(selectedWinnerNames.length),
-                    () -> assertThat(winners.getCars().get(0).getName()).isEqualTo(selectedWinnerNames[0]),
-                    () -> assertThat(winners.getCars().get(1).getName()).isEqualTo(selectedWinnerNames[1])
-            );
+            assertThat(winners.getAllNames()).isSubsetOf(carNames);
         }
     }
 }
