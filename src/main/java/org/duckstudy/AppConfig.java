1f70be6604adbe7d1659c63c2f2c57ade633bf41
Mint <audwl03071@gmail.com>
2024-04-24 21:57:02 +0900

    feat: AppConfig 추가
    - Application에서 객체 생성 및 의존성 주입 책임 분리, 실행 책임만 갖도록 한다.

---
+++ src/main/java/org/duckstudy/AppConfig.java
@@ -0,0 +1,23 @@
+package org.duckstudy;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import org.duckstudy.model.generator.DefaultRandomValueGenerator;
+import org.duckstudy.model.generator.RandomValueGenerator;
+import org.duckstudy.view.InputView;
+import org.duckstudy.view.OutputView;
+
+public class AppConfig {
+    public OutputView outputView() {
+        return new OutputView();
+    }
+
+    public InputView inputView() {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        return new InputView(bufferedReader, outputView());
+    }
+
+    public RandomValueGenerator randomValueGenerator() {
+        return new DefaultRandomValueGenerator();
+    }
+}
--- src/main/java/org/duckstudy/Application.java
+++ src/main/java/org/duckstudy/Application.java
@@ -1,10 +1,7 @@
 package org.duckstudy;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import org.duckstudy.controller.CarRacingController;
-import org.duckstudy.model.generator.DefaultRandomValueGenerator;
 import org.duckstudy.model.generator.RandomValueGenerator;
 import org.duckstudy.view.InputView;
 import org.duckstudy.view.OutputView;
@@ -14,14 +11,12 @@ public class Application {
     }
 
     public static void main(String[] args) throws IOException {
-        OutputView outputView = new OutputView();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        InputView inputView = new InputView(bufferedReader, outputView);
-        RandomValueGenerator randomValueGenerator = new DefaultRandomValueGenerator();
+        AppConfig appConfig = new AppConfig();
+        OutputView outputView = appConfig.outputView();
+        InputView inputView = appConfig.inputView();
+        RandomValueGenerator randomValueGenerator = appConfig.randomValueGenerator();
 
         CarRacingController carRacingController = new CarRacingController(inputView, outputView, randomValueGenerator);
         carRacingController.run();
-
-        bufferedReader.close();
     }
 }
