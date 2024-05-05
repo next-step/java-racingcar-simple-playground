ff891e02a33f1990964157707904e5196a3f7b4d
Seokju Hong <iamseokjuhong@gmail.com>
2024-05-05 20:29:13 +0900

    2단계 - 우승 자동차 구하기 구현

--- README.md
+++ README.md
@@ -61,3 +61,41 @@ public interface PositionDecider {
 ```
 
 - getPosition의 메서드를 템플릿 메서드로 만들고, generate는 각 구현체에서 구현하여 값을 제공해주면 된다.
+
+# 2단계 구현
+
+## 기능 요구사항
+
+- n대의 자동차가 참여할 수 있다.
+- 주어진 횟수 동안 n대의 자동차는 전진 또는 멈출 수 있다.
+- 0에서 9 사이에서 random 값을 구한 후 random 값이 4 이상일 경우 전진하고, 3 이하의 값이면 멈춘다.
+- 자동차 경주 게임을 완료한 후 누가 우승했는지를 구할 수 있다. 우승자는 한 명 이상일 수 있다.
+
+## 나의 구현
+
+- Racing이라는 클래스의 생성자에서 횟수와 참가 자동차를 받고 canOpen이라는 메서드를 통해 횟수가 음수이거나 참가자가 없으면 예외블 발생시킴
+- 나머지는 요구사항에 맞도록 최대한 구현해봄
+
+```java
+public class Racing {
+
+    private int times = 0;
+    private List<Car> cars = new ArrayList<>();
+
+    public Racing(int times, Car... car) {
+        canOpen(car);
+        this.times = times;
+        this.cars = List.of(car);
+    }
+
+  private void canOpen(int times, Car[] car) {
+    if (times <= 0) {
+      throw new IllegalArgumentException("times must be greater than 0");
+    }
+
+    if (car.length == 0) {
+      throw new IllegalArgumentException("Racing cars must have at least one car");
+    }
+  }
+}
+```
---
+++ src/main/java/Racing.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+public class Racing {
+
+    private int times = 0;
+    private List<Car> cars = new ArrayList<>();
+
+    public Racing(int times, Car... car) {
+        canOpen(times, car);
+        this.times = times;
+        this.cars = List.of(car);
+    }
+
+    private void canOpen(int times, Car[] car) {
+        if (times <= 0) {
+            throw new IllegalArgumentException("times must be greater than 0");
+        }
+
+        if (car.length == 0) {
+            throw new IllegalArgumentException("Racing cars must have at least one car");
+        }
+    }
+
+    public void start() {
+        while (times > 0) {
+            moveCar();
+            times--;
+        }
+    }
+
+    private void moveCar() {
+        for (Car car : cars) {
+            car.move();
+        }
+    }
+
+    public List<Car> getWinners() {
+        final Integer maxValue = findMaxScore();
+
+        return cars.stream()
+            .filter(car -> car.getPosition() == maxValue)
+            .toList();
+    }
+
+    private Integer findMaxScore() {
+        final Optional<Integer> optional = cars.stream()
+            .max(Comparator.comparingInt(Car::getPosition))
+            .map(Car::getPosition);
+        return optional.get();
+    }
+}
