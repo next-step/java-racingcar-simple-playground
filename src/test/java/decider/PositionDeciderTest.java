93057d9aa051735531d4f39013828243817f8c91
Seokju Hong <iamseokjuhong@gmail.com>
2024-05-12 22:39:01 +0900

    랜덤값 생성 테스트

--- src/test/java/decider/PositionDeciderTest.java
+++ src/test/java/decider/PositionDeciderTest.java
@@ -1,8 +1,10 @@
 package decider;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -15,5 +17,13 @@ class PositionDeciderTest {
         PositionDecider positionDecider = new StaticPositionDecider(value);
         assertThatThrownBy(positionDecider::getPosition)
             .isInstanceOf(RuntimeException.class);
+    }
+
+    @RepeatedTest(1000)
+    @DisplayName("랜덤 값 테스트 1000번 테스트해서 0~9의 범위를 벗어나지 않는지 확인")
+    void randomPositionDeciderTest() {
+        PositionDecider positionDecider = new RandomPositionDecider();
+        final int position = positionDecider.getPosition();
+        assertThat(position).isBetween(0, 9);
     }
 }
