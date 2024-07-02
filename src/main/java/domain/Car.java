e32ef1b9d5e92e5df7b569503c1b47884f5de364
춘식 <142300831+Choon0414@users.noreply.github.com>
2024-07-02 10:47:00 +0900

    1단계 - 움직이는 자동차

---
+++ src/main/java/domain/Car.java
@@ -0,0 +1,25 @@
+package domain;
+
+public class Car {
+
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void move(int num) {
+        if (num >= 4) {
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
