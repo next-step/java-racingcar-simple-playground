169f1cd22300e76493e7ea2ec7029f770407dd63
HyeonsuLee <leehyeonsu4888@naver.com>
2024-07-02 17:54:44 +0900

    feat: 4단계 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -10,7 +10,13 @@ public class InputView {
 
     public static List<String> inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        return Arrays.stream(scan.nextLine().split(",")).toList();
+        List<String> carNames = Arrays.stream(scan.nextLine().split(",")).toList();
+        carNames.forEach(carName -> {
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            }
+        });
+        return carNames;
     }
 
     public static int inputTryCount() {
