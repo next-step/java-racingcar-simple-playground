9e4d4e89295dffe1fd8778929b3093e94cb0c5d3
davidolleh <asherolleh@gmail.com>
2024-09-22 02:06:47 +0900

    <FEAT> Car class 추가

---
+++ src/main/java/Car.java
@@ -0,0 +1,18 @@
+public class Car {
+    public final String name;
+
+    public int location;
+
+    public Car(String name) {
+        this.name = name;
+        this.location = 0;
+    }
+
+    public void move() {
+        this.location++;
+    }
+
+
+}
+
+
