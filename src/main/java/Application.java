3671ff64243292013ed813d4b1c8e7bd5839c481
davidolleh <asherolleh@gmail.com>
2024-09-23 01:19:22 +0900

    <Fix> ui, domain package 분리

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,11 +1,19 @@
+import domain.RaceGame;
+import domain.RandomValueRule;
+import view.InputView;
+import view.ResultView;
+
 public class Application {
     public static void main(String[] args) {
-        final Game raceGame = new RaceGame();
 
-        raceGame.preparing();
+        String[] carNames = InputView.getCarsName();
+        int tryCount = InputView.getTryCount();
+
+        final RaceGame raceGame = new RaceGame(carNames, tryCount, new RandomValueRule());
 
         raceGame.start();
 
-        raceGame.end();
+
+        ResultView.printWinners(raceGame.getWinners());
     }
 }
