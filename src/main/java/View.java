9c531c2467f1ea473358889fd7d0f30f509d32b7
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2024-09-23 03:16:25 +0900

    View 자동차 이름, 시도 횟수 입력 view 구현

---
+++ src/main/java/View.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class View {
+    Scanner input = new Scanner(System.in);
+
+    public String inputCarName(){
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return input.nextLine();
+    }
+
+    public int inputTryCount(){
+        System.out.println("시도할 회수는 몇회인가요?");
+        return input.nextInt();
+    }
+}
