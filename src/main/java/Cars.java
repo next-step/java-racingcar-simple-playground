649fc1c50ae2f228daab3d9184fee9ef1f59f32d
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2024-09-23 01:52:28 +0900

    Cars n개의 자동차 생성함수 작성

---
+++ src/main/java/Cars.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    private static List<Car> cars;
+
+    private static List<Car> generateCar(){
+        final List<Car> cars = new ArrayList<>();
+        return cars;
+    }
+}
