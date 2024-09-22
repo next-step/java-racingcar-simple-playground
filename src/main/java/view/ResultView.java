161e42baab37f25fff5f8c0b4248460d3d3eda8b
davidolleh <asherolleh@gmail.com>
2024-09-23 00:26:07 +0900

    <FEAT> Ui 관련 역할 view class로 분리

---
+++ src/main/java/view/ResultView.java
@@ -0,0 +1,20 @@
+package view;
+
+import domain.Car;
+
+import java.util.List;
+
+public class ResultView {
+    static public void printWinners(List<Car> winners) {
+        String carNames = "";
+        for (int i = 0; i < winners.size(); i++) {
+            carNames += winners.get(i).name;
+
+            if (i != winners.size() - 1) {
+                carNames += ", ";
+            }
+        }
+
+        System.out.println(carNames + "가 최종 우승했습니다");
+    }
+}
