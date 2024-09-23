528568d688f6c356ffcb502610aed935c94834e7
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2024-09-23 14:30:14 +0900

    Cars 자동차 이름들 세팅하기

--- src/main/java/Cars.java
+++ src/main/java/Cars.java
@@ -4,8 +4,22 @@ import java.util.List;
 public class Cars {
     private static List<Car> cars;
 
+    private static String[] splitCarName(){
+        String input = View.inputCarName();
+        String[] carNames = input.split(",");
+        return carNames;
+    }
+
+    private static int carSize(String[] carNames){
+        return carNames.length;
+    }
+
     private static List<Car> generateCar(){
-        final List<Car> cars = new ArrayList<>();
+        final List<Car> cars = new ArrayList<>(carSize(splitCarName()));
+        for(String carName : splitCarName()){
+            cars.add(new Car(carName));
+        }
         return cars;
     }
+
 }
--- src/main/java/View.java
+++ src/main/java/View.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
 public class View {
-    Scanner input = new Scanner(System.in);
+    static Scanner input = new Scanner(System.in);
 
-    public String inputCarName(){
+    public static String inputCarName(){
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         return input.nextLine();
     }
