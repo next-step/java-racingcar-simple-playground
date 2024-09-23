1a3ff7d63e5ca6778c35af2637e29d3966a75e69
davidolleh <asherolleh@gmail.com>
2024-09-24 00:05:26 +0900

    <FIX> getter로 사용

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -8,7 +8,7 @@ public class ResultView {
     static public void printWinners(List<Car> winners) {
         String carNames = "";
         for (int i = 0; i < winners.size(); i++) {
-            carNames += winners.get(i).name;
+            carNames += winners.get(i).getName();
 
             if (i != winners.size() - 1) {
                 carNames += ", ";
