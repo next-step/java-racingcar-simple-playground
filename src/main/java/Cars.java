05fe7e7e2499efaec7911be469733ecaca208f3d
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2024-09-25 14:10:09 +0900

    자동차 이름 입력 예외처리

--- src/main/java/Cars.java
+++ src/main/java/Cars.java
@@ -6,6 +6,7 @@ public class Cars {
     static String[] carNames;
 
     public static void splitCarName(String input){
+        checkNull(input);
         carNames = input.split(",");
     }
 
@@ -16,14 +17,27 @@ public class Cars {
     static void generateCar(){
         cars = new ArrayList<>(carSize(carNames));
         for(String carName : carNames){
+            checkNull(carName);
+            checkLength(carName);
             cars.add(new Car(carName));
-
         }
     }
 
     public static void moveCars(){
         for(Car car : cars){
             car.moveCar();
+        }
+    }
+
+    private static void checkNull(String carName){
+        if(carName == null || carName.isBlank()){
+            throw new IllegalArgumentException("이름은 공백으로 입력될 수 없습니다.");
+        }
+    }
+
+    private static void checkLength(String carName){
+        if(carName.length()>5){
+            throw new IllegalArgumentException("이름은 최대 5자까지만 가능합니다.");
         }
     }
 
