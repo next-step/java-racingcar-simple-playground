6af44adae4f287b5cf56c892795390dd2e03c39e
davidolleh <asherolleh@gmail.com>
2024-09-26 02:16:47 +0900

    <FIX> List 생성 후 for문 도는 대신 stream 사용해보기

--- src/main/java/domain/Cars.java
+++ src/main/java/domain/Cars.java
@@ -2,6 +2,7 @@ package domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cars {
 
@@ -12,12 +13,17 @@ public class Cars {
     }
 
     public static Cars from(List<CarName> carNames) {
-        List<Car> cars = new ArrayList<Car>();
-        for (CarName carName : carNames) {
-            cars.add(new Car(carName));
-        }
+//        List<Car> cars = new ArrayList<Car>();
+//        for (CarName carName : carNames) {
+//            cars.add(new Car(carName));
+//        }
+//        return new Cars(cars);
 
-        return new Cars(cars);
+
+        return new Cars(
+                carNames.stream().map(Car::new).collect(Collectors.toList())
+        );
+
     }
 
     public List<Car> getCars() {
