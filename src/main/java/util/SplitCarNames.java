b25f048191f1afbe37e1494b9dcc084a8799662f
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2024-09-27 02:14:05 +0900

    refact : 코드리뷰 수정 - regax 매직넘버 수정

--- src/main/java/util/SplitCarNames.java
+++ src/main/java/util/SplitCarNames.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SplitCarNames {
+    private static final String SPLIT_CHARACTERS = ",";
+
     public static List<String> splitCarNames(String carNames) {
-        return new ArrayList<String>(List.of(carNames.split(",")));
+        return new ArrayList<String>(List.of(carNames.split(SPLIT_CHARACTERS)));
     }
 }
