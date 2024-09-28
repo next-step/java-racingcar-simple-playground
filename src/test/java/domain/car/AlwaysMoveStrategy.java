9005ee6ba4bf53a1bb6f47c7156e9baf6d2dd362
goldm0ng <jujs45177@gmail.com>
2024-09-28 18:22:16 +0900

    <Add> 테스트를 위한 MoveStrategy 구현체 추가

---
+++ src/test/java/domain/car/AlwaysMoveStrategy.java
@@ -0,0 +1,11 @@
+package domain.car;
+
+import domain.racegame.MoveStrategy;
+
+public class AlwaysMoveStrategy implements MoveStrategy {
+
+    @Override
+    public boolean canMove(int number) {
+        return true;
+    }
+}
