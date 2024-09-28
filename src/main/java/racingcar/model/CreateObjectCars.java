ae75eb5963636994c472746625c6e62d3671366f
hyebin <shin378378@naver.com>
2024-09-29 00:25:34 +0900

    Rule을 CreateObjectCars 안으로 넣기

--- src/main/java/racingcar/model/CreateObjectCars.java
+++ src/main/java/racingcar/model/CreateObjectCars.java
@@ -15,12 +15,19 @@ public class CreateObjectCars {
     //자동차 객체 생성 및 리스트에 추가
     public List<Car> createCarList(String[] carNamesArr) {
         List<Car> carList = new ArrayList<>();
-        Rule rule = new Rule();
         for (String name : carNamesArr) {
-            rule.CarNameLengthLimit(name);
+            carNameLengthLimit(name);
             Car car = new Car(name);
             carList.add(car);
         }
         return carList;
+    }
+
+     //자동차 이름 글자수 제한
+    public void carNameLengthLimit(String carName) {
+        int lengthLimit=5;
+        if (carName.length() > lengthLimit) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다. 프로그램을 종료합니다.");
+        }
     }
 }
