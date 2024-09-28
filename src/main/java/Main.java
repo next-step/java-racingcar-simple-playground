57e85572e4bee691576ca6f8a2ce77cf4c7ba5f4
goldm0ng <jujs45177@gmail.com>
2024-09-28 18:11:45 +0900

    <Fix> controller 패키지 생성에 따른 import

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,11 @@
+import controller.RacingGameController;
 import view.InputView;
 import view.ResultView;
 
 public class Main {
     public static void main(String[] args) {
 
-        RacingGameController racingGameController = new RacingGameController(new InputView(),new ResultView());
+        RacingGameController racingGameController = new RacingGameController(new InputView(), new ResultView());
 
         racingGameController.racingStart();
 
