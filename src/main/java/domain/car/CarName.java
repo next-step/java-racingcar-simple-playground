b2595ac9bc20607b1adc4f51c573393d6c3f69b9
goldm0ng <jujs45177@gmail.com>
2024-09-28 09:34:55 +0900

    <Add Class> 자동차 이름 래퍼클래스 사용

---
+++ src/main/java/domain/car/CarName.java
@@ -0,0 +1,13 @@
+package domain.car;
+
+public class CarName {
+    private final String name;
+
+    public CarName(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
