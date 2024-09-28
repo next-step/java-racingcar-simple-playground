df7ce1515c75e079e1721f28fbbc31e5688dbfe5
goldm0ng <jujs45177@gmail.com>
2024-09-28 11:41:31 +0900

    <Fix> 메서드명 구체적으로 수정 및 출력 로직 변경

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -1,23 +1,22 @@
 package view;
 
-import domain.Car;
-
-import java.util.List;
+import domain.car.Cars;
 
 public class ResultView {
 
-    public void printOneRoundResult(List<Car> cars){
-        for (Car car : cars) {
-            System.out.printf("%s : %s\n",car.getCarName(),"-".repeat(car.getPosition()));
-        }
+    public void printExecutionResultsMessage() {
+        System.out.println("\n실행 결과");
+    }
+
+    public void printRaceResult(Cars cars) {
+        cars.getCars().forEach(car -> {
+            System.out.println(car.getCarName() + " : " + "-".repeat(car.getCarPosition()));
+        });
         System.out.println();
     }
 
-    public void printWinners(String winners){
-        System.out.println(winners+"가 최종 우승했습니다.");
+    public void printWinners(String winners) {
+        System.out.println(winners + "가 최종 우승했습니다.");
     }
 
-    public void printString() {
-        System.out.println("\n실행 결과");
-    }
 }
