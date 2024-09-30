51723e4810e55a5e03750a10b96ea47e270481c1
신지 <sinji@sinjis-MacBook-Air.local>
2024-09-30 16:50:58 +0900

    refactor: line 12 public->private.

--- src/main/java/domain/CarName.java
+++ src/main/java/domain/CarName.java
@@ -4,16 +4,19 @@ public record CarName(String value) {
     private static final int MAX_LENGTH = 5;
 
     public static CarName from(final String name) {
-        validateName(name);
+        validateNotBlank(name);
+        validateLengthUnderFive(name);
         return new CarName(name);
     }
 
-    public static void validateName(final String name) {
-        // 이름에서 발생할 수 있는 예외) 이름이 없을 경우
+    private static void validateNotBlank(final String name) {
         if (name.isBlank()) {
             throw new IllegalArgumentException("자동차 이름은 공백일 수 없습니다.");
         }
-        // 3단계 기능 요구사항(자동차 이름은 5자 이하)
+
+    }
+
+    private static void validateLengthUnderFive(final String name) {
         if (name.length() > MAX_LENGTH) {
             throw new IllegalArgumentException("자동차의 이름은 5자 이하여야 합니다.");
         }
