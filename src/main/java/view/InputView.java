0dd46c23637b2ab6452a1ac674d07ae60e865414
신지 <sinji@sinjis-MacBook-Air.local>
2024-09-30 14:25:44 +0900

    docs: sc -> scanner

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,19 +1,23 @@
 package view;
 
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class InputView {
 
-    private static final Scanner sc = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static List<String> readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        final String carNames = sc.nextLine(); // 연속적으로 입력되는(근데 ,를 포함한) 차량 이름을 담는 String 형 carNames
-        return new ArrayList<>(Arrays.asList(carNames.split(","))); // 배열의 요소를 리스트로 반환..?
+        final String carNames = scanner.nextLine();
+        return new ArrayList<>(Arrays.asList(carNames.split(",")));
     }
 
     public static int readAttemptNumber() {
         System.out.println("시도할 회수는 몇회인가요?");
-        return sc.nextInt();
+        return scanner.nextInt();
     }
 }
