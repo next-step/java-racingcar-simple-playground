e6a5ecef0db14533bfb0489b63984e95977f97b8
IDLE <83659656+idle2534@users.noreply.github.com>
2024-10-03 20:15:49 +0900

    Feat: 게임 실행 가능한 메인 메서드 추가

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    RacingCarGame game = new RacingCarGame();
+    game.setGame();
+    game.startGame();
+    game.resetGame();
+  }
+}
