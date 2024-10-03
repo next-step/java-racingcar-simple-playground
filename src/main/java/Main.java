f1755382d742862917857a8aa34ef99ea4b2a1f8
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2024-10-03 15:44:16 +0900

    [REFACTOR] Game과 Main 합치기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,24 +5,7 @@ import view.View;
 
 public class Main {
     public static void main(String[] args) {
-        final String inputNames = View.inputCarName();;
-        final int tryCount = View.inputTryCount();
-        Cars cars = new Cars();
-
-        try {
-            //입력 받은 자동차 이름 분리
-            cars.splitCarName(inputNames);
-            //자동차 생성 및 자동차 이름 부여
-            cars.generateCar();
-            //게임 시작 및 게임 과정 출력
-            Game racingame = new Game(cars, tryCount);
-        }catch (IllegalArgumentException e){
-            System.out.println(e.getMessage());
-            return;
-        }
-
-        WinnerSelector winnerSelector = new WinnerSelector();
-        //게임 결과 우승자 출력
-        View.printWinner(winnerSelector.getWinners(cars.getCars()));
+        Game game = new Game();
+        game.startGame();
     }
 }
--- src/main/java/controller/Game.java
+++ src/main/java/controller/Game.java
@@ -1,12 +1,37 @@
 package controller;
 
 import domain.Cars;
+import domain.WinnerSelector;
 import view.View;
 
 public class Game {
 
+    public void startGame() {
+        try {
+            String inputNames = View.inputCarName();
+            int tryCount = View.inputTryCount();
+
+            Cars cars = new Cars();
+
+            //입력 받은 자동차 이름 분리 및 자동차 생성
+            cars.splitCarName(inputNames);
+            cars.generateCar();
+
+            //게임 시작 및 게임 과정 출력
+            Game(cars, tryCount);
+
+            //우승자 출력
+            WinnerSelector winnerSelector = new WinnerSelector();
+            View.printWinner(winnerSelector.getWinners(cars.getCars()));
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
     //생성자를 통해 게임 시작
-    public Game(Cars cars, int tryCount){
+    public void Game(Cars cars, int tryCount){
         checkNum(tryCount);
         for(int i = 0; i<tryCount; i++){
             cars.moveCars();
