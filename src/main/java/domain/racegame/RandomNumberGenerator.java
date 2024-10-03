671045fa8ead7a7b69d7a6f62ebc1c4c7910cd01
goldm0ng <jujs45177@gmail.com>
2024-10-03 14:40:43 +0900

    <Fix> 매직 넘버 제거 및 정적 메서드로 변경

--- src/main/java/domain/racegame/RandomNumberGenerator.java
+++ src/main/java/domain/racegame/RandomNumberGenerator.java
@@ -3,9 +3,13 @@ package domain.racegame;
 import java.util.Random;
 
 public class RandomNumberGenerator {
+
     private static final Random RANDOM = new Random();
 
-    public int generateRandomNumber() {
-        return RANDOM.nextInt(10);
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+
+    public static int generateRandomNumber() {
+        return RANDOM.nextInt(MAX_NUMBER - MIN_NUMBER + 1) + MIN_NUMBER;
     }
 }
