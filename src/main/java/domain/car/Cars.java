036d9a72d5ed53da1d369dbb5a122761b39e6106
goldm0ng <jujs45177@gmail.com>
2024-10-03 14:39:20 +0900

    <Fix> Cars에 대한 예외처리 추가

--- src/main/java/domain/car/Cars.java
+++ src/main/java/domain/car/Cars.java
@@ -3,6 +3,7 @@ package domain.car;
 import domain.racegame.MoveStrategy;
 import domain.racegame.RandomNumberGenerator;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,18 +11,21 @@ public class Cars {
     private final List<Car> cars;
 
     public Cars(List<Car> cars) {
+        if (cars.size() <= 1) {
+            throw new IllegalArgumentException("자동차 수는 최소 2대여야 합니다.");
+        }
         this.cars = cars;
     }
 
-    public void raceOneRound(MoveStrategy moveStrategy, RandomNumberGenerator randomNumberGenerator) {
+    public void raceOneRound(MoveStrategy moveStrategy) {
         for (Car car : cars) {
-            int randomNumber = randomNumberGenerator.generateRandomNumber();
+            int randomNumber = RandomNumberGenerator.generateRandomNumber();
             car.move(moveStrategy, randomNumber);
         }
     }
 
     public List<Car> getCars() {
-        return cars;
+        return Collections.unmodifiableList(cars);
     }
 
     public int getMaxPosition() {
