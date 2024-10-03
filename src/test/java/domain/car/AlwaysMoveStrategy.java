a24af32bfb3a40515fd1601065132fce7e39987f
goldm0ng <jujs45177@gmail.com>
2024-10-03 14:56:14 +0900

    <Maintain>

--- src/test/java/domain/car/AlwaysMoveStrategy.java
+++ src/test/java/domain/car/AlwaysMoveStrategy.java
@@ -3,7 +3,6 @@ package domain.car;
 import domain.racegame.MoveStrategy;
 
 public class AlwaysMoveStrategy implements MoveStrategy {
-
     @Override
     public boolean canMove(int number) {
         return true;
--- src/test/java/domain/car/NeverMoveStrategy.java
+++ src/test/java/domain/car/NeverMoveStrategy.java
@@ -3,7 +3,6 @@ package domain.car;
 import domain.racegame.MoveStrategy;
 
 public class NeverMoveStrategy implements MoveStrategy {
-
     @Override
     public boolean canMove(int number) {
         return false;
