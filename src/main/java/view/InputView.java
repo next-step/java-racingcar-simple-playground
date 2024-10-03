cdba416fdfe926a439c7c5ea64fbe1d1dca0662f
goldm0ng <jujs45177@gmail.com>
2024-10-03 14:49:46 +0900

    <Fix> 정적 메서드 사용 및 입력 역할 부여

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,17 +1,21 @@
 package view;
 
+import java.util.Scanner;
+
 public class InputView {
 
-    public void printCarsNameInputSentence() {
+    public static String inputCarsNameFromUser() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
     }
 
-    public void printRaceCountInputSentence() {
+    public static int inputRaceCountFromUser() {
         System.out.println("시도할 회수는 몇회인가요?");
-    }
 
-    public void printError(String errorMessage) {
-        System.out.println("[에러] " + errorMessage);
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
     }
 
 }
