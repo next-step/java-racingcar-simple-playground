6b51467f06d28e27bc660866425193a8199a0783
csh3424 <csh3424@icloud.com>
2024-10-03 14:38:16 +0900

    Feat: 경주 우승자 찾기 기능 추가

--- src/main/java/CarRace.java
+++ src/main/java/CarRace.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+
 public class CarRace {
   public RacingCar[] cars;
   private final int rounds;
+  private int winnerIndex;
+  private final ArrayList<Integer> coWinners = new ArrayList<>();
 
   public CarRace(int car_num, int round_num) {
     rounds = round_num;
@@ -22,5 +26,33 @@ public class CarRace {
   private void moveAllCars() {
     for(RacingCar car : cars)
       car.move();
+  }
+
+  private int findWinningDistance() {
+    winnerIndex = 0;
+    for(int i = 0; i < cars.length; i++)
+      updateWinner(i);
+    return cars[winnerIndex].distance;
+  }
+
+  private void updateWinner(int number) {
+    if (number < cars.length - 1 && cars[number].distance < cars[number + 1].distance)
+      winnerIndex = number + 1;
+  }
+
+  private void findAllWinners(int number, int winning_distance) {
+    if(cars[number].distance == winning_distance)
+      coWinners.add(number);
+  }
+
+  public void displayAllWinners() {
+    int winning_distance = findWinningDistance();
+
+    for(int i = 0; i < cars.length; i++)
+      findAllWinners(i, winning_distance);
+
+    System.out.println("Winner:");
+    for(int coWinnerIndex : coWinners)
+      System.out.println(cars[coWinnerIndex].name);
   }
 }
