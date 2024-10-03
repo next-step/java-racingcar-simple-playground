482b0f8d07a85dbd7aafc21a6a3804f1ad1250c3
goldm0ng <jujs45177@gmail.com>
2024-10-03 14:17:00 +0900

    <Fix> 래퍼클래스 내에서 예외처리 하도록 수정

--- src/main/java/domain/car/CarName.java
+++ src/main/java/domain/car/CarName.java
@@ -3,7 +3,12 @@ package domain.car;
 public class CarName {
     private final String name;
 
-    public CarName(String name){
+    public CarName(String name) {
+
+        if (name.length() > 5 || name.isBlank()) {
+            throw new IllegalArgumentException("자동차 이름은 1자 이상 5자 이하여야 합니다.");
+        }
+
         this.name = name;
     }
 
