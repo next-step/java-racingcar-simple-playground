40404d09c7e6f6fed6784a0f88dd2497f580ba4f
StoneCAU <seokwoo1999@naver.com>
2024-10-04 16:28:07 +0900

    Car 객체 구현

---
+++ src/main/java/Car.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+public class Car {
+    private final String name;
+    private int location;
+
+    private Car(String name) {
+        this.name = name;
+        this.location = 0;
+    }
+
+    public void move() {
+        if(isMovable()) location++;
+    }
+
+    private static boolean isMovable() {
+        Random random = new Random();
+
+        return random.nextInt(9) + 1 >= 4;
+    }
+
+    public int getLocation() {
+        return location;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
