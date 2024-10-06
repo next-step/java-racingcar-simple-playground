91488de9edb8d8b2cc7f97fe028103dc80af7462
bingle625 <fhqhxm8899@gmail.com>
2024-10-06 12:04:44 +0900

    feat: 게임 진행 기능 및 승자 확인 기능 구현

--- src/main/java/RacingGame.java
+++ src/main/java/RacingGame.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class RacingGame {
 
-  private List<Car> cars;
+  private final List<Car> cars;
 
   public RacingGame() {
     this.cars = new ArrayList<>();
@@ -11,6 +12,31 @@ public class RacingGame {
 
   public void addCar(Car car) {
     this.cars.add(car);
+  }
+
+  public void proceedGame() {
+    Random random = new Random();
+    this.cars.forEach((car) -> {
+      int randomNumber = random.nextInt(9);
+      car.move(randomNumber);
+    });
+  }
+
+  public List<String> getWinners() {
+    int maxLocation = 0;
+    List<String> winners = new ArrayList<>();
+
+    for (Car car : this.cars) {
+      if (maxLocation < car.getCoordinateX()) {
+        winners.clear();
+        winners.add(car.getName());
+      }
+
+      if (maxLocation == car.getCoordinateX()) {
+        winners.add(car.getName());
+      }
+    }
+    return winners;
   }
 
   public int getCurrentCarCount() {
