74656e577d24164459596e8d579b8fbc22f2cce2
IDLE <83659656+idle2534@users.noreply.github.com>
2024-10-11 07:58:08 +0900

    Remove: Remove class unused in mvc model

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-    RacingCarGame game = new RacingCarGame();
-    game.setGame();
-    game.startGame();
-    game.resetGame();
-  }
-}
