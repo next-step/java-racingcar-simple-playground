d66c9fe791dd479e413e8ca9da96f70fd0388a3d
StoneCAU <seokwoo1999@naver.com>
2024-10-12 17:32:13 +0900

    feat: CarParser 검증에 중복된 이름 검증 추가

--- src/main/java/domain/CarParser.java
+++ src/main/java/domain/CarParser.java
@@ -7,12 +7,23 @@ public class CarParser {
     public static List<Car> parsing(String input) {
         if (!isValidInput(input)) throw new IllegalArgumentException("올바르지 않은 입력 형식입니다.");
 
-        return Arrays.stream(input.split(","))
+        List<Car> cars = Arrays.stream(input.split(","))
                 .map(name -> {
                     if (!isValidLength(name)) throw new IllegalArgumentException("자동차 이름은 1글자 이상 5글자 이하여야 합니다.");
                     return new Car(name);
                 })
                 .toList();
+
+        if(isDuplicatedName(cars)) throw new IllegalArgumentException("중복된 자동차 이름은 입력될 수 없습니다.");
+
+        return cars;
+    }
+
+    private static boolean isDuplicatedName(List<Car> cars) {
+        return cars.stream()
+                .map(Car::getName)
+                .distinct()
+                .count() != cars.size();
     }
 
     private static boolean isValidInput(String input) {
