749841a9acc6c7edc65b362ad4d103ed0e7c056d
wzrabbit <curious.wzrabbit@gmail.com>
2025-01-05 23:19:00 +0900

    ✨ feat: Car 클래스에서 자동차 이름이 잘못된 경우 예외를 발생시키도록 구현

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -4,10 +4,23 @@ public class Car {
     private final String name;
     private int distance = 0;
     private final RandomNumberGenerator randomNumberGenerator;
+    public static final int MAX_CAR_NAME_LENGTH = 5;
 
     public Car(String name, RandomNumberGenerator randomNumberGenerator) {
+        validateCarName(name);
+
         this.name = name;
         this.randomNumberGenerator = randomNumberGenerator;
+    }
+
+    private void validateCarName(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 자동차의 이름은 비어 있어서는 안 됩니다.");
+        }
+
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("[ERROR] 자동차의 이름의 길이는 " + MAX_CAR_NAME_LENGTH + "자를 넘어서는 안 됩니다.");
+        }
     }
 
     public String getName() {
