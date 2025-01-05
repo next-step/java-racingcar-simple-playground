2f886b70e2a938518ab38b0f9c024f7b753fb80b
wzrabbit <curious.wzrabbit@gmail.com>
2025-01-05 22:25:38 +0900

    ✨ feat: 자동차 경주를 진행하는 Controller를 구현

---
+++ src/main/java/controller/CarRaceController.java
@@ -0,0 +1,33 @@
+package controller;
+
+import model.Car;
+import model.CarRaceGame;
+import model.CarRandomNumberGenerator;
+import view.InputView;
+import view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarRaceController {
+    private final CarRandomNumberGenerator carRandomNumberGenerator = new CarRandomNumberGenerator();
+
+    public void start() {
+        List<String> carNames = InputView.inputCarNames();
+        int roundCount = InputView.inputRoundCount();
+        List<Car> cars = new ArrayList<>();
+
+        for (String carName: carNames) {
+            cars.add(new Car(carName, carRandomNumberGenerator));
+        }
+
+        CarRaceGame carRaceGame = new CarRaceGame(cars);
+
+        for (int i = 1; i <= roundCount; i++) {
+            List<Car> currentCars = carRaceGame.playSingleRoundAndGetCars();
+            OutputView.printRaceStatus((ArrayList<Car>) currentCars);
+        }
+
+        OutputView.printWinners(carRaceGame.getWinners());
+    }
+}
