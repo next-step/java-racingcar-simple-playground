c7d6c520d176ff3b016a41a1338981434ec23fb1
Rulu <79538610+hafnium1923@users.noreply.github.com>
2025-01-16 13:59:57 +0900

    고생하셨습니다.
    :tada: PR 머지 완료! :tada:

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import controller.RaceController;
+
+public class Main {
+    public static void main(String[] args) {
+        RaceController controller = new RaceController();
+        controller.run();
+    }
+}
---
+++ src/main/java/domain/Car.java
@@ -0,0 +1,27 @@
+package domain;
+
+import java.util.Random;
+
+public class Car {
+    private final String name;
+    private int distance = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void move(Random random) {
+        int randomValue = random.nextInt(10);
+        if (randomValue >= 4) {
+            distance++;
+        }
+    }
+}
