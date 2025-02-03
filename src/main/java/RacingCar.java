b298a024857729550950513937a855f64a52215e
dldb-chamchi <dladkfl49@gmail.com>
2025-02-03 11:28:32 +0900

    RacingCar 생성자 설정

--- src/main/java/RacingCar.java
+++ src/main/java/RacingCar.java
@@ -1,3 +1,9 @@
 public class RacingCar {
-    //레이싱 카
+    private final String carName;
+    public int numOfForward = 0;
+    RacingCar(String carName){
+        if(carName == null || carName.isEmpty()) throw new RuntimeException("제대로된 자동차 이름을 입력해주세요.");
+        if(carName.length() > 5) throw new RuntimeException("자동차 이름은 5자 이하로 설정해주세요.");
+        this.carName = carName;
+    }
 }
