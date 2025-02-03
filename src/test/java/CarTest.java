25b1a0e46eff64fd96c20265d535d2956df9347d
Donghun <2dh2@naver.com>
2025-02-03 15:45:22 +0900

    feat. 테스트 메소드마다 중복되는 부분 beforeEach로 제거

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -5,11 +6,15 @@ import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("움직이는 자동차 테스트 클래스")
 public class CarTest {
+    private Car car;
+
+    @BeforeEach
+    public void beforeEach() {
+        car = new Car("Kim");
+    }
     @Test
     @DisplayName("자동차 객체 생성이 제대로 되었는가")
     public void testCreateCar() {
-        Car car = new Car("Kim");
-
         assertThat(car.getName()).isEqualTo("Kim");
         assertThat(car.getPos()).isEqualTo(0);
     }
@@ -17,8 +22,6 @@ public class CarTest {
     @Test
     @DisplayName("자동차가 정상적으로 전진하는가")
     public void testMoveCar() {
-        Car car = new Car("Kim");
-
         car.move(4);
 
         assertThat(car.getPos()).isEqualTo(1);
@@ -27,8 +30,6 @@ public class CarTest {
     @Test
     @DisplayName("자동차가 정상적으로 정지하는가")
     public void testStopCar() {
-        Car car = new Car("Kim");
-
         car.move(3);
 
         assertThat(car.getPos()).isEqualTo(0);
--- src/test/java/GameControllerTest.java
+++ src/test/java/GameControllerTest.java
@@ -1,4 +1,5 @@
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,64 +10,41 @@ import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("자동차 경주 관련 테스트 클래스")
 public class GameControllerTest {
-    @Test
-    @DisplayName("자동차 참여가 정상적으로 되는가")
-    public void testCarJoin() {
-        GameController gc = new GameController();
+    private GameController gc;
+    private Car car1, car2, car3;
 
-        Car car1 = new Car("Kim");
-        Car car2 = new Car("Lee");
-        Car car3 = new Car("Park");
+    @BeforeEach
+    public void beforeEach() {
+        gc = new GameController();
+
+        car1 = new Car("Kim");
+        car2 = new Car("Lee");
+        car3 = new Car("Park");
 
         gc.join(car1);
         gc.join(car2);
         gc.join(car3);
+    }
 
-        List<Car> carList = gc.getCarList();
-
-        assertThat(carList.size()).isEqualTo(3);
+    @Test
+    @DisplayName("자동차 참여가 정상적으로 되는가")
+    public void testCarJoin() {
+        assertThat(gc.getCarList()).hasSize(3);
     }
 
     @Test
     @DisplayName("단독 우승자가 정상적으로 구해지는가")
     public void testGetSoloWinners() {
-        GameController gc = new GameController();
-
-        Car car1 = new Car("Kim");
-        Car car2 = new Car("Lee");
-        Car car3 = new Car("Park");
-
-        gc.join(car1);
-        gc.join(car2);
-        gc.join(car3);
-
-        List<Car> carList = gc.getCarList();
-
-        carList.get(1).move(4);
-
-        List<Car> expected = new ArrayList<>();
-        expected.add(car2);
+        gc.getCarList().get(1).move(4);
 
         assertThat(gc.getWinners())
                 .extracting(Car::getName)
-                .isEqualTo(expected);
+                .containsExactly("Lee");
     }
 
     @Test
     @DisplayName("공동 우승자가 정상적으로 구해지는가")
     public void testGetSameWinners() {
-        GameController gc = new GameController();
-
-        Car car1 = new Car("Kim");
-        Car car2 = new Car("Lee");
-        Car car3 = new Car("Park");
-
-        gc.join(car1);
-        gc.join(car2);
-        gc.join(car3);
-
-        List<Car> carList = gc.getCarList();
-
         assertThat(gc.getWinners())
                 .containsExactlyInAnyOrder(car1, car2, car3);
     }
