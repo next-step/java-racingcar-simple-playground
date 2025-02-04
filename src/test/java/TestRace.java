c52bad405d54ab20c7200d0fbfa8d299a2988ac6
digital8150 <wptpgud@gmail.com>
2025-02-04 17:03:57 +0900

    test: 자동차 경주 (우승 자동차 구하기) 테스트

---
+++ src/test/java/TestRace.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Nested;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("자동차 경주 테스트")
+public class TestRace {
+
+    @Nested
+    @DisplayName("자동차 경주 기능 테스트")
+    class TestRaceFeatures{
+
+        @Test
+        @DisplayName("우승 자동차 구하기 테스트")
+        void testRace(){
+            //given
+            final Race testRace = new Race("test1,test2,test3,test4");
+
+            //when
+            //10회 동안 경주
+            testRace.runRace(10);
+
+            //then
+            assertThat(testRace.getWinner()).isNotNull();
+        }
+    }
+}
