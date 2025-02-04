50e86c5f611a0abe0cccfebf584cd86a22487e34
dldb-chamchi <dladkfl49@gmail.com>
2025-02-04 12:31:59 +0900

    레이싱카 이름 설정

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,6 +1,7 @@
-import java.util.Scanner;
-import  java.util.Random;
+import java.util.*;
+
 public class Game {
+    private List<RacingCar> racingCars = new ArrayList<>();
 
     static Scanner in = new Scanner(System.in);
 
@@ -24,12 +25,35 @@ public class Game {
         if(runCount <= 0) throw new RuntimeException("실행횟수가 0이하 입니다.");
     }
 
-    public String[] splitInput(String carNameInput){
+    private String[] splitInput(String carNameInput){
         return carNameInput.split(",");
     }
 
+    private Set<String> setCarName(String[] carNameInput){
+        Set<String> carNames = new HashSet<>();
+        for(var name : carNameInput) carNames.add(name.trim());
+        if(carNames.isEmpty()) throw new RuntimeException("");
+        return carNames;
+    }
+
+
+
     public int randomPick(){
         return new Random().nextInt();
+    }
+
+    public void gameStatePrint(){
+
+    }
+
+    public void firstGameStart(){
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public void gameResultPrint(){
+        System.out.println("실행 결과");
     }
 
     public static void runTest(){
