4a90a8ca42d6cbdd11d9b609dc59a1143864e3cf
dldb-chamchi <dladkfl49@gmail.com>
2025-02-04 11:56:11 +0900

    실행 횟수 입력 메소드 예외 기능 추가

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -8,8 +8,20 @@ public class Game {
         return in.nextLine();
     }
 
-    public static int inputRunNumber(){
-        return in.nextInt();
+    public static int inputRunCount(){
+        int runCount;
+        try{
+            runCount = in.nextInt();
+            runCountMinusCheck(runCount);
+        }
+        catch (NumberFormatException e) {
+            throw new RuntimeException("정수만 가능합니다.");
+        }
+        return runCount;
+    }
+
+    public static void runCountMinusCheck(int runCount){
+        if(runCount <= 0) throw new RuntimeException("실행횟수가 0이하 입니다.");
     }
 
     public String[] splitInput(String carNameInput){
