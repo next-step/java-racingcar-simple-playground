f312de5df697cd8e6d11dcbd4ca2fbedcf747fe7
Baejinho4028 <onionring4028@naver.com>
2025-02-08 03:24:02 +0900

    feat: 1단계 움직이는 자동차 구하기 기능&테스트 추가

---
+++ src/main/java/Car.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+
+public class Car {
+
+    private final static int MOVE_CONDITION = 4;
+    private final Random random = new Random();
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름은 필수입니다.");
+        }
+    }
+
+    public void move() {
+        int randomValue = getRandomInt();
+        if (isMovable(randomValue)) {
+            position++;
+        }
+    }
+
+    int getRandomInt() {
+        return random.nextInt(10);
+    }
+
+    private boolean isMovable(int randomValue) {
+        return randomValue >= MOVE_CONDITION;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
