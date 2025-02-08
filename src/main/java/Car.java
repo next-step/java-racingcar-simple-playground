dd627ff3f5702dc517e0a92b90afe8b531be9226
kh277 <jkhcoding@naver.com>
2025-02-08 22:10:15 +0900

    Finish step3

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,11 +1,10 @@
 import java.util.Random;
-import java.util.ArrayList;
 
 public class Car
 {
     private final Random random = new Random();
     private String carName = "";
-    private int moveCount = 0;
+
     private int curPos = 0;
 
     public void setCarName(String name)
@@ -23,15 +22,19 @@ public class Car
         return curPos;
     }
 
+    private void printCurPos()
+    {
+        System.out.println(carName + " : " + "-".repeat(curPos));
+    }
+
     public int MoveCar()
     {
-        moveCount += 1;
         int randomSpeed = random.nextInt(10);
 
-        if (randomSpeed < 4)
-            return curPos;
+        if (randomSpeed >= 4)
+            curPos += 1;
 
-        curPos += randomSpeed;
+        printCurPos();
         return curPos;
     }
 }
--- src/main/java/Racing.java
+++ src/main/java/Racing.java
@@ -1,35 +1,89 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class Racing
 {
-    private final List<Car> cars = new ArrayList<>();
+    private static final List<Car> cars = new ArrayList<>();
+    private static final Scanner scanner = new Scanner(System.in);
 
-    public void addCar(Car curCar)
+    // 이름이 5글자 이하의 차만 필터링 후 Car 객체 생성
+    private static String[] checkFiltering(String[] tempCarName)
     {
+        String[] carName = Arrays.stream(tempCarName)
+                .filter(name -> name.length() <= 5)
+                .toArray(String[]::new);
+
+        // 에러 처리
+        if (carName.length == 0)
+            throw new IllegalArgumentException("There is no car.");
+
+        return carName;
+    }
+
+    private static void addCar(String curCarName)
+    {
+        Car curCar = new Car();
+        curCar.setCarName(curCarName);
         cars.add(curCar);
     }
 
-    public void startRace(int raceCount)
+    public static void startRace(int raceCount)
     {
         for (int i=0; i<raceCount; i++)
+        {
             cars.forEach(Car::MoveCar);
+            System.out.println();
+        }
     }
 
-    private int findMaxPos()
+    private static int findMaxPos()
     {
         return cars.stream()
                 .mapToInt(Car::getCarPos)
                 .max()
-                .orElseThrow(() -> new IllegalArgumentException("There is no car."));
+                .orElseThrow(() -> new IllegalArgumentException("5글자 이내인 차가 존재하지 않습니다."));
     }
 
-    public List<String> findWinner()
+    public static List<String> findWinner()
     {
         return cars.stream()
                 .filter(car -> car.getCarPos() == findMaxPos())
                 .map(Car::getCarName)
                 .collect(Collectors.toList());
+    }
+
+    public static void startGame()
+    {
+        // 이름 입력
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String[] tempCarName = scanner.nextLine().split(",");
+
+        // Car 객체 생성
+        for (String name: checkFiltering(tempCarName))
+            addCar(name);
+
+        // 시도 횟수 입력
+        System.out.println("시도할 횟수는 몇회인가요?");
+        int raceCount = scanner.nextInt();
+
+        // 에러 처리
+        if (raceCount <= 0)
+            throw new IllegalArgumentException("자연수를 입력해 주세요.");
+
+        // 경주 진행
+        System.out.println("\n실행 결과");
+        startRace(raceCount);
+
+        // 승자 출력
+        List<String> winner = findWinner();
+        System.out.println(String.join(", ", winner) + "가 최종 우승했습니다.");
+    }
+
+    public static void main(String[] args)
+    {
+        startGame();
     }
 }
