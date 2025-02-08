7bcc0253cba2b0ba31945e6a6fef0bd72641bbf1
dldb-chamchi <dladkfl49@gmail.com>
2025-02-08 14:53:03 +0900

    리팩터링 시작

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,5 +1,8 @@
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class GameTest {
     @Test
     void test1(){
