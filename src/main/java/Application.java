e96b905d990977fcc84a2eab2c901743d8e32356
Donghun <2dh2@naver.com>
2025-02-09 20:16:32 +0900

    feat. 메인 클래스 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Application {
+import java.util.List;
+
+public class Application {
+    public static void main(String[] args) {
+        InputController inputController = new InputController();
+        RacingGame racingGame = new RacingGame();
+
+        List<String> nameList = inputController.inputNames();
+        int times = inputController.inputTimes();
+
+        nameList.forEach((name) -> {
+            racingGame.join(new Car(name));
+        });
+
+        racingGame.race(times);
+        racingGame.printWinners();
+    }
 }
