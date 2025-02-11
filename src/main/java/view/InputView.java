b8764ff686528058d22a8a21802ede6a5a6ca424
윤준석(YunJunSeok) <wicaduce@gmail.com>
2025-02-11 22:05:06 +0900

    Feat: carList인라인화 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -14,12 +14,10 @@ public class InputView {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String input = scanner.nextLine();
 
-        List<Car> carList = Arrays.stream(input.split(","))
+        return Arrays.stream(input.split(","))
                 .map(String::trim) // 공백 제거
                 .map(Car::new)
                 .collect(Collectors.toList());
-
-        return carList;
     }
 
     public static int getRaceRounds() {
