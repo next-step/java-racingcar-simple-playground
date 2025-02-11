8dfd714774dba8eaf05207c46d41dc395c4f26f8
윤준석(YunJunSeok) <wicaduce@gmail.com>
2025-02-11 22:03:26 +0900

    Feat: 1-3단계 미션 코드 작성

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,29 @@
+package view;
+
+import model.Car;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class InputView {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static List<Car> getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String input = scanner.nextLine();
+
+        List<Car> carList = Arrays.stream(input.split(","))
+                .map(String::trim) // 공백 제거
+                .map(Car::new)
+                .collect(Collectors.toList());
+
+        return carList;
+    }
+
+    public static int getRaceRounds() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return scanner.nextInt();
+    }
+}
