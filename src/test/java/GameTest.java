001cf1544105e2cb4a4f928b0db2f6b80bd58fca
dldb-chamchi <dladkfl49@gmail.com>
2025-02-15 19:52:31 +0900

    feat: 테스트 케이스 추가

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,7 +1,12 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 import java.util.*;
 
+@ExtendWith(MockitoExtension.class)
 class GameTest {
     @Test
     void 자동차_전진_테스트() {
@@ -26,6 +31,23 @@ class GameTest {
 
         List<String> winners = game.findWinners();
         assertEquals(Collections.singletonList("neo"), winners);
+    }
+
+    @Test
+    void 랜덤_전진_테스트() {
+        Random mockRandom = mock(Random.class);
+        when(mockRandom.nextInt(10)).thenReturn(4, 2, 7, 8, 3); // 순서대로 반환
+
+        RacingGame game = new RacingGame(mockRandom);
+        game.initializeGame(new String[]{"pobi", "crong"}, 5);
+
+        game.playRound(); // 1라운드 실행
+        assertEquals(1, game.getRacingCars().get(0).getForwardCount()); // pobi(4)
+        assertEquals(0, game.getRacingCars().get(1).getForwardCount()); // crong(2)
+
+        game.playRound();
+        assertEquals(2, game.getRacingCars().get(0).getForwardCount()); // pobi(7)
+        assertEquals(1, game.getRacingCars().get(1).getForwardCount()); // crong(8)
     }
 
 }
