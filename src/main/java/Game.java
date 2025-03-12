92c56ec17fec69e18dde14bf096da9f8a5f72cf3
jihwanyeom <yum020428@gmail.com>
2025-03-12 22:00:32 +0900

    * feat : 2단계 구현

---
+++ src/main/java/Game.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+
+    List<Car> carList = new ArrayList<Car>();
+    List<Car> winnerList = new ArrayList<Car>();
+
+    public void playGame(int carNum, int roundNum) {
+        initGame(carNum);
+        for (int i = 0; i < roundNum; i++) {
+            playRound();
+        }
+        makeWinnerList();
+    }
+
+    public void initGame(int carNum){
+        for (int i = 0 ; i < carNum ; i++)
+            carList.add(new Car("Car_"+i+""));
+    }
+
+    public void playRound() {
+        for (Car car : carList) {
+            car.move(getRandom());
+        }
+    }
+
+    public int getRandom(){
+        return (int)(Math.random()*10);
+    }
+
+    public void makeWinnerList() {
+        Car winner = carList.get(0);
+        int maxDistance = getMaxDistance();
+        for (Car car : carList) {
+            addWinner(car, maxDistance);
+        }
+    }
+
+    private void addWinner(Car car, int maxDistance) {
+        if (car.getDistance() == maxDistance) {
+            winnerList.add(car);
+        }
+    }
+
+    private int getMaxDistance() {
+
+        int maxDistance  = 0;
+        for (int i = 0; i < carList.size(); i++) {
+            maxDistance = Math.max(maxDistance, carList.get(i).getDistance());
+        }
+        return maxDistance;
+    }
+}
