b26021128182e627e155acea93710d7384206dcb
jihwanyeom <yum020428@gmail.com>
2025-03-12 21:59:41 +0900

    * feat : 1단계 구현

---
+++ src/main/java/Car.java
@@ -0,0 +1,24 @@
+public class Car {
+
+    private String name;
+    private int distance;
+
+    public Car(String name) {
+        this.name = name;
+        this.distance = 0;
+    }
+
+    public void move(int randomNumber) {
+        if (randomNumber >= 4) {
+            distance++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+}
