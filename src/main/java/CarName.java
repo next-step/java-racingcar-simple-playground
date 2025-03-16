fe0decf0d3f9907af5efd284bda653da7ecd7108
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-03-16 23:50:51 +0900

    [기능개선][1,2단계] 정적 팩토리 메소드 적용

--- src/main/java/CarName.java
+++ src/main/java/CarName.java
@@ -2,12 +2,16 @@ public class CarName {
 
   private final String name;
 
-  public CarName(String name) {
+  private CarName(String name) {
     validate(name);
     this.name = name;
   }
 
-  private void validate(String name) {
+  public static CarName of(String name) {
+    return new CarName(name);
+  }
+
+  private static void validate(String name) {
     if (name == null || name.isBlank() || name.length() > 5) {
       throw new IllegalArgumentException("이름은 1글자에서 5글자 사이여야 합니다.");
     }
