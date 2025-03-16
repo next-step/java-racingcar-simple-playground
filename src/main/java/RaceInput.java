0041a44b61447de20a8ffce06485ddbde7e700e1
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-03-16 23:49:16 +0900

    [기능개선][1,2단계] 검증 기능 Cars로 구분

--- src/main/java/RaceInput.java
+++ src/main/java/RaceInput.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class RaceInput {
@@ -8,35 +10,27 @@ public class RaceInput {
     this.scanner = scanner;
   }
 
-  public void inputCars(CarFactory carFactory) {
+  public Cars inputCars() {
     int carCount = getCarCount();
     scanner.nextLine();
+    List<String> carNames = new ArrayList<>();
     for (int i = 1; i <= carCount; i++) {
-      String name = getCarName(i);
-      carFactory.addCar(name);
+      carNames.add(getCarName(i));
     }
-
+    return new Cars(carNames);
   }
 
   public int getCarCount() {
     System.out.print("자동차 대수를 입력하세요 (2대 이상): ");
     int carCount = scanner.nextInt();
-
-    if (carCount < 2) {
-      throw new IllegalArgumentException("자동차 대수는 2 이상이어야 합니다.");
-    }
-
+    Cars.validateCarCount(carCount);
     return carCount;
   }
 
   public int getAttempts() {
     System.out.print("시도 횟수를 입력하세요 (1회 이상): ");
     int attempts = scanner.nextInt();
-
-    if (attempts < 1) {
-      throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
-    }
-
+    Cars.validateAttempts(attempts);
     return attempts;
   }
 
