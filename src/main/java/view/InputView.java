d1ec837bb3a56ae27445e6a91b1af97a093c6d76
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-03-19 14:54:12 +0900

    [기능추가][3,4단계] view 패키지 입력,출력 기능추가

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,26 @@
+package view;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+
+  private final Scanner scanner;
+
+  public InputView(Scanner scanner) {
+    this.scanner = scanner;
+  }
+
+  public List<String> inputCars() {
+    System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+    String carNameLine = scanner.nextLine();
+    return Arrays.asList(carNameLine.split(","));
+  }
+
+  public int getAttempts() {
+    System.out.println("시도할 회수는 몇회인가요?");
+    int attempts = scanner.nextInt();
+    return attempts;
+  }
+}
