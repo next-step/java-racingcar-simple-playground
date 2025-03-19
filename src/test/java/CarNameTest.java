b54a9a945dd9e56408335aa93c72ed770541d46d
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-03-19 14:43:39 +0900

    [기능개선][3,4단계] 도메인 패키지 추가

--- src/test/java/CarNameTest.java
+++ src/test/java/CarNameTest.java
@@ -1,6 +1,7 @@
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import domain.CarName;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
