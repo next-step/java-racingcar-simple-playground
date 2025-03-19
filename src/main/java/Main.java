4983359d818ea9a5b633defe1650ba6c3744be85
gjtjrl303 <gjtjrl303@naver.com>
2025-03-19 22:03:57 +0900

    feat: Main 구현

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import controller.GameController;
+import domain.CarManager;
+import domain.Game;
+import domain.RandomGenerator;
+
+public class Main {
+    public static void main(String[] args) {
+        CarManager carManager = new CarManager(new RandomGenerator());
+        Game game = new Game(carManager);
+        GameController gameController = new GameController(game);
+
+        gameController.createCars();
+        gameController.gameStart();
+        gameController.displayGameStatus();
+        gameController.findWinner();
+    }
+}
