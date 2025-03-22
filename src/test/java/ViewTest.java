40fd20949e5d84a4bbe4aaccec8a9d48dfc3b143
jihwanyeom <yum020428@gmail.com>
2025-03-22 16:23:54 +0900

    * test : View 테스트 코드 작성
    View 패키지의 OutputView클래스 중 Formatting 관련 메서드의 테스트 코드 작성

---
+++ src/test/java/ViewTest.java
@@ -0,0 +1,27 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import domain.Car;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import view.OutputView;
+
+public class ViewTest {
+
+    @Test
+    public void testFormatCarInfo() {
+        Car car = new Car("A", new MovableNumberGenerator());
+        car.move();
+        car.move();
+        String formattedInfo = OutputView.formatCarInfo(car);
+        assertThat(formattedInfo).isEqualTo("A : --");
+    }
+
+    @Test void testFormatWinnerNames() {
+        List<String> winners = Arrays.asList("A", "B", "C");
+        String formattedWinners = OutputView.formatWinnerNames(winners);
+
+        assertThat(formattedWinners).isEqualTo("A, B, C");
+    }
+
+}
