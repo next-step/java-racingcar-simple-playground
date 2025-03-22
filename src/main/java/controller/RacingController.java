294309880902f8b393935ab8e0c17f4bd916965b
jihwanyeom <yum020428@gmail.com>
2025-03-22 16:38:01 +0900

    * refactor : Controller 인스턴스 변수 최소화
    Controller에 불필요한 인스턴스 변수를
    줄이기 위해 지역 변수로 변환

--- src/main/java/controller/RacingController.java
+++ src/main/java/controller/RacingController.java
@@ -1,35 +1,45 @@
 package controller;
 
-import domain.Car;
 import domain.Cars;
 import domain.Racing;
 import domain.NumberGenerator;
+import java.util.List;
 import view.InputView;
 import view.OutputView;
 
 public class RacingController {
 
-    Racing racing;
-    Cars carList;
-    Cars winnerList;
-    int RoundNumber;
+    private static final int NAME_LENGTH_LIMIT = 5;
 
     public void run(NumberGenerator numberGenerator) {
-        carList = Cars.create(InputView.getCarNames(), numberGenerator);
-        RoundNumber = InputView.getRoundNumber();
-        racing = new Racing(carList);
+
+        OutputView.printInputCarNames();
+        List<String> carNames = InputView.getCarNames();
+        checkCarNameLengths(carNames);
+        Cars carList = Cars.create(carNames, numberGenerator);
+
+        OutputView.printInputRoundNumber();
+        int roundNumber = InputView.getRoundNumber();
+
+        Racing racing = new Racing(carList);
 
         OutputView.printResult();
-        playRace();
+        playRace(racing, carList, roundNumber);
 
-        winnerList = racing.findWinners();
+        Cars winnerList = racing.findWinners();
         OutputView.printWinners(winnerList.getCars());
     }
 
-    public void playRace() {
-        for (int i = 0; i < RoundNumber; i++) {
+    public void playRace(Racing racing, Cars carList, int roundNumber) {
+        for (int i = 0; i < roundNumber; i++) {
             racing.playRound();
             OutputView.printProcess(carList.getCars());
+        }
+    }
+
+    public void checkCarNameLengths(List<String> names) {
+        if (names.stream().anyMatch(name -> name.length() > NAME_LENGTH_LIMIT)) {
+            throw new IllegalArgumentException("이름의 길이는 " + NAME_LENGTH_LIMIT + "자 이하여야 합니다.");
         }
     }
 }
