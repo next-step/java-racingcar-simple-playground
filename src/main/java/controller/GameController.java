6b2a8484ea813e20d4d28c4e2b119d915bf64d42
gjtjrl303 <gjtjrl303@naver.com>
2025-03-23 12:56:57 +0900

    feat: 차량 입력 미제공 및 음의 턴 수에 대한 검증 추가

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -18,20 +18,24 @@ public class GameController {
     public void createCars() {
         String carNamesList = gameView.getCarNamesView();
         String[] carNames = carNamesList.split(",");
+        if (carNames.length == 0) {
+            throw new IllegalStateException("차량을 입력하세요");
+        }
+
         for (String carName : carNames) {
-            Car car = new Car(carName);
-            game.addCar(car);
+            game.addCar(Car.createCar(carName));
         }
     }
 
     public void gameStart() {
         int turnCount = gameView.getTurnCountView();
-        game.gameStart(turnCount);
-    }
-
-    public void displayGameStatus() {
-        List<List<Car>> gameHistory = game.getGameHistory();
-            gameView.displayGameStatus(gameHistory);
+        if (turnCount <= 0) {
+            throw new IllegalArgumentException("턴의 수는 1이상이어야 합니다.");
+        }
+        for (int i = 0; i <turnCount; i++) {
+            game.turn();
+            gameView.displayGameStatus(game.findAllCar());
+        }
     }
 
     public void findWinner() {
