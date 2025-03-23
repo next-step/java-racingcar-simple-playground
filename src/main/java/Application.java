21769392b3b3cb47fa1bbc940af316c5172ea2fa
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-03-23 15:54:45 +0900

    [문서][3,4단게] Application에 클래스 변경 적용

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,18 +1,18 @@
-import controller.CarRunner;
+import controller.RacingGame;
 import domain.NumberGenerator;
 import domain.RandomNumberGenerator;
 import java.util.Scanner;
 import view.InputView;
-import view.OutputView;
+import view.ResultView;
 
 public class Application {
 
   public static void main(String[] args) {
     InputView inputView = new InputView(new Scanner(System.in));
-    OutputView outputView = new OutputView();
+    ResultView resultView = new ResultView();
     NumberGenerator numberGenerator = new RandomNumberGenerator();
-    CarRunner carRunner = new CarRunner(inputView, outputView, numberGenerator);
-    carRunner.run();
+    RacingGame racingGame = new RacingGame(inputView, resultView, numberGenerator);
+    racingGame.run();
   }
 }
 
