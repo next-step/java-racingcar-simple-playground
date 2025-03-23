0c0ba234beac3f26ed95d2ec9952ee807615d17b
jihwanyeom <yum020428@gmail.com>
2025-03-23 14:29:58 +0900

    style : 실행파일 불필요한 개행 제거

--- src/main/java/RacingApplication.java
+++ src/main/java/RacingApplication.java
@@ -4,9 +4,7 @@ import domain.RandomNumberGenerator;
 public class RacingApplication {
 
     public static void main(String[] args) {
-
         new RacingController().run(new RandomNumberGenerator());
-
     }
 
 }
