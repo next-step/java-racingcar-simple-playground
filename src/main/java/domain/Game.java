3d63027f0be032d54fdad8ce759976e8aaa4bc05
gjtjrl303 <gjtjrl303@naver.com>
2025-03-23 12:54:55 +0900

    refactor: 실시간 정보를 출력하기 위해서 차량의 gameStart()를 gameController가 제어하도록 변경

--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -2,17 +2,16 @@ package domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Game {
 
     private final WinnerFinder winnerFinder;
     private final CarManager carManager;
-    private final List<List<Car>> gameHistory;
 
     public Game(CarManager carManager) {
         this.carManager = carManager;
         winnerFinder = new WinnerFinder();
-        gameHistory = new ArrayList<>();
     }
 
     public void addCar(Car car) {
@@ -24,31 +23,8 @@ public class Game {
         return winnerFinder.findWinner(cars);
     }
 
-    public void gameStart(int turnCount) {
-        if (turnCount <= 0) {
-            throw new IllegalArgumentException("턴의 수는 1이상이어야 합니다.");
-        }
-        for (int i = 0; i <turnCount; i++) {
-            turn();
-        }
-    }
-
     public void turn() {
         carManager.moveIfCan();
-        addCarHistoryToGameHistory();
-    }
-
-    private void addCarHistoryToGameHistory() {
-        List<Car> carsHistory = new ArrayList<>();
-        List<Car> cars = findAllCar();
-        for (Car car : cars) {
-            carsHistory.add(new Car(car));
-        }
-        gameHistory.add(carsHistory);
-    }
-
-    public List<List<Car>> getGameHistory() {
-        return gameHistory;
     }
 
     public List<Car> findAllCar() {
