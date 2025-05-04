f29d936bfc9809cd135f45b2ad6d801529bf174b
SanghyunLee <lgw9736@naver.com>
2025-05-04 14:08:54 +0900

    feat: Car 움직이는 메소드 추가

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -2,8 +2,24 @@ package domain;
 
 public class Car {
     private String name;
+    private int distance = 0;
+    private final int MOVE_STANDARD_NUMBER = 4;
 
     public Car(String name){
         this.name = name;
+    }
+
+    public void tryMoveByNumber(int number){
+        if(number >= MOVE_STANDARD_NUMBER){
+            move();
+        }
+    }
+
+    private void move(){
+        distance+=1;
+    }
+
+    public int getDistance(){
+        return distance;
     }
 }
