63e1cf13335778df3b260a3efefe490bbdc049c3
dd-jiyun <dlwldbs8015@gmail.com>
2025-05-06 00:14:26 +0900

    feat: 자동차 생성

---
+++ src/main/java/Car.java
@@ -0,0 +1,11 @@
+public class Car {
+    private final String name;
+
+    public Car(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
