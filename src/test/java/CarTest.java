5f505ad7552f5f49f8a3c8240345c4f6ae829302
dd-jiyun <dlwldbs8015@gmail.com>
2025-05-06 00:44:04 +0900

    test: 자동차 이동 테스트

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -15,4 +15,31 @@ class CarTest {
         assertThat(car.getName()).isEqualTo("jiyun");
     }
 
+    @Test
+    @DisplayName("자동차는 랜덤 값이 4 이상일 경우 전진할 수 있다.")
+    void shouldMoveForward_whenMovableRandomValue() {
+        // given
+        Car car = new Car("jiyun");
+
+        // when
+        int randomValue = 4;
+        car.canMove(randomValue);
+
+        // then
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("자동차는 랜덤 값이 3 이하일 경우 멈춘다.")
+    void shouldStop_whenImmovableRandomValue() {
+        // given
+        Car car = new Car("jiyun");
+
+        // when
+        int randomValue = 3;
+        car.canMove(randomValue);
+
+        // then
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
 }
