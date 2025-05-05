6e4819f4b4e91fd1add02b3e3ead3ee54d8dc080
SanghyunLee <lgw9736@naver.com>
2025-05-05 15:49:46 +0900

    test: 자동차 이름 테스트 추가

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -1,8 +1,12 @@
 package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class CarTest {
@@ -33,4 +37,25 @@ public class CarTest {
         // Then
         assertThat(car.getDistance()).isEqualTo(0);
     }
+
+    @Nested
+    @DisplayName("자동차 생성 - 이름 테스트")
+    class InvalidCarNameTest {
+        @Test
+        @DisplayName("자동차 생성 이름 테스트: 예외")
+        void invalidCarNameTest() {
+            assertAll(
+                () -> assertThatThrownBy(() -> new Car("aaaaaa")).isInstanceOf(IllegalArgumentException.class),
+                () -> assertThatThrownBy(() -> new Car("  ")).isInstanceOf(IllegalArgumentException.class),
+                () -> assertThatThrownBy(() -> new Car("")).isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+
+        @Test
+        @DisplayName("자동차 생성 이름 테스트: 정상")
+        void validCarName() {
+            assertDoesNotThrow(()->new Car("aaa"));
+        }
+    }
+
 }
