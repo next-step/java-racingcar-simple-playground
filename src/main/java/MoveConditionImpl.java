eaf945a81aa2f4418c6b5b08d7c9565b973e0d52
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-05-07 08:18:44 +0900

    Feat: MoveConditionImpl 클래스 구현

---
+++ src/main/java/MoveConditionImpl.java
@@ -0,0 +1,8 @@
+public class MoveConditionImpl implements MoveCondition{
+    private static final int MOVE_THRESHOLD = 4;
+
+    @Override
+    public boolean isMovable(int value) {
+        return value >= MOVE_THRESHOLD;
+    }
+}
