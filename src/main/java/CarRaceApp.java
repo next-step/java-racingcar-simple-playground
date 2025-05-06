e2ea68bc3cc6573d555de0ae23153e9ce539bab4
SanghyunLee <lgw9736@naver.com>
2025-05-06 18:30:18 +0900

    feat: CarRaceApp 생성

---
+++ src/main/java/CarRaceApp.java
@@ -0,0 +1,32 @@
+import domain.Car;
+import domain.CarNameParser;
+import domain.CarRace;
+import domain.NumberGenerator;
+import domain.NumberGeneratorImpl.RandomNumberGenerator;
+import java.util.List;
+import view.InputView;
+import view.OutputView;
+
+public class CarRaceApp {
+    public static void main(String[] args) {
+        final InputView inputView = new InputView();
+        final OutputView outputView = new OutputView();
+        final CarNameParser carNameParser = new CarNameParser();
+        final NumberGenerator numberGenerator = new RandomNumberGenerator();
+
+        outputView.printInputCarsName();
+        String carNames = inputView.readCarNames();
+        List<Car> cars = carNameParser.parse(carNames).stream().map(Car::new).toList();
+
+        outputView.printInputGameRounds();
+        int gameRounds = inputView.readGameRounds();
+
+        final CarRace carRace = new CarRace(cars, gameRounds, numberGenerator);
+        carRace.start();
+
+        // TODO: 실행 결과 출력
+
+        // TODO: 우승자 출력
+
+    }
+}
