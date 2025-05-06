eaecd529ac301412927539ec1216e93774e74f62
SanghyunLee <lgw9736@naver.com>
2025-05-06 18:28:10 +0900

    feat: CarNameParser 객체 생성
    - , 를 기준으로 이름 나누는 기능

---
+++ src/main/java/domain/CarNameParser.java
@@ -0,0 +1,14 @@
+package domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CarNameParser {
+    private final String DELIMITER=",";
+
+    public List<String> parse(String carNames) {
+        return Arrays.stream(carNames.trim().split(DELIMITER))
+            .map(String::trim)
+            .toList();
+    }
+}
