b907bed0245ae3f6269e441558b0dce3fa544809
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-05-07 18:09:27 +0900

    feat: CarWinner 객체 구현

---
+++ src/main/java/CarWinner.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+
+public class CarWinner {
+    private Car[] cars; //참가한 자동차들
+    private int winnerPosition; //우승 자동차의 위치(최대값)
+    private int winnerCnt; //우승 자동차의 수
+    private ArrayList<String> winnerNames; //우승 자동차의 이름
+
+    public CarWinner(Car[] cars) {
+        this.cars = cars;
+        winnerCnt = 0;
+        winnerPosition = Integer.MIN_VALUE;
+        winnerNames = new ArrayList<>();
+    }
+
+    public int getWinnerCnt() {
+        return winnerCnt;
+    }
+
+    public int getWinnerPosition() {
+        return winnerPosition;
+    }
+
+    public ArrayList<String> getWinnerName() {
+        return winnerNames;
+    }
+
+    public void whichWinner(int testRounds){
+        //1. 모든 자동차를 주어진 횟수만큼 이동
+        moveAllCars(testRounds);
+        //2. 우승자의 수와 자동차의 위치(최대값) 계산
+        countWinners();
+        //3. 우승자 이름 저장
+        saveWinnerName();
+    }
+
+    //1. 모든 자동차를 주어진 횟수만큼 이동
+    private void moveAllCars(int testRounds){
+        for (Car car : cars) {
+            for (int i = 0; i < testRounds; i++) {
+                car.move(RandomUtil.randomGenerator());
+            }
+        }
+    }
+
+    //2. 우승자의 수와 자동차의 위치(최대값) 계산
+    private void countWinners(){
+        for (Car car : cars) {
+            updateWinnerCount(car);
+        }
+    }
+
+    //2-1. 자동차 한 대씩 우승자의 수와 최고 위치 갱신
+    private void updateWinnerCount(Car car) {
+        int position = car.getPosition();
+        if (position > winnerPosition) {
+            winnerCnt = 1;
+            winnerPosition = position;
+            return;
+        }
+        if (position == winnerPosition) {
+            winnerCnt++;
+        }
+    }
+
+    //3. 우승자 이름 저장
+    private void saveWinnerName(){
+        for (Car car : cars) {
+            if (car.getPosition() == winnerPosition) {
+                winnerNames.add(car.getName());
+            }
+        }
+    }
+}
+
+
