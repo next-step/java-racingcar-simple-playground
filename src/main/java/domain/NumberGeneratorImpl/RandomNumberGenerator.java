56c62d385576bfa4dc20f45ecf8e68199e412452
SanghyunLee <lgw9736@naver.com>
2025-05-07 15:41:35 +0900

    fix: 랜덤값 생성 코드 수정

--- src/main/java/domain/NumberGeneratorImpl/RandomNumberGenerator.java
+++ src/main/java/domain/NumberGeneratorImpl/RandomNumberGenerator.java
@@ -1,13 +1,15 @@
 package domain.NumberGeneratorImpl;
 
 import domain.NumberGenerator;
+import java.util.Random;
 
 public class RandomNumberGenerator implements NumberGenerator {
     private final int MAX_RANDOM_NUMBER = 10;
     private final int MIN_RANDOM_NUMBER = 0;
+    private static final Random random = new Random();
 
     @Override
-    public int generate(){
-        return (int)(Math.random()*(MAX_RANDOM_NUMBER - MIN_RANDOM_NUMBER) + MIN_RANDOM_NUMBER);
+    public int generate() {
+        return random.nextInt(MAX_RANDOM_NUMBER - MIN_RANDOM_NUMBER) + MIN_RANDOM_NUMBER;
     }
 }
