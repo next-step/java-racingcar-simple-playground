d2bdcf8ffa4b344dc5e3d326a9503e89581bbf5b
dd-jiyun <dlwldbs8015@gmail.com>
2025-05-08 14:12:37 +0900

    feat: 자동차의 움직임 제어와 우승자 반환 객체 생성
    - 자동차들에게 시도 횟수만큼의 움직임을 명령합니다.
    - 자동차들 중 우승자를 반환합니다.

--- src/main/java/model/Cars.java
+++ src/main/java/model/Cars.java
@@ -3,6 +3,7 @@ package model;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import strategy.MoveStrategy;
 
 public class Cars {
     private static final String CAR_NAME_DELIMITER = ",";
@@ -13,13 +14,38 @@ public class Cars {
         this.cars = parseToCars(names);
     }
 
-    public List<Car> getCars() {
-        return Collections.unmodifiableList(cars);
-    }
-
     private List<Car> parseToCars(final String names) {
         return Arrays.stream(names.split(CAR_NAME_DELIMITER))
                 .map(name -> new Car(name.trim()))
                 .toList();
+    }
+
+    public List<Car> getCars() {
+        return Collections.unmodifiableList(cars);
+    }
+
+    public List<Car> findWinners() {
+        return cars.stream()
+                .filter(car -> car.getPosition() == findMaxPosition())
+                .toList();
+    }
+
+    private int findMaxPosition() {
+        return cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+    }
+
+    public List<Integer> getAllPositions() {
+        return cars.stream()
+                .map(Car::getPosition)
+                .toList();
+    }
+
+    public void move(final MoveStrategy moveStrategy) {
+        for (Car car : cars) {
+            car.moveForward(moveStrategy);
+        }
     }
 }
--- src/test/java/model/CarsTest.java
+++ src/test/java/model/CarsTest.java
@@ -2,24 +2,54 @@ package model;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
-import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import strategy.AlwaysTrueMoveStrategy;
 
 class CarsTest {
+    private Cars cars;
+
+    @BeforeEach
+    void setUp() {
+        cars = new Cars("pobi,jiyun,juno");
+    }
 
     @Test
     @DisplayName("각 자동차들의 이름을 전달받아 정상적으로 자동차 객체가 생성된다.")
     void shouldCreateCar_whenInputCarsName() {
+        // given & when & then
+        assertThat(cars.getCars())
+                .extracting(Car::getName)
+                .containsExactly("pobi", "jiyun", "juno");
+    }
+
+    @Test
+    @DisplayName("각 자동차에게 정상적으로 움직임 명령을 내린다.")
+    void shouldMoveOrderToCar() {
         // given
-        String carNames = "pobi,jiyun,juno";
-        Cars cars = new Cars(carNames);
+        AlwaysTrueMoveStrategy alwaysTrueMoveStrategy = new AlwaysTrueMoveStrategy();
 
         // when
-        List<Car> carList = cars.getCars();
+        cars.move(alwaysTrueMoveStrategy);
 
         // then
-        assertThat(carList)
+        assertThat(cars.getCars())
+                .extracting(Car::getPosition)
+                .containsExactly(1, 1, 1);
+    }
+
+    @Test
+    @DisplayName("자동차들 중 우승자를 정상적으로 반환한다.")
+    void shouldReturnWinnersOfCars() {
+        // given
+        AlwaysTrueMoveStrategy alwaysTrueMoveStrategy = new AlwaysTrueMoveStrategy();
+
+        // when
+        cars.move(alwaysTrueMoveStrategy);
+
+        // then
+        assertThat(cars.findWinners())
                 .extracting(Car::getName)
                 .containsExactly("pobi", "jiyun", "juno");
     }
