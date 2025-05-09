460375e5eeff90e5145099154cc876345419426c
SanghyunLee <lgw9736@naver.com>
2025-05-09 13:28:17 +0900

    fix: 상수 네이밍 컨벤션 적용

--- src/main/java/domain/NumberGeneratorImpl/RandomNumberGenerator.java
+++ src/main/java/domain/NumberGeneratorImpl/RandomNumberGenerator.java
@@ -5,8 +5,8 @@ import java.util.Random;
 
 public class RandomNumberGenerator implements NumberGenerator {
     private static final Random random = new Random();
-    private final int MAX_RANDOM_NUMBER = 10;
-    private final int MIN_RANDOM_NUMBER = 0;
+    private static final int MAX_RANDOM_NUMBER = 10;
+    private static final int MIN_RANDOM_NUMBER = 0;
 
     @Override
     public int generate() {
