47b82b3ddcb7ff4f6c62524ac4e1fc4c674bdfe1
dd-jiyun <dlwldbs8015@gmail.com>
2025-05-10 22:56:39 +0900

    feat: I/O view 생성

---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,28 @@
+package view;
+
+import java.util.List;
+import model.Car;
+import model.Cars;
+
+public class OutputView {
+
+    private final static String RESULT_OF_EXECUTION_HEADER = "실행 결과";
+
+    private OutputView() {
+    }
+
+    public static void printResultHeader() {
+        System.out.println(RESULT_OF_EXECUTION_HEADER);
+    }
+
+    public static void printCarStatus(Cars cars) {
+        for (Car car : cars.getCars()) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+        }
+        System.out.println();
+    }
+
+    public static void printWinners(List<String> winners) {
+        System.out.println(String.join(", ", winners) + "가 최종 우승했습니다.");
+    }
+}
