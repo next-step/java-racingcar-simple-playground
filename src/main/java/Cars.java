37eb85bbf41f2ea8618e515096d808e36988a97c
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-05-10 15:39:35 +0900

    refactor: 우승자 판단 책임을 분리하여 Cars는 이동만 담당하도록 수정

--- src/main/java/Cars.java
+++ src/main/java/Cars.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Cars {
@@ -18,32 +17,8 @@ public class Cars {
         }
     }
 
-    //이동된 자동차들의 최대위치 구하기
-    private int getMaxPosition() {
-        return cars.stream()
-                .mapToInt(Car::getPosition)
-                .max()
-                .orElse(0);
-    }
-
-    //우승 자동차 목록
-    public List<Car> getWinners() {
-        int max = getMaxPosition();
-        return cars.stream()
-                .filter(car -> car.getPosition() == max)
-                .collect(Collectors.toList());
-    }
-
-    //우승 자동차 이름 목록
-    public List<String> getWinnerNames() {
-        return getWinners().stream()
-                .map(Car::getName)
-                .collect(Collectors.toList());
-    }
-
-    //우승 자동차의 수
-    public int getWinnerCount() {
-        return getWinners().size();
+    public List<Car> getCars() {
+        return cars;
     }
 
 }
