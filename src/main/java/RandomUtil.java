7bbbd9754331dd7136aa9198e751ceaf045a70fd
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-05-10 16:24:50 +0900

    refactor: RandomUtil에 시드 고정하여 일정한 랜덤 값 반환

--- src/main/java/RandomUtil.java
+++ src/main/java/RandomUtil.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 public class RandomUtil {
     private static final int RANDOM_LIMIT = 10;
-    private static final Random random = new Random();
+    private static final Random random = new Random(42); //시드 고정
 
     public static int randomGenerator(){
         //0 <= value <= 9
