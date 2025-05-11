5511db180b75fe110c9acc2cded4cc78820ae82c
dd-jiyun <dlwldbs8015@gmail.com>
2025-05-11 17:02:20 +0900

    refactor: 한 명의 우승자 반환 테스트 추가

--- src/test/java/model/RaceManagerTest.java
+++ src/test/java/model/RaceManagerTest.java
@@ -11,6 +11,24 @@ class RaceManagerTest {
 
     @Test
     @DisplayName("전달된 시도 횟수만큼 자동차가 정상적으로 움직인다.")
+    void shouldMoveCar_whenInputTryCount() {
+        // given
+        int tryCount = 3;
+        List<String> carNames = List.of("jiyun");
+        RaceManager raceManager = new RaceManager(carNames, tryCount);
+        AlwaysTrueMoveStrategy alwaysTrueMoveStrategy = new AlwaysTrueMoveStrategy();
+
+        // when
+        raceManager.moveOnce(alwaysTrueMoveStrategy);
+
+        // then
+        assertThat(raceManager.getRaceCars().getCars())
+                .extracting(Car::getPosition)
+                .containsExactly(1);
+    }
+
+    @Test
+    @DisplayName("전달된 시도 횟수만큼 자동차가 정상적으로 움직인다.")
     void shouldMoveCars_whenInputTryCount() {
         // given
         int tryCount = 3;
@@ -28,8 +46,25 @@ class RaceManagerTest {
     }
 
     @Test
-    @DisplayName("우승자를 정상적으로 반환한다.")
-    void shouldReturnWinnerNames() {
+    @DisplayName("한 명의 우승자을 정상적으로 반환한다.")
+    void shouldReturnSingleWinnerName() {
+        // given
+        int tryCount = 1;
+        List<String> carNames = List.of("pobi");
+        RaceManager raceManager = new RaceManager(carNames, tryCount);
+        AlwaysTrueMoveStrategy moveStrategy = new AlwaysTrueMoveStrategy();
+
+        // when
+        raceManager.moveOnce(moveStrategy);
+
+        // then
+        assertThat(raceManager.findWinnerNames())
+                .containsExactly("pobi");
+    }
+
+    @Test
+    @DisplayName("우승자들을 정상적으로 반환한다.")
+    void shouldReturnMultiWinnerNames() {
         // given
         int tryCount = 1;
         List<String> carNames = List.of("pobi", "jiyun");
