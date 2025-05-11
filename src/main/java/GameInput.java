c4942ff4a0ed66c38aac58af8e05491a94acdd82
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-05-11 09:19:29 +0900

    refactor: trim() 대신 strip()으로 공백 제거 방식 개선

--- src/main/java/GameInput.java
+++ src/main/java/GameInput.java
@@ -11,7 +11,7 @@ public class GameInput {
     public List<String> readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         return Arrays.stream(scanner.nextLine().split(","))
-                .map(String::trim)
+                .map(String::strip)
                 .collect(Collectors.toList());
     }
 
