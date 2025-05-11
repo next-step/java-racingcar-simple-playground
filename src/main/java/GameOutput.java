d301b08d9bfaedad4d226a86083a5f8f3507bce0
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-05-11 09:29:02 +0900

    refactor: 출력 메시지 생성 시 StringBuilder 사용

--- src/main/java/GameOutput.java
+++ src/main/java/GameOutput.java
@@ -7,12 +7,19 @@ public class GameOutput {
     }
     public static void printRound(Cars cars) {
         for (Car car : cars.getCars()) {
-            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+            StringBuilder sb = new StringBuilder();
+            sb.append(car.getName())
+                    .append(" : ")
+                    .append("-".repeat(car.getPosition()));
+            System.out.println(sb);
         }
         System.out.println();
     }
 
     public static void printWinners(List<String> winners) {
-        System.out.println(String.join(", ", winners) + "가 최종 우승했습니다.");
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.join(", ", winners))
+                .append("가 최종 우승했습니다.");
+        System.out.println(sb);
     }
 }
