397857e28e1549a9ece992dffff6aa4781e15449
wosyh18 <wosyh18@naver.com>
2025-05-12 20:24:55 +0900

    feat : Car 테스트 코드 구현

---
+++ src/test/java/CarTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+class CarTest {
+    @Test
+    @DisplayName("자동차 움직이기")
+    void moveCarTest() {
+        Car a = new Car("a");
+        assertThat(a.moveCar()).isGreaterThanOrEqualTo(0);
+    }
+
+}
