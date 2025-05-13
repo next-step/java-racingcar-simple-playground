94878ab8b86cb1b1ed1d45608a3bc55f143a69a6
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-05-13 22:15:16 +0900

    test: tryCount가 0 이하일 경우 테스트 추가

---
+++ src/test/java/RacingGameTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class RacingGameTest {
+    @Test
+    @DisplayName("시도 횟수가 0 이하일 경우 예외를 던진다")
+    void WhenTryCountIsNonPositiveThrowException() {
+        List<String> carNames = List.of("A", "B", "C");
+
+        assertAll(
+                () -> assertThrows(IllegalArgumentException.class, () -> {
+                    new RacingGame(carNames, 0); // 0일 때
+                }),
+                () -> assertThrows(IllegalArgumentException.class, () -> {
+                    new RacingGame(carNames, -1); // 음수일 때
+                })
+        );
+    }
+
+}
