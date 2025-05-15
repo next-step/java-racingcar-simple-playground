1e336812aa38b4540259af7e575d6303ca0cc8c4
wosyh18 <wosyh18@naver.com>
2025-05-15 13:43:27 +0900

    chore : 객체 전달 부분 수정, 메서드 이름 변경

--- src/main/java/RacingCars.java
+++ src/main/java/RacingCars.java
@@ -2,27 +2,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Random;
 
-public class RacingCar {
+public class RacingCars {
 
     private Car[] cars;
 
-    public RacingCar(Car[] cars) {
+    public RacingCars(Car[] cars) {
         this.cars = cars;
     }
 
-    public RacingCar(String[] names, IClass iclass) {
-        this.cars = new Car[names.length]; //사용자가 준 car 이름 배열 크기만큼 객체 배열 생성
-
-        //cars라는 객체 배열에 사용자가 준 이름과 랜덤 객체 넘겨주기
-        for (int i = 0; i < names.length; i++) {
-            cars[i] = new Car(names[i], iclass);
-        }
-    }
-
     //cars 배열 내의 객체들 움직이기
-    public void moveCar() {
+    public void moveCarList() {
         for (Car car : cars) {
             car.moveCar();
         }
@@ -31,7 +21,7 @@ public class RacingCar {
     //주어진 횟수만큼 차 움직이기
     public void playRound(int roundcnt) {
         for (int i = 0; i < roundcnt; i++) {
-            moveCar();
+            moveCarList();
         }
     }
 
