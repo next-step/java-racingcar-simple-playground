4b7220416944105fe5a9cf991bbe799d7cbafead
wosyh18 <wosyh18@naver.com>
2025-05-15 13:44:27 +0900

    refact : 불필요 생성자 삭제

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -2,18 +2,12 @@ import java.util.Random;
 
 public class Car {
 
-    //속성 : 이름, 위치 ,.랜덤 객체
+    //속성 : 이름, 위치 , 랜덤객체 주입
     //메서드 : 이름 설정 생성자, 위치 설정 메서드
     public String name;
     public int location = 0;
     private IClass iclass;
 
-    //사용자 입장
-    public Car(String name) {
-        this.name = name;
-    }
-
-    //테스트 용이성을 위해 오버라이딩 (random 고정을 위해서)
     public Car(String name, IClass iclass) {
         this.name = name;
         this.iclass = iclass;
