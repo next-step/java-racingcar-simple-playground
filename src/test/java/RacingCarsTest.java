43903405468edb05d33d26e6c4801e4e64c052a3
wosyh18 <wosyh18@naver.com>
2025-05-15 13:43:58 +0900

    test : 객체 전달 부분 수정

--- src/test/java/RacingCarsTest.java
+++ src/test/java/RacingCarsTest.java
@@ -1,17 +1,19 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Random;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
-class RacingCarTest {
+class RacingCarsTest {
 
     @Test
     @DisplayName("다같이 1등일 때")
     void AllWinnerTest() {
-        String[] names = {"1등", "2등", "3등"};
-        RacingCar game = new RacingCar(names, new FixedInt(5));
+        Car[] cars = new Car[]{
+            new Car("1등", new FixedInt(5)),
+            new Car("2등", new FixedInt(5)),
+            new Car("3등", new FixedInt(5))
+        };
+        RacingCars game = new RacingCars(cars);
 
         game.playRound(4);
 
@@ -26,7 +28,7 @@ class RacingCarTest {
             new Car("2등", new FixedInt(3))
         };
         //1등 -> 5 이동성공, 2등 -> 3 이동 실패
-        RacingCar game = new RacingCar(cars);
+        RacingCars game = new RacingCars(cars);
 
         game.playRound(4);
 
