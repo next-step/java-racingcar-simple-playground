62d6dca4b419621e7198722ca2f1d9a72afc07f3
wosyh18 <wosyh18@naver.com>
2025-05-15 13:46:19 +0900

    chore : 줄 정리

--- src/main/java/FixedInt.java
+++ src/main/java/FixedInt.java
@@ -1,9 +1,11 @@
 public class FixedInt implements IClass {
+
     private int value;
 
     public FixedInt(int value) {
-        this.value=value;
+        this.value = value;
     }
+
     @Override
     public int giveInt() {
         return value;
--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -10,20 +10,21 @@ class CarTest {
     @Test
     @DisplayName("자동차 움직이기")
     void moveCarTest() {
-        Car a = new Car("a",new RandInt());
+        Car a = new Car("a", new RandInt());
         assertThat(a.moveCar()).isGreaterThanOrEqualTo(0);
     }
+
     @Test
     @DisplayName("자동차 전진")
     void goCarTest() {
-        Car b = new Car("b",new FixedInt(6));
+        Car b = new Car("b", new FixedInt(6));
         assertThat(b.moveCar()).isEqualTo(1);
     }
 
     @Test
     @DisplayName("자동차 정지")
     void stopCarTest() {
-        Car b = new Car("b",new FixedInt(2));
+        Car b = new Car("b", new FixedInt(2));
         assertThat(b.moveCar()).isEqualTo(0);
     }
 
