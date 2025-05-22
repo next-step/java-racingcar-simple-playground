c077761a2a5c8738b3e3e5e06582e8e4b4100c96
wosyh18 <wosyh18@naver.com>
2025-05-22 10:27:26 +0900

    fix : 배열에서 리스트로 수정

--- src/test/java/RacingCarsTest.java
+++ src/test/java/RacingCarsTest.java
@@ -1,6 +1,9 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RacingCarsTest {
@@ -8,11 +11,10 @@ class RacingCarsTest {
     @Test
     @DisplayName("다같이 1등일 때")
     void AllWinnerTest() {
-        Car[] cars = new Car[]{
-            new Car("1등", new FixedInt(5)),
-            new Car("2등", new FixedInt(5)),
-            new Car("3등", new FixedInt(5))
-        };
+        List<Car> cars = new ArrayList<>(
+                List.of(new Car("1등", new FixedInt(5))
+                        , new Car("2등", new FixedInt(3))
+                        , new Car("3등", new FixedInt(5))));
         RacingCars game = new RacingCars(cars);
 
         game.playRound(4);
@@ -23,15 +25,14 @@ class RacingCarsTest {
     @Test
     @DisplayName("1명만 우승일 때")
     void WinnerOnlyOne() {
-        Car[] cars = new Car[]{
-            new Car("1등", new FixedInt(5)),
-            new Car("2등", new FixedInt(3))
-        };
+        List<Car> cars = new ArrayList<>(
+                List.of(new Car("1등", new FixedInt(5))
+                        , new Car("2등", new FixedInt(3))));
         //1등 -> 5 이동성공, 2등 -> 3 이동 실패
         RacingCars game = new RacingCars(cars);
 
         assertThat(game.getWinner())
-            .map(it -> it.name)
-            .containsExactlyInAnyOrder("1등");
+                .map(it -> it.name)
+                .containsExactlyInAnyOrder("1등");
     }
 }
