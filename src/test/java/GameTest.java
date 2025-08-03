04700346ff64b5f53d00c35fe0b47e5c397fdcc0
dldb-chamchi <dladkfl49@gmail.com>
2025-08-03 23:31:30 +0900

    feat: 레이싱 게임 테스트 생성

---
+++ src/test/java/GameTest.java
@@ -0,0 +1,56 @@
+import model.RacingGame;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import java.util.*;
+
+@ExtendWith(MockitoExtension.class)
+class GameTest {
+
+    @Test
+    void pickWinnerTest() {
+        RacingGame game = new RacingGame(new Random());
+        game.initializeGame(new String[]{"neo", "brie"}, 5);
+
+        game.getRacingCars().get(0).move(5);
+        game.getRacingCars().get(1).move(3);
+
+        List<String> winners = game.findWinners();
+        assertEquals(Collections.singletonList("neo"), winners);
+    }
+
+    @Test
+    void pickMoreThanOneWinnerTest() {
+        RacingGame game = new RacingGame(new Random());
+        game.initializeGame(new String[]{"neo", "brie"}, 5);
+
+        game.getRacingCars().get(0).move(5);
+        game.getRacingCars().get(1).move(5);
+
+        List<String> winners = game.findWinners();
+        assertEquals(
+                List.of("neo", "brie"),
+                winners
+        );
+    }
+
+    @Test
+    void randomMoveTest() {
+        Random mockRandom = mock(Random.class);
+        when(mockRandom.nextInt(10)).thenReturn(4, 2, 7, 8, 3);
+
+        RacingGame game = new RacingGame(mockRandom);
+        game.initializeGame(new String[]{"pobi", "crong"}, 5);
+
+        game.playRound(); // 1라운드 실행
+        assertEquals(1, game.getRacingCars().get(0).getForwardCount());
+        assertEquals(0, game.getRacingCars().get(1).getForwardCount());
+
+        game.playRound();
+        assertEquals(2, game.getRacingCars().get(0).getForwardCount());
+        assertEquals(1, game.getRacingCars().get(1).getForwardCount());
+    }
+}
