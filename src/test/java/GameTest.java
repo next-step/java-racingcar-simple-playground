89d8701b809a47e847b5878802e4da8af54c5be7
dldb-chamchi <dladkfl49@gmail.com>
2025-08-04 14:58:32 +0900

    feat: 테스트 케이스 추가
    # Conflicts:
    #	src/test/java/GameTest.java

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,11 +1,14 @@
-
+import model.RacingCar;
+import model.RacingGame;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @ExtendWith(MockitoExtension.class)
 class GameTest {
@@ -23,8 +26,16 @@ class GameTest {
     }
 
     @Test
-    void pickMoreThanOneWinnerTest() {
+    void pickWinner2Test() {
+        RacingGame game = new RacingGame(new Random());
+        game.initializeGame(new String[]{"neo", "brie"}, 5);
 
+        List<String> winners = game.findWinners();
+        assertEquals(List.of("neo", "brie"), winners);
+    }
+
+    @Test
+    void pickMoreThanOneWinnerTest() {
         RacingGame game = new RacingGame(new Random());
         game.initializeGame(new String[]{"neo", "brie"}, 5);
 
@@ -53,5 +64,29 @@ class GameTest {
         game.playRound();
         assertEquals(2, game.getRacingCars().get(0).getForwardCount());
         assertEquals(1, game.getRacingCars().get(1).getForwardCount());
+    }
+
+    @Test
+    void createCarsTest() {
+        String[] names = {"pobi", "cron", "bob"};
+
+        RacingGame game = new RacingGame(new Random());
+        game.initializeGame(names, 1);
+
+        List<RacingCar> cars = game.getRacingCars();
+
+        assertEquals(3, cars.size());
+
+        List<String> actualNames = cars.stream()
+                .map(RacingCar::getCarName)
+                .collect(Collectors.toList());
+        assertEquals(List.of("pobi", "cron", "bob"), actualNames);
+    }
+
+    @Test
+    void duplicateCarNameTest() {
+        RacingGame game = new RacingGame(new Random());
+        game.initializeGame(new String[]{"pobi", "pobi"}, 1);
+        assertThat(game.getRacingCars()).hasSize(2);
     }
 }
