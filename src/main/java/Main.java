3287717fec40e2edffc8838a6856aba801cc5a2e
asa9874 <pjkpjk1129@gmail.com>
2025-08-04 10:55:27 +0900

    refact: Main 클래스 class 첫줄 개행추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import view.InputView;
 import view.ResultView;
 
 public class Main {
+
     public static void main(String[] args) {
         final List<String> carNames = InputView.getCarNames();
         final int tryCount = InputView.getNumberOfRounds();
