eca96e24127f0f37ee901f11e9389f5f2bb389b1
asa9874 <pjkpjk1129@gmail.com>
2025-08-06 09:19:09 +0900

    refact: 파싱 사용 위치 변경

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,14 @@
 import java.util.List;
 
+import util.InputParser;
 import view.InputView;
 import view.ResultView;
 
 public class Main {
 
     public static void main(String[] args) {
-        final List<String> carNames = InputView.getCarNames();
-        final int tryCount = InputView.getNumberOfRounds();
+        final List<String> carNames = InputParser.parseCarNames(InputView.getCarNames());
+        final int tryCount = InputParser.parseNumberOfRounds(InputView.getNumberOfRounds());
 
         CarSimulator carSimulator = new CarSimulator(carNames, tryCount);
         carSimulator.simulate();
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,22 +1,19 @@
 package view;
 
-import java.util.List;
 import java.util.Scanner;
-
-import util.InputParser;
 
 public class InputView {
 
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static List<String> getCarNames() {
+    public static String getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        return InputParser.parseCarNames(scanner.nextLine());
+        return scanner.nextLine();
     }
 
-    public static int getNumberOfRounds() {
+    public static String getNumberOfRounds() {
         System.out.println("시도할 회수는 몇회인가요?");
-        return InputParser.parseNumberOfRounds(scanner.nextLine());
+        return scanner.nextLine();
     }
 
 }
