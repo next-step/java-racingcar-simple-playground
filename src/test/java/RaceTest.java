7df9482ab7ba9cc4d194b4957b888a32f30b2605
mintcoke123 <115970383+mintcoke123@users.noreply.github.com>
2025-09-10 02:29:22 +0900

    test: Race 테스트코드 작성

---
+++ src/test/java/RaceTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class RaceTest {
+    int testCarCount = 3;
+    int testRaceTurn = 5;
+
+    @Test
+    void 레이스의_우승자가_존재한다() {
+        Race race = new Race(testCarCount, testRaceTurn);
+        assertTrue(race.winner.size() >= 1);
+    }
+}
