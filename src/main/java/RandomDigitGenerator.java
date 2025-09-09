3f0445ff64d17426b3049fc1b94a73250085e240
mintcoke123 <115970383+mintcoke123@users.noreply.github.com>
2025-09-10 01:53:23 +0900

    feat: RandomDigitGenerator 클래스 생성

---
+++ src/main/java/RandomDigitGenerator.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class RandomDigitGenerator {
+    private Random random = new Random();
+
+    public int generateRandomDigit(){
+        return random.nextInt(10);
+    }
+
+}
