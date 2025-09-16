a0e91675fea225aeb89963b1b0fd3480182d72da
mintcoke123 <115970383+mintcoke123@users.noreply.github.com>
2025-09-16 13:27:54 +0900

    refactor: 차 이름을 외부에서 가져온다고 가정, CarListGenerator 리팩토링

--- src/main/java/domain/CarListGenerator.java
+++ src/main/java/domain/CarListGenerator.java
@@ -8,15 +8,11 @@ public class CarListGenerator {
 
     private final Scanner scanner = new Scanner(System.in);
 
-    public List<Car> generateCars(int carCount) {
+    public List<Car> generateCars(List<String> carNames) { //외부에서 받는다고 가정
 
-        List<Car> cars = new ArrayList<>();
-        for (int i = 0; i < carCount; i++) {
-            System.out.print((i + 1) + "번 자동차의 이름: ");
-            String carName = scanner.nextLine();
-
-            Car car = new Car(carName, 0);
-            cars.add(car);
+        List<Car> cars = new ArrayList<>(carNames.size());
+        for (String name : carNames) {
+            cars.add(new Car(name, 0));
         }
         return cars;
     }
