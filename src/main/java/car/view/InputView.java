21cbc1fb5523ef20a4a6f199ed704f4f2217deed
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-01-23 02:36:15 +0900

    feat: 함수형 인터페이스 Supplier(매개변수 사용X, 리턴O)를 통해 try-catch를 전담하는 메서드를 만들고, 이를 호출하는 형태로 관리

--- src/main/java/car/view/InputView.java
+++ src/main/java/car/view/InputView.java
@@ -3,6 +3,7 @@ package car.view;
 import car.domain.CarGroup;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.Supplier;
 
 public class InputView {
     private static final Scanner sc = new Scanner(System.in);
@@ -11,28 +12,31 @@ public class InputView {
         throw new AssertionError("InputView는 인스턴스를 생성할 수 없습니다.");
     }
 
-    public static CarGroup getCars() {
-        try {
-            System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-            String input = sc.nextLine();
-            List<String> names = List.of(input.split(","));
-
-            return CarGroup.from(names);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return getCars();
+    private static <T> T repeatUntilSuccess(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] " + e.getMessage());
+            }
         }
     }
 
+    public static CarGroup getCars() {
+        return repeatUntilSuccess(() -> {
+            System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+            String input = sc.nextLine();
+            List<String> names = List.of(input.split(","));
+            return CarGroup.from(names);
+        });
+    }
+
     public static int getTryCount() {
-        try {
+        return repeatUntilSuccess(() -> {
             System.out.println("시도할 회수는 몇회인가요?");
             String input = sc.nextLine();
             return validateCount(input);
-        } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR] " + e.getMessage());
-            return getTryCount(); // 예외 발생 시 다시 처음부터 실행 (재귀)
-        }
+        });
     }
 
     private static int validateCount(String input) {
