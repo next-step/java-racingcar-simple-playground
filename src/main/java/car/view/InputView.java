de441d828ca20fd68ffdf8c0a9847abc8681be50
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-01-23 02:17:33 +0900

    feat: 필드가 없고 static 메서드만 가진 클래스에 대해 생성을 방지하고자 private 접근지정자를 가진 생성자 구현

--- src/main/java/car/view/InputView.java
+++ src/main/java/car/view/InputView.java
@@ -7,6 +7,10 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner sc = new Scanner(System.in);
 
+    private InputView() {
+        throw new AssertionError("InputView는 인스턴스를 생성할 수 없습니다.");
+    }
+
     public static CarGroup getCars() {
         try {
             System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
