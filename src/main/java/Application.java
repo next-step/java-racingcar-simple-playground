bc2eddc6392583fe3580725432455464e98f7033
minko <minye0812@naver.com>
2026-01-24 23:16:50 +0900

    refactor: 네이밍 역할에 맞게 변경

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,7 +3,7 @@ import domain.Race;
 import domain.RandomNumberGenerator;
 import domain.RandomNumberGeneratorImpl;
 import service.RacingCarGame;
-import util.CarNamesParser;
+import util.CarsFactory;
 import view.InputView;
 import view.OutputView;
 
@@ -17,7 +17,7 @@ public final class Application {
         InputView inputView = new InputView(scanner);
         OutputView outputView = new OutputView();
 
-        Cars cars = new Cars(new CarNamesParser().parse(inputView.readCarNames()));
+        Cars cars = new Cars(new CarsFactory().from(inputView.readCarNames()));
         int tryCount = inputView.readTryCount();
 
         Race race = new Race(cars);
