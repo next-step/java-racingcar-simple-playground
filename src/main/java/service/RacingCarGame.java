ae8a5694b007bbca983068bb15d6e8c3d284f2a3
minko <minye0812@naver.com>
2026-01-25 03:39:04 +0900

    refactor: 쓸데없는 import문 정리

--- src/main/java/service/RacingCarGame.java
+++ src/main/java/service/RacingCarGame.java
@@ -1,7 +1,6 @@
 package service;
 
 import domain.*;
-import view.OutputView;
 
 import java.util.List;
 import java.util.function.Consumer;
--- src/test/java/RaceTest.java
+++ src/test/java/RaceTest.java
@@ -2,7 +2,6 @@ import domain.Car;
 import domain.Cars;
 import domain.Race;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
