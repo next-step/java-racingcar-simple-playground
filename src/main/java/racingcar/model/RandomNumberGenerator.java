b5ee3529ddbb530dfee04ce2b1cfc2f9051a5143
htdufhc-bob <htdufhc@gmail.com>
2026-03-15 18:29:29 +0900

    feat: RandomNumberGenerator 인터페이스 생성 및 NumberGenerator 클래스 생성

---
+++ src/main/java/racingcar/model/RandomNumberGenerator.java
@@ -0,0 +1,13 @@
+package racingcar.model;
+
+import java.util.Random;
+
+public class RandomNumberGenerator implements NumberGenerator {
+    private final Random random = new Random();
+    private static final int MAX_RANGE = 10;
+
+    @Override
+    public int generate() {
+        return random.nextInt(MAX_RANGE);
+    }
+}
