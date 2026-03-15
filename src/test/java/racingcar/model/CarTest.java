b5a9368f6adb3b237682aafb6f8095c2a2ff7631
htdufhc-bob <htdufhc@gmail.com>
2026-03-15 20:28:43 +0900

    refactor: DisplayName 수정

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -3,13 +3,15 @@ package racingcar.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class CarTest {
+    private static final int START_POSITION = 0;
     private static final int MOVABLE_NUMBER = 4;
     private static final int NON_MOVABLE_NUMBER = 3;
 
     @Test
-    @DisplayName("자동차 앞으로 이동: 이동 조건이 성립할 경우, 위치는 계속해서 1 증가.")
+    @DisplayName("자동차 앞으로 이동: 이동 조건이 성립할 경우, 위치 1 증가 확인.")
     void move_Forward() {
         //given
         Car car = new Car("Red");
@@ -23,7 +25,7 @@ class CarTest {
     }
 
     @Test
-    @DisplayName("자동차 정지: 이동 조건이 성립하지 않을 경우, 위치는 변화 없음.")
+    @DisplayName("자동차 정지: 이동 조건이 성립하지 않을 경우, 위치는 변화 없음 확인.")
     void move_Stop() {
         //given
         Car car = new Car("Red");
