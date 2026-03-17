7b4b4db6abaab27062c0fb54da368521e5f0480a
htdufhc-bob <htdufhc@gmail.com>
2026-03-17 22:32:06 +0900

    refactor: 메서드와 변수 선언 순서 수정 및 이름 검증 로직 추가

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,14 +1,30 @@
 package racingcar.model;
 
 public class Car {
+    private static final int FORWARD_RULE = 4;
+
     private final String name;
     private int position;
 
-    private static final int FORWARD_RULE = 4;
-
     public Car(String name) {
+        validName(name);
         this.name = name;
         position = 0;
+    }
+
+    public void moveForward(NumberGenerator numberGenerator) {
+        if (numberGenerator.generateNumber() >= FORWARD_RULE) {
+            position++;
+        }
+    }
+
+    private void validName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("비어 있는 이름은 허용되지 않습니다.");
+        }
+        if (name.length() > 10) {
+            throw new IllegalArgumentException("10자를 초과하는 이름은 허용되지 않습니다.");
+        }
     }
 
     public String getName() {
@@ -17,11 +33,5 @@ public class Car {
 
     public int getPosition() {
         return position;
-    }
-
-    public void moveForward(NumberGenerator numberGenerator) {
-        if (numberGenerator.generateNumber() >= FORWARD_RULE) {
-            position++;
-        }
     }
 }
