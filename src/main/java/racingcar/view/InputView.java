d92b73c977522dcc024166de6fcb4844600edbae
htdufhc-bob <htdufhc@gmail.com>
2026-03-22 00:10:53 +0900

    refactor: 사용되지 않는 코드 제거

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,21 +1,11 @@
 package racingcar.view;
 
-import racingcar.model.NumberGenerator;
-
 import java.util.Scanner;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
     public String readInput() { // 자동차들의 이름 입력 기능
-        return scanner.nextLine();
-    }
-
-    public int readTryCount() { // 게임 반복 횟수 입력 기능
-        return scanner.nextInt();
-    }
-
-    public String retryReadCarNames() {
         return scanner.nextLine();
     }
 }
