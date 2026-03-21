d6da2cef2bc8261725de3b51a391f48f29048a7d
htdufhc-bob <htdufhc@gmail.com>
2026-03-21 23:44:28 +0900

    refactor: CarStatus 반환 메서드 추가와 우승자 문자열 리스트 반환하도록 수정

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -2,6 +2,7 @@ package racingcar.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cars {
     private static final int START_POSITION = 0;
@@ -12,9 +13,28 @@ public class Cars {
         this.cars = cars;
     }
 
-    private void addIfWinner(List<Car> winners, Car car, int maxPosition) {
+    public void moveCars(NumberGenerator numberGenerator) {
+        cars.forEach(car -> car.moveForward(numberGenerator));
+    }
+
+    public List<String> getWinners() {
+        int maxPosition = getMaxPosition();
+        List<String> winners = new ArrayList<>();
+        for (Car car : cars) {
+            addIfWinner(winners, car, maxPosition);
+        }
+        return winners;
+    }
+
+    public List<CarStatus> getCarsStatus() {
+        return cars.stream()
+                .map(car -> new CarStatus(car.getName(), car.getPosition()))
+                .collect(Collectors.toList());
+    }
+
+    private void addIfWinner(List<String> winners, Car car, int maxPosition) {
         if (car.getPosition() == maxPosition) {
-            winners.add(car);
+            winners.add(car.getName());
         }
     }
 
@@ -24,14 +44,5 @@ public class Cars {
             maxPosition = Math.max(car.getPosition(), maxPosition);
         }
         return maxPosition;
-    }
-
-    public List<Car> getWinners() {
-        int maxPosition = getMaxPosition();
-        List<Car> winners = new ArrayList<>();
-        for (Car car : cars) {
-            addIfWinner(winners, car, maxPosition);
-        }
-        return winners;
     }
 }
