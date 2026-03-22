ac77052d7f6853b27b440a983d61ab920a4b0b5c
dahyn <k0d5h16@gmail.com>
2026-03-23 00:42:24 +0900

    refactor: 메인 단순화

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -13,21 +13,31 @@ import java.util.List;
 public class Application {
 
     public static void main(String[] args) {
-        List<Car> carList = Arrays.stream(InputView.readCarNames())
+        String[] carNames = InputView.readCarNames();
+        int tryCount = InputView.readTryCount();
+
+        RacingGame racingGame = createRacingGame(carNames);
+
+        OutputView.printResultHeader();
+        playRounds(racingGame,tryCount);
+        OutputView.printWinners(racingGame.getWinners());
+    }
+
+    private static RacingGame createRacingGame(String[] carNames) {
+        return new RacingGame(new Cars(createCars(carNames)), new RandomNumberGenerator());
+    }
+
+    private static List<Car> createCars(String[] carNames) {
+        return Arrays.stream(carNames)
                 .map(String::trim)
                 .map(Car::new)
                 .toList();
+    }
 
-        RacingGame racingGame = new RacingGame(new Cars(carList), new RandomNumberGenerator());
-
-        int tryCount = InputView.readTryCount();
-
-        OutputView.printResultHeader();
+    private static void playRounds(RacingGame racingGame, int tryCount){
         for (int i = 0; i < tryCount; i++) {
             racingGame.playRound();
             OutputView.printRoundResult(racingGame.getCars());
         }
-
-        OutputView.printWinners(racingGame.getWinners());
     }
 }
