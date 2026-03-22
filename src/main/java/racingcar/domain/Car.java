8ae7e27312ed26218d09bf6346ba5d93a00716f5
dahyn <k0d5h16@gmail.com>
2026-03-22 20:36:31 +0900

    feat: 자동차 이름 5자 이하 검증 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,11 +3,13 @@ package racingcar.domain;
 public class Car {
 
     private static final int MOVE_CONDITION = 4;
+    private static final int MAXIMUM_CAR_NAME_LENGTH = 5;
 
     private final String name;
     private int position;
 
     public Car(String name) {
+        validNameLength(name);
         this.name = name;
     }
 
@@ -27,6 +29,12 @@ public class Car {
 
     private boolean isMovable(int number) {
         return number >= MOVE_CONDITION;
+    }
+
+    private void validNameLength(String name) {
+        if (name.length() > MAXIMUM_CAR_NAME_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름은 " + MAXIMUM_CAR_NAME_LENGTH + "자 이하만 가능합니다.");
+        }
     }
 
 }
