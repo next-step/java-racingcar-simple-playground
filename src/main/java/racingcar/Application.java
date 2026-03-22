0ec4d3a6e9a2b78407cf075282da30e7f1c57d9f
dahyn <k0d5h16@gmail.com>
2026-03-22 21:54:26 +0900

    refactor: 입력과 출력을 view로 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,55 +4,30 @@ import racingcar.domain.Car;
 import racingcar.domain.Cars;
 import racingcar.domain.RacingGame;
 import racingcar.generator.RandomNumberGenerator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class Application {
 
     public static void main(String[] args) {
-
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        String[] items = input.split(",");
-
-        List<Car> carList = Arrays.stream(items)
+        List<Car> carList = Arrays.stream(InputView.readCarNames())
                 .map(String::trim)
                 .map(Car::new)
                 .toList();
 
-        Cars cars = new Cars(carList);
-        RacingGame racingGame = new RacingGame(cars, new RandomNumberGenerator());
+        RacingGame racingGame = new RacingGame(new Cars(carList), new RandomNumberGenerator());
 
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        int tryCount = scanner.nextInt();
+        int tryCount = InputView.readTryCount();
 
-        System.out.println();
-        System.out.println("실행 결과");
+        OutputView.printResultHeader();
         for (int i = 0; i < tryCount; i++) {
             racingGame.playRound();
-            printRoundResult(racingGame.getCars());
+            OutputView.printRoundResult(racingGame.getCars());
         }
 
-        printWinners(racingGame.getWinners());
-    }
-
-    private static void printRoundResult(List<Car> cars) {
-        for (Car car : cars) {
-            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
-        }
-        System.out.println();
-    }
-
-    private static void printWinners(List<Car> winners) {
-        String names = winners.stream()
-                .map(Car::getName)
-                .collect(Collectors.joining(", "));
-
-        System.out.println(names + "가 최종 우승했습니다.");
+        OutputView.printWinners(racingGame.getWinners());
     }
 }
---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,29 @@
+package racingcar.view;
+
+import racingcar.domain.Car;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OutputView {
+
+    public static void printResultHeader(){
+        System.out.println();
+        System.out.println("실행 결과");
+    }
+
+    public static void printRoundResult(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+        }
+        System.out.println();
+    }
+
+    public static void printWinners(List<Car> winners) {
+        String names = winners.stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(", "));
+
+        System.out.println(names + "가 최종 우승했습니다.");
+    }
+}
