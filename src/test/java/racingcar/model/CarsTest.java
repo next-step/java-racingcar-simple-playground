7bcae094835e639e070fa3bbc51aa50599408b19
htdufhc-bob <htdufhc@gmail.com>
2026-03-25 15:20:29 +0900

    test: Car 객체 이름 조건 변화에 따른 CarsTest.java 수정

--- src/test/java/racingcar/model/CarsTest.java
+++ src/test/java/racingcar/model/CarsTest.java
@@ -13,9 +13,9 @@ public class CarsTest {
     @Test
     void 우승자가_1명일_수_있다() {
         //given
-        Car winner = new Car("winner1");
-        Car loser1 = new Car("loser1");
-        Car loser2 = new Car("loser2");
+        Car winner = new Car("win1");
+        Car loser1 = new Car("lose1");
+        Car loser2 = new Car("lose2");
         winner.moveForward(()->MOVABLE_NUMBER);
         loser1.moveForward(()->NON_MOVABLE_NUMBER);
         loser2.moveForward(()->NON_MOVABLE_NUMBER);
@@ -24,16 +24,16 @@ public class CarsTest {
         //when
         List<String> winners = cars.getWinners();
         //then
-        assertThat(winners).containsExactlyInAnyOrder("winner1");
+        assertThat(winners).containsExactlyInAnyOrder("win1");
     }
 
     @Test
     void 우승자가_여러_명일_수_있다() {
         //given
-        Car winner1 = new Car("winner1");
-        Car winner2 = new Car("winner2");
-        Car loser1 = new Car("loser1");
-        Car loser2 = new Car("loser2");
+        Car winner1 = new Car("win1");
+        Car winner2 = new Car("win2");
+        Car loser1 = new Car("lose1");
+        Car loser2 = new Car("lose2");
         winner1.moveForward(()->MOVABLE_NUMBER);
         winner2.moveForward(()->MOVABLE_NUMBER);
         loser1.moveForward(()->NON_MOVABLE_NUMBER);
@@ -43,6 +43,6 @@ public class CarsTest {
         //when
         List<String> winners = cars.getWinners();
         //then
-        assertThat(winners).containsExactlyInAnyOrder("winner1", "winner2");
+        assertThat(winners).containsExactlyInAnyOrder("win1", "win2");
     }
 }
