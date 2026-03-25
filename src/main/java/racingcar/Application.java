d13fa00ec1123dafd975e6314f7e93b9fd6ff41f
htdufhc-bob <htdufhc@gmail.com>
2026-03-25 17:28:23 +0900

    refactor: GameManageController 의존성 주입 및 실행 로직 구성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,14 +1,20 @@
 package racingcar;
 
 import racingcar.controller.GameManageController;
+import racingcar.controller.InputValidator;
 import racingcar.model.RandomNumberGenerator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class Application {
 
     public static void main(String[] args) {
         final RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        final InputView inputView = new InputView();
+        final OutputView outputView = new OutputView();
+        final InputValidator inputValidator = new InputValidator();
 
-        GameManageController gameManageController = new GameManageController();
+        GameManageController gameManageController = new GameManageController(inputView, outputView, inputValidator);
         gameManageController.readyRacingGame();
         gameManageController.runRacingGame(randomNumberGenerator);
     }
