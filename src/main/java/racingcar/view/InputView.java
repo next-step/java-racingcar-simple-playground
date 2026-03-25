a33e829908f2416a70265e093180b0c98d82235d
htdufhc-bob <htdufhc@gmail.com>
2026-03-25 17:24:56 +0900

    refactor: 테스트 가능성 향상을 위해 InputView 구조 개선

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,7 +5,11 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
-    public String readInput() { // 자동차들의 이름 입력 기능
+    public String readCarNames() { // 자동차들의 이름 입력 기능
+        return scanner.nextLine();
+    }
+
+    public String readTryCount() {
         return scanner.nextLine();
     }
 }
