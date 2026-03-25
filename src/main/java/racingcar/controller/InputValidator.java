2c1e5d7d01e967e52659ab963fad310c6c618546
htdufhc-bob <htdufhc@gmail.com>
2026-03-25 16:47:35 +0900

    feat: 자동차 이름 중복 검증 로직 추가

--- src/main/java/racingcar/controller/InputValidator.java
+++ src/main/java/racingcar/controller/InputValidator.java
@@ -1,12 +1,16 @@
 package racingcar.controller;
 
 import racingcar.model.Car;
+import racingcar.constant.ErrorMessage;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class InputValidator {
     public List<Car> validateCarName(List<String> nameList) {
         validateNameCount(nameList);
+        validateDuplicateName(nameList);
         return nameList.stream()
                 .map(Car::new)
                 .toList();
@@ -14,7 +18,14 @@ public class InputValidator {
 
     private void validateNameCount(List<String> carNameList) {
         if (carNameList.size() <= 1) {
-            throw new IllegalArgumentException("게임 플레이에는 2명 이상이 필요합니다.");
+            throw new IllegalArgumentException(ErrorMessage.MINIMUM_CAR_COUNT.getMessage());
+        }
+    }
+
+    private void validateDuplicateName(List<String> carNameList) {
+        Set<String> uniqueNameSet = new HashSet<>(carNameList);
+        if (uniqueNameSet.size() != carNameList.size()) {
+            throw new IllegalArgumentException(ErrorMessage.NAME_DUPLICATE.getMessage());
         }
     }
 
@@ -28,13 +39,13 @@ public class InputValidator {
         try {
             return Integer.parseInt(inputTryCount);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자가 아닌 값이 입력되었습니다.");
+            throw new IllegalArgumentException(ErrorMessage.INVALID_NUMBER_FORMAT.getMessage());
         }
     }
 
     private void validateCountRange(int tryCount) {
         if (tryCount <= 0) {
-            throw new IllegalArgumentException("1번 이상의 시도를 해야 합니다.");
+            throw new IllegalArgumentException(ErrorMessage.MINIMUM_TRY_COUNT.getMessage());
         }
     }
 }
