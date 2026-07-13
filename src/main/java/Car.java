b060087a9190954f1e35aa8ee499b740e66850e8
Seong Jin <oroi2009@gmail.com>
2026-07-13 18:53:55 +0900

    feat : Car 도메인 구현

---
+++ src/main/java/Car.java
@@ -0,0 +1,15 @@
+public class Car {
+    private final String name;
+    private int position;
+
+    private static final int FORWARD_RULE = 4;
+
+    public Car(String name){
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void move(NumberGenerator generator){
+        if(generator.generate() >= FORWARD_RULE) position++;
+    }
+}
