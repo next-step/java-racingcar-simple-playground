f9a660ab03a7971dc08fc8eb227e053bcbc3cbac
anrwkd9902 <anrwkd9902@naver.com>
2026-07-15 03:33:01 +0900

    1단계

---
+++ src/main/java/RacingCar.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+
+public class RacingCar {
+    private String name;
+    private int distance;
+
+    public RacingCar(String name) {
+        this.name = name;
+        this.distance = 0;
+    }
+
+    public int randomNum() {
+        Random random = new Random();
+        int num = random.nextInt(10);
+
+        return num;
+    }
+
+    public void move(int num) {
+        if(num >= 4) {
+            this.distance++;
+        }
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public int getDistance() {
+        return this.distance;
+    }
+
+}
+
+
