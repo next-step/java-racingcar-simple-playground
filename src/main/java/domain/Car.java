815743d1da7a277d5817bcfb7a2f5e6031128e41
ThreeeJ <2171266@hansung.ac.kr>
2026-07-16 15:41:31 +0900

    refactor: validateName의 예외 타입을 구체화 (RuntimeException → IllegalArgumentException)

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -32,7 +32,7 @@ public class Car {
 
     private void validateName(String name) {
         if (name.length() > 5) {
-            throw new RuntimeException("자동차 이름은 5자 이하만 가능합니다.");
+            throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
         }
     }
 }
