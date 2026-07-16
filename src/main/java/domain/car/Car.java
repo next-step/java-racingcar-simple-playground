e6ba1c2d9a9b6ceb06c15c90f1cbb5e36281a436
Seong Jin <oroi2009@gmail.com>
2026-07-16 14:22:59 +0900

    feat : 자동차 이동 여부 판단 예외처리 추가

--- src/main/java/domain/car/Car.java
+++ src/main/java/domain/car/Car.java
@@ -5,6 +5,9 @@ public class Car {
     private int position;
 
     private static final int FORWARD_RULE = 4;
+    private static final int MIN_MOVING_NUMBER = 0;
+    private static final int MAX_MOVING_NUMBER = 9;
+    private static final int MAX_NAME_LENGTH = 5;
 
     public Car(String name) {
         validateName(name);
@@ -14,6 +17,8 @@ public class Car {
     }
 
     public void move(int number) {
+        validateMoveNumber(number);
+
         if (number >= FORWARD_RULE) {
             position++;
         }
@@ -32,8 +37,14 @@ public class Car {
             throw new IllegalArgumentException("이름은 비어 있을 수 없습니다.");
         }
 
-        if (name.length() > 5) {
-            throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("이름은 " + MAX_NAME_LENGTH + "자 이하만 가능합니다.");
+        }
+    }
+
+    private void validateMoveNumber(int number) {
+        if (number > MAX_MOVING_NUMBER || number < MIN_MOVING_NUMBER) {
+            throw new IllegalArgumentException("유효하지 않은 숫자값입니다.");
         }
     }
 }
