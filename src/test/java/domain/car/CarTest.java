28965db2118e4fa2ef405db2759abe03a92851cb
Seong Jin <oroi2009@gmail.com>
2026-07-17 11:41:22 +0900

    style: 테스트 클래스 접근 제어자 제거

--- src/test/java/domain/car/CarTest.java
+++ src/test/java/domain/car/CarTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class CarTest {
+class CarTest {
 
     @Test
     void random값이_4이상이면_position값이_증가한다() {
--- src/test/java/domain/game/RacingGameTest.java
+++ src/test/java/domain/game/RacingGameTest.java
@@ -10,7 +10,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class RacingGameTest {
+class RacingGameTest {
     private List<Car> cars;
 
     @BeforeEach
