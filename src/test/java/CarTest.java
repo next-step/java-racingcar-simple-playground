3cebd05a3fc49b36a2815caaba2307307cff0a97
jh-lee0 <justin030627@naver.com>
2026-07-19 15:39:15 +0900

    refactor: 경곗값 테스트 개선(@ParameterizedTest)

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,6 +1,8 @@
 import domain.Car;
 import domain.NumberGenerator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,46 +24,29 @@ class CarTest {
     /*
      * 경계값 테스트 => 0, 3, 4, 9
      * */
-    @Test
-    void 랜덤값_0일때_멈춤() {
+    @ParameterizedTest
+    @ValueSource(ints = {0, 3})
+    void 랜덤값_0과_3일때_멈춤(int ranVal) {
         // Given(준비)
-        NumberGenerator testNumberGenerator = new TestNumberGenerator(0);
+        NumberGenerator testNumberGenerator = new TestNumberGenerator(ranVal);
         Car car = new Car("car", testNumberGenerator);
         // When(실행)
         car.move();
         // Then(검증)
         assertEquals(0, car.getTotalDistance());
     }
-    @Test
-    void 랜덤값_3일때_멈춤() {
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 9})
+    void 랜덤값_4와_9일때_전진(int ranVal) {
         // Given(준비)
-        NumberGenerator testNumberGenerator = new TestNumberGenerator(3);
-        Car car = new Car("car", testNumberGenerator);
-        // When(실행)
-        car.move();
-        // Then(검증)
-        assertEquals(0, car.getTotalDistance());
-    }
-    @Test
-    void 랜덤값_4일때_전진() {
-        // Given(준비)
-        NumberGenerator testNumberGenerator = new TestNumberGenerator(4);
+        NumberGenerator testNumberGenerator = new TestNumberGenerator(ranVal);
         Car car = new Car("car", testNumberGenerator);
 
         // When(실행)
         car.move();
         // Then(검증)
         assertEquals(1, car.getTotalDistance());
-    }
-    @Test
-    void 랜덤값_9일때_전진() {
-        // Given(준비)
-        NumberGenerator testNumberGenerator = new TestNumberGenerator(9);
-        Car car = new Car("car", testNumberGenerator);
-        // When(실행)
-        car.move();
-        // Then(검증)
-        assertEquals(1, car.getTotalDistance()); // 주의) 각 테스트는 독립적으로 실행되므로 예상값은 2가 아닌 1
     }
 
     /*
