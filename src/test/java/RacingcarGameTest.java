ef2b77a2c03c9a46435723260c119a8e784259ea
jh-lee0 <justin030627@naver.com>
2026-07-19 17:49:46 +0900

    refactor: 우승자 선정 테스트 케이스 추가

--- src/test/java/RacingcarGameTest.java
+++ src/test/java/RacingcarGameTest.java
@@ -24,7 +24,7 @@ class RacingcarGameTest {
     * 우승 자동차 구하기 로직 테스트
     * */
     @Test
-    void 공동_우승자가_없을_때() {
+    void 공동_우승자가_없는_때() {
         // Given
         String[] names = {"car1", "car2", "car3"};
         RacingcarGame testGame = new RacingcarGame(names, new TestNumberGenerator(7));
@@ -40,7 +40,7 @@ class RacingcarGameTest {
         assertEquals(1, winnerCars.size());
     }
     @Test
-    void 공동_우승자가_있을_때() {
+    void 공동_우승자가_있는_경우() {
         // Given
         String[] names = {"car1", "car2", "car3"};
         RacingcarGame testGame = new RacingcarGame(names, new TestNumberGenerator(7));
@@ -56,5 +56,22 @@ class RacingcarGameTest {
         assertEquals(cars.get(1), winnerCars.get(0));
         assertEquals(cars.get(2), winnerCars.get(1));
         assertEquals(2, winnerCars.size());
+    }
+    @Test
+    void 모든_자동차가_공동_우승자인_경우() {
+        // Given
+        String[] names = {"car1", "car2", "car3"};
+        RacingcarGame testGame = new RacingcarGame(names, new TestNumberGenerator(7));
+
+        List<Car> cars = testGame.getCarList();
+
+        // When
+        List<Car> winnerCars = testGame.getWinner();
+
+        // Then
+        assertEquals(cars.get(0), winnerCars.get(0));
+        assertEquals(cars.get(1), winnerCars.get(1));
+        assertEquals(cars.get(2), winnerCars.get(2));
+        assertEquals(3, winnerCars.size());
     }
 }
