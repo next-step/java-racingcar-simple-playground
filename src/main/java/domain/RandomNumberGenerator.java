57d456d819174b409693ca8cee323f4cda55bcd8
ThreeeJ <2171266@hansung.ac.kr>
2026-07-20 10:27:34 +0900

    refactor: RandomNumberGenerator의 매직 넘버를 상수로 추출

--- src/main/java/domain/RandomNumberGenerator.java
+++ src/main/java/domain/RandomNumberGenerator.java
@@ -3,10 +3,12 @@ package domain;
 import java.util.Random;
 
 public class RandomNumberGenerator implements NumberGenerator {
+    private static final int RANDOM_BOUND = 10;
+
     private final Random random = new Random();
 
     @Override
     public int generate() {
-        return random.nextInt(10);
+        return random.nextInt(RANDOM_BOUND);
     }
 }
