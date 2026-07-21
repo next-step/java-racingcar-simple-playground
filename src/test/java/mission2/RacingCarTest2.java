c648513a8be17d156ad19bd572cfe84edff1e4de
KDYoung5606 <2271323@hansung.ac.kr>
2026-07-21 16:55:20 +0900

    [Refactor] 테스크 코드 수정 및 같은 이름이 존재할 떄 발생하는 예외에 대한 테스트 코드 작성

--- src/test/java/mission2/RacingCarTest2.java
+++ src/test/java/mission2/RacingCarTest2.java
@@ -1,5 +1,6 @@
 package mission2;
 
+import mission4.RandomNum;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,19 +8,27 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RacingCarTest2 {
     @Test
-    @DisplayName("레이킹카 리뉴얼 버전")
+    @DisplayName("레이싱 게임 시작")
     void test_레이싱카(){
         String[] names = {"a", "b", "c"};
         int count = 5;
-        Racing racing = new Racing(names,count);
+        Racing racing = new Racing(names,count,new RandomNum(10));
         racing.start();
     }
 
     @Test
-    @DisplayName("들어온 이름의 길이가 5를 넘어간다면?")
-    void test_이름길이가_5글자를_넘는경우(){
+    @DisplayName("들어온 이름의 길이가 5를 넘어간다면 예외처리")
+    void test_이름길이가_5글자를_넘는경우_예외처리(){
         String[] names = {"aaaaaa", "b","c"};
         int count = 5;
-        assertThrows(RuntimeException.class, ()->new Racing(names, count));
+        assertThrows(IllegalArgumentException.class, ()->new Racing(names, count, new RandomNum(10)));
+    }
+
+    @Test
+    @DisplayName("객체를 생성하기 이전에 동일한 플레이어 이름이 존재하는 경우 예외처리")
+    void test_동일한_이름이_존재한다면_예외처리(){
+        String[] names = {"a", "a", "c"};
+        int count = 5;
+        assertThrows(IllegalArgumentException.class, ()->new Racing(names, count, new RandomNum(10)));
     }
 }
