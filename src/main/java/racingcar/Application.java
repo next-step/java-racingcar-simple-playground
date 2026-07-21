b9f65a025d601c8ce216f8e541bfbcb7a9d0ad45
juhee0223 <pjuhee23@dankook.ac.kr>
2026-07-22 00:45:06 +0900

    chore: 오타수정 및 자원반환코드 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,18 +7,17 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         //사용자 입력 받기 (쉼표로 구분해서 자동차 이름 입력)
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)룰 기준으로 구분).");
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
 
-        Scanner scanner1 = new Scanner(System.in);
-        String value = scanner1.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        String value = scanner.nextLine();
         List<String> carNames = List.of(value.split(","));  // comma로 쪼개서 carNames에 문자열 형태로 차 이름들을 받는다
 
         int carCount = carNames.size(); //총 몇대의 차가 경주에 참여하는지 carCount
 
         //사용자 입력 받기 (시도 횟수 정수로 입력)
         System.out.println("시도할 회수는 몇회인가요?");
-        Scanner scanner2 = new Scanner(System.in);
-        int number = scanner2.nextInt();
+        int number = scanner.nextInt();
         System.out.println(); //출력 형식에 맞추기 위한 줄바꿈
 
         Racing racing = new Racing(carCount, number);
@@ -32,5 +31,6 @@ public class Application {
         System.out.println("실행 결과");
         racing.startRacing();
 
+        scanner.close();
     }
 }
