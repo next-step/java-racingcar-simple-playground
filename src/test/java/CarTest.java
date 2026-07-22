45f494165a6579c8872efb04d758ad7dcfba94dd
johaeunn <jheun04@naver.com>
2026-07-22 21:34:48 +0900

    test: 자동차 이동, 우승자 판별 및 예외 테스트 추가

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,9 +1,12 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CarTest {
     @Test
+    @DisplayName("숫자가 4이상이면 자동차가 전진한다")
     void movesWhenNumberIsGreaterThanThree() {
         Car car = new Car("test");
 
@@ -13,11 +16,21 @@ public class CarTest {
     }
 
     @Test
+    @DisplayName("숫자가 3이하이면 자동차가 멈춘다")
     void doesNotMoveWhenNumberIsThreeOrLess() {
         Car car = new Car("test");
 
         car.move(3);
 
         assertEquals(0, car.getPosition());
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 5자를 초과하면 예외가 발생한다")
+    void throwExceptionWhenNameExceedsFiveCharacters() {
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> new Car("abcdef")
+        );        
     }
 }
--- src/test/java/RacingGameTest.java
+++ src/test/java/RacingGameTest.java
@@ -1,11 +1,14 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.*;
 
 public class RacingGameTest {
     @Test
+    @DisplayName("한 라운드에 자동차 여러대가 각자 이동한다")
     void movesMultipleCars() {
         List<Car> cars = new ArrayList<>();
         cars.add(new Car("test1"));
@@ -22,6 +25,7 @@ public class RacingGameTest {
     }
 
     @Test
+    @DisplayName("가장 멀리 이동한 자동차가 한 대이면 우승자도 한 명이다")
     void findWinnerWithMaxPosition() {
         List<Car> cars = new ArrayList<>();
         cars.add(new Car("test1"));
@@ -40,6 +44,7 @@ public class RacingGameTest {
     }
 
     @Test
+    @DisplayName("가장 멀리 이동한 자동차가 여러 대이면 우승자도 여러 명이다")
     void findAllWinnersWithMaxPosition() {
         List<Car> cars = new ArrayList<>();
         cars.add(new Car("test1"));
@@ -56,5 +61,38 @@ public class RacingGameTest {
         assertEquals(2, winners.size());
         assertEquals("test1", winners.get(0).getName());
         assertEquals("test3", winners.get(1).getName());
+    }
+
+    @Test
+    @DisplayName("자동차 수와 이동 값의 개수가 다르면 예외가 발생한다")
+    void throwExceptionWhenCarAndNumberCountsDiffer() {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("test1"));
+        cars.add(new Car("test2"));
+        cars.add(new Car("test3"));
+
+        List<Integer> numbers = Arrays.asList(6, 2);
+
+        RacingGame racingGame = new RacingGame(cars);
+
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> racingGame.moveCars(numbers)
+        ); 
+    }
+
+    @Test
+    @DisplayName("모든 자동차의 위치가 같으면 모두 공동 우승자이다")
+    void findsAllCarsAsWinnersWhenPositionsAreEqual() {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("test1"));
+        cars.add(new Car("test2"));
+        cars.add(new Car("test3"));
+
+        RacingGame racingGame = new RacingGame(cars);
+
+        List<Car> winners = racingGame.findWinners();
+
+        assertEquals(3, winners.size());
     }
 }
