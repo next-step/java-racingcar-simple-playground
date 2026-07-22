8c7c043a9ad10b99e41aa848cc224e9e3c0800d8
johaeunn <jheun04@naver.com>
2026-07-22 17:54:47 +0900

    refactor: 자동차 경주 실행 흐름과 출력 메서드 분리

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Application {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<Car> cars = new ArrayList<>();
         
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String carNames = scanner.nextLine();
@@ -11,36 +12,51 @@ public class Application {
         int round = scanner.nextInt();
         System.out.println();
 
-        List<Car> cars = new ArrayList<>();
-
         for (String name : carNames.split(",")) {
             cars.add(new Car(name));
         }
 
         RacingGame racingGame = new RacingGame(cars);
 
+        runGame(round, racingGame, cars);
+
+        scanner.close();
+    }
+
+    public static void runGame(int round, RacingGame racingGame, List<Car> cars) {
         System.out.println("실행 결과");
+
         for (int i = 0; i < round; i++) {
             racingGame.playRound();
-            for (Car car : cars) {
-                System.out.print(car.getName() + ":");
-                for (int j = 0; j < car.getPosition(); j++) {
-                    System.out.print("-");
-                }
-                System.out.println();
-            }
+            printRoundResult(cars);
             System.out.println();
         }
 
-        List<Car> winners = new ArrayList<>();
+        printWinners(racingGame);
+    }
 
-        winners = racingGame.findWinners();
-
-        for (int i = 0; i < winners.size(); i++) {
-            System.out.print(winners.get(i).getName());
+    public static void printRoundResult(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.print(car.getName() + " : ");
+            printPosition(car.getPosition());
+            System.out.println();
         }
-        System.out.print("가 최종 우승했습니다.");
+    }
 
-        scanner.close();
+    public static void printPosition(int position) {
+        for (int i = 0; i < position; i++) {
+            System.out.print("-");
+        }
+    }
+
+    public static void printWinners(RacingGame racingGame) {
+        StringJoiner winnerNames = new StringJoiner(", ");
+        List<Car> winners = racingGame.findWinners();
+
+        for (Car winner : winners) {
+            winnerNames.add(winner.getName());
+        }
+
+        System.out.print(winnerNames + "가 최종 우승했습니다.");
     }
 }
