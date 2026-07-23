c458e4a8edcbe48e28c99c75345fa4f1a5464ebf
johaeunn <jheun04@naver.com>
2026-07-24 05:10:22 +0900

    refactor: 비즈니스 규칙의 숫자 값을 상수로 분리

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -3,17 +3,19 @@ package domain;
 public class Car {
     private String name;
     private int position;
+    public static final int MAX_NAME_LENGTH = 5;
+    public static final int MOVE_THRESHOLD = 4;
 
     public Car(String name) {
-        if(name.length() > 5) {
-            throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+        if(name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름은 " + MAX_NAME_LENGTH + "자 이하여야 합니다.");
         }
         this.name = name;
         this.position = 0;
     }
 
-    public void move(int n) {
-        if(n > 3) {
+    public void move(int randomNumber) {
+        if(randomNumber >= MOVE_THRESHOLD) {
             position++;
         }
     }
