6af2c1acc2c7916831620fb6c6461982bcf4c8c1
juhee0223 <pjuhee23@dankook.ac.kr>
2026-07-24 21:23:17 +0900

    chore: 불필요하게 코드 두줄이었던것 한줄로 정리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,9 +9,7 @@ import java.util.List;
 public class Application {
     public static void main(final String... args) {
         final List<String> carNames = InputView.setCarNames();
-        final int tryCount = InputView.setTryCount();
-
-        final RacingGameSetting racingGameSetting = new RacingGameSetting(carNames, tryCount);
+        final RacingGameSetting racingGameSetting = new RacingGameSetting(carNames, InputView.setTryCount());
         final RacingGame racingGame = new RacingGame(racingGameSetting);
 
         System.out.println("실행 결과");
