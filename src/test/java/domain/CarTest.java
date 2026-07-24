d13986ae62e4b142004ba4fcb53ee62cf2e0ae4b
johaeunn <jheun04@naver.com>
2026-07-25 01:33:12 +0900

    test: 자동차 이름이 5자인 경우 성공 테스트 추가

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -35,4 +35,12 @@ public class CarTest {
             () -> new Car("abcdef")
         );        
     }
+
+    @Test
+    @DisplayName("자동차 이름이 5자 이하이면 정상적으로 생성된다")
+    void createCarWithFiveCharacterNameSuccessfully() {
+        Car car = new Car("12345");   
+        
+        assertEquals("12345", car.getName());
+    }
 }
