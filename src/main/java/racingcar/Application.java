2a7bf556c35f50b6830b441550afec62bf502fa6
juhee0223 <pjuhee23@dankook.ac.kr>
2026-07-26 22:45:31 +0900

    refactor: 입력값 준비방식 통일-변수로 따로 분리하여 생성자에 전달

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import racingcar.domain.Car;
 import racingcar.domain.RacingCars;
 import racingcar.domain.RacingGameWinner;
 import racingcar.domain.RacingLap;
@@ -18,7 +19,9 @@ public class Application {
         RacingLap racingLap = new RacingLap(lapCount);  //입력을 받자마자 검증
 
         //검증된 것으로 인스턴스를 생성
-        RacingGame racingGame = new RacingGame(racingCars.getCarsList(), racingLap.getLapCount());
+        final List<Car> validatedCarNames = racingCars.getCarsList();
+        final int validatedLapCount = racingLap.getLapCount();
+        RacingGame racingGame = new RacingGame(validatedCarNames,validatedLapCount);
 
         System.out.println("실행 결과");
         for (int i = 0; i < racingLap.getLapCount(); i++) {
