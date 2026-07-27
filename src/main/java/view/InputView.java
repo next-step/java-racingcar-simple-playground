f9da22f121191abe43c8e258ef2d00df4f4859b0
johaeunn <jheun04@naver.com>
2026-07-28 07:09:00 +0900

    refactor: Inputview Scanner의 접근 범위와 불변성 개선

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Arrays;
 
 public class InputView {
-    Scanner scanner = new Scanner(System.in);
+    private final Scanner scanner = new Scanner(System.in);
 
     public List<String> getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
