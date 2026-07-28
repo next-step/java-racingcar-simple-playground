a60d9624f42e24aecdb5ddfcab53116991414c48
JisuHan <ivyjisu@gmail.com>
2026-07-28 14:21:15 +0900

    refactor: totalPlay 초기화 로직 개선

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,14 +11,13 @@ public class Main {
     public static void main(String[] args) {
         List<Car> carsList = new ArrayList<>();
         String[] carNames = getCarNames();
-        int totalPlay = 0;
+        int totalPlay = getTryCount();
         for (int i = 0; i < carNames.length; i++) {
             carsList.add(new Car(carNames[i]));
         }
         Cars cars = new Cars(carsList);
         NumberGenerator numberGenerator = new MakeRandomNum();
         Play race = new Play(cars, numberGenerator);
-        totalPlay = getTryCount();
         for (int i = 0; i < totalPlay; i++) {
             race.playRound();
             ResultView.printRace(cars);
