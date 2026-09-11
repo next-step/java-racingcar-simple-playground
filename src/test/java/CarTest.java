108de6d69fc1d1c4c0e4ad9260195c5f60ce1e57
calamellucy <sua050526@naver.com>
2026-09-11 15:21:26 +0900

    test: Car 테스트 코드 파일 생성

---
+++ src/test/java/CarTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+
+    @Test
+    void moveCar() {
+        Car car = new Car("Car1");
+        car.moveCar();
+
+    }
+}
