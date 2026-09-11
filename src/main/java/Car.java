8dac1642f2e9221901d8b23a1766f3ddc204d845
calamellucy <sua050526@naver.com>
2026-09-11 15:30:13 +0900

    feat: getter setter 추가

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,18 +1,25 @@
 public class Car {
-    // 자동차 이름 멤버변수
     private String name;
-    // 자동차 위치 멤버변수
     private int pos;
-    
-    // 생성자 - 자동차 이름 설정, 위치 0으로 설정
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getPos() {
+        return pos;
+    }
+    public void setPos(int pos) {
+        this.pos = pos;
+    }
+
     public Car(String name) {
         this.name = name;
         pos = 0;
     }
-    // 자동차 움직이는 메서드
-        // 난수 생성 메서드
-        // 생성한 랜덤 값을 이용해서 움직일지 말지 결정하는 메서드
-        // 움직일지 말지 결정된거를 이용해서 자동차 위치 변경
+
     public void moveCar() {
         if (shouldMove(makeRandomNum())) pos++;
     }
