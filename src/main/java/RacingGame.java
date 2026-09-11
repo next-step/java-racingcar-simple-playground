8add0495b9d0b813a9e341c7757ef448e073c570
woosukkk <ekstor8544@naver.com>
2026-09-11 13:12:00 +0900

    feat : RacingGame 클래스 구현

---
+++ src/main/java/RacingGame.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingGame {
+
+    private final List<Car> cars;
+
+    public RacingGame(List<Car> cars) {
+        this.cars = new ArrayList<>(cars);
+    }
+
+    public void race(int rounds){
+        for(int round = 0 ; round <rounds ; round++) {
+            raceOneRound();
+        }
+    }
+
+    private void raceOneRound(){
+        for(Car car:cars){
+            car.move(generateRandomNumber());
+        }
+    }
+
+    private int generateRandomNumber() {
+        return (int) (Math.random() * 10);
+    }
+
+    public List<String> getWinners(){
+        int maxPosistion = findMaxPosition();
+        List<String> winners = new ArrayList<>();
+        for(Car car : cars){
+            addWinnersName(car,maxPosistion,winners);
+        }
+        return winners;
+    }
+
+    private void addWinnersName(Car car, int maxPosition, List<String> winners){
+        if(car.getPosition() == maxPosition){
+            winners.add(car.getName());
+        }
+
+    }
+    private int findMaxPosition(){
+        int maxPosition = 0;
+        for(Car car : cars){
+            maxPosition = Math.max(car.getPosition(),maxPosition);
+        }
+        return maxPosition;
+    }
+}
