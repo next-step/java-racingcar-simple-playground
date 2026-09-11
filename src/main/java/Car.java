7a20f9e7d6b951f02d57a3323d327c4a7c3d11bc
yejoo0310 <yejoo0310@naver.com>
2026-09-11 22:05:47 +0900

    feat: Car 클래스 구현

---
+++ src/main/java/Car.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+
+public class Car {
+    private static final int RANDOM_BOUND = 10;
+    private static final int MOVE_CRITERION = 4;
+
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move(int randomValue) {
+        if (randomValue >= MOVE_CRITERION) {
+            position++;
+        }
+    }
+
+    public int getRandomValue() {
+        return new Random().nextInt(RANDOM_BOUND);
+    }
+}
