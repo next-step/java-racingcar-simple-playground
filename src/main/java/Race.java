c1770d006133acddc0fbda67b723092f7b0288bd
ynaeun <yne325@naver.com>
2026-09-12 13:26:21 +0900

    feat:자동차 경주 클래스 수정

--- src/main/java/Race.java
+++ src/main/java/Race.java
@@ -3,22 +3,22 @@ import java.util.List;
 
 public class Race {
 
-    private List<Car> Cars=new ArrayList<>();
+    private List<Car> cars=new ArrayList<>();
 
     void initCars(String name){
-        Cars.add(new Car(name));
+        cars.add(new Car(name));
     }
     //List는 인덱스로 접근 불가 !! 메서드이용할것.
 
     void start(NumberGenerator numberGenerator){
         int n=numberGenerator.generate();
-        for (Car car : Cars) {
+        for (Car car : cars) {
             car.Move(n);
         }
     }
 
     void getResult(){
-        for (Car car : Cars) {
+        for (Car car : cars) {
             System.out.println(car.getName()+": "+car.getDistance());
         }
     }
@@ -29,7 +29,7 @@ public class Race {
 
         List<Car> winners = new ArrayList<>();
         int max = getMax();
-        for (Car car : Cars) {
+        for (Car car : cars) {
             if (car.getDistance() == max)
                 winners.add(car);
         }
@@ -38,7 +38,7 @@ public class Race {
 
     private int getMax() {
         int max= 0;
-        for (Car car : Cars) {
+        for (Car car : cars) {
             if (max <= car.getDistance())
                 max = car.getDistance();
         }
