7d1f425019683ee71454845989d90ea7dc61b1b0
rahwan10 <donggeon10@naver.com>
2026-09-12 18:36:10 +0900

    test: 차하나만 우승, 공동우승 테스트 구현

---
+++ src/test/java/racingcar/CarRaceTest.java
@@ -0,0 +1,43 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import java.util.List;
+
+public class CarRaceTest {
+    @DisplayName("1,2번차 공동우승한다")
+    @Test
+    void findWinnersTest() {
+        Car car1=new Car("동건");
+        Car car2=new Car("규민");
+        Car car3=new Car("재홍");
+        List<Car> cars=List.of(car1,car2,car3);
+        NumberGenerator numberGenerator=new TestNumberGenerator(List.of(5,5,1));
+        CarRace carRace=new CarRace(cars,numberGenerator);
+
+        carRace.race(1);
+        List<Car> winners=carRace.findWinners();
+
+        assertThat(winners).isEqualTo(List.of(car1,car2));
+    }
+    @DisplayName("1번차만 승리한다")
+    @Test
+    void findOnlyOneWinnerTest() {
+        Car car1=new Car("동건");
+        Car car2=new Car("규민");
+        Car car3=new Car("재홍");
+        List<Car> cars=List.of(car1,car2,car3);
+        NumberGenerator numberGenerator=new TestNumberGenerator(List.of(5,2,1));
+        CarRace carRace=new CarRace(cars,numberGenerator);
+
+        carRace.race(1);
+        List<Car> winners=carRace.findWinners();
+
+        assertThat(winners).isEqualTo(List.of(car1));
+    }
+
+
+}
