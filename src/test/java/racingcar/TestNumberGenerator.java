5f03d1211ec2fa3a401210924412db5bf4b006cf
rahwan10 <donggeon10@naver.com>
2026-09-12 18:30:43 +0900

    test: 테스트에서 숫자를 고정할 수 있는 TestNumberGenerator 작성

---
+++ src/test/java/racingcar/TestNumberGenerator.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+import java.util.List;
+
+public class TestNumberGenerator implements NumberGenerator {
+    private final List<Integer> numbers;
+    private int index = 0;
+
+    public TestNumberGenerator(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    @Override
+    public int generate() {
+        int number=this.numbers.get(index);
+        index++;
+        return number;
+    }
+}
