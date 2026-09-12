2c66477ea529c86349b28ac09a06295cc5fd4e67
rahwan10 <donggeon10@naver.com>
2026-09-12 17:09:23 +0900

    feat: car클래스 생성 및 필드 생성

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,14 +3,4 @@ package racingcar;
 public class Car {
     private int position = 0;
     private final String name;
-
-    public Car(String name){
-        this.name=name;
-    }
-    public void move(int number){
-        if(number>=4){
-            position++;
-        }
-    }
-
 }
