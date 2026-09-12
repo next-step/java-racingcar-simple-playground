61943ed3be6953a5c33b2e0e33d77712f07aca6f
rahwan10 <donggeon10@naver.com>
2026-09-12 17:08:55 +0900

    feat: car 생성자 메서드, 4이상일때 움직이는 메서드 생성

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -4,5 +4,13 @@ public class Car {
     private int position = 0;
     private final String name;
 
+    public Car(String name){
+        this.name=name;
+    }
+    public void move(int number){
+        if(number>=4){
+            position++;
+        }
+    }
 
 }
