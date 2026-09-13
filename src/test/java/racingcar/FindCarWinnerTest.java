b4297b3b15ee82db65452fc96359acd696eefb3b
guhyung5645 <leeknivoc@naver.com>
2026-09-13 20:39:01 +0900

    test: 우승자 찾기 테스트(FindCarWinner)를 수정 -난수 기반 carRace 대신 movePoint로 위치를 고정하여 단독/공동 우승 각각 검증

--- src/test/java/racingcar/FindCarWinnerTest.java
+++ src/test/java/racingcar/FindCarWinnerTest.java
@@ -10,34 +10,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class FindCarWinnerTest {
 
     @Test
-    @DisplayName("우승자 찾기 테스트")
-    public void winnerTest() {
+    @DisplayName("가장 멀리 간 자동차가 우승자다")
+    void findWinnerTest() {
         Car a = new Car("A");
         Car b = new Car("B");
-        Car c = new Car("C");
-        Car d = new Car("D");
-        Car e = new Car("E");
+        a.movePoint(4);
+        a.movePoint(5);
+        b.movePoint(4);
 
-        Cars cars = new Cars(List.of(a, b, c, d, e));
+        Cars cars = new Cars(List.of(a, b));
 
-        cars.carRace(new RandomNumber(), 5);
-
-        List<Car> winners = cars.findWinner();
-
-        if (winners.isEmpty()) {
-            System.out.println("우승자가 없습니다.");
-            return;
-        }
-
-        assertThat(winners).isNotEmpty();
-        System.out.print("우승자: ");
-        assertThat(winners).allMatch(car -> car.getCarPosition() == winners.get(0).getCarPosition());
-
-        for (int i = 0; i < winners.size(); i++) {
-            Car winner = winners.get(i);
-            System.out.print(winner.getCarName() + " ");
-        }
-        System.out.println();
+        assertThat(cars.findWinner()).containsExactly(a);
     }
+
+    @Test
+    @DisplayName("공동 우승자가 있을 수 있다")
+    void findWinnerTests() {
+        Car a = new Car("A");
+        Car b = new Car("B");
+        a.movePoint(4);
+        b.movePoint(4);
+
+        Cars cars = new Cars(List.of(a, b));
+
+        assertThat(cars.findWinner()).containsExactlyInAnyOrder(a, b);
+    }
+
 
 }
