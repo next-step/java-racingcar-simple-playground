7881dc5d4c0175d9bdf80cc106c9cd5b7d88b83c
guhyung5645 <leeknivoc@naver.com>
2026-09-13 18:07:56 +0900

    refactor: CarMove, FindCarWinner를 Cars로 통합  - Cars가 상태(자동차 목록)와 행동(이동, 우승자 찾기)을 함께 갖도록 변경  - 요청하신 리뷰대로 행동만 있던 CarMove, FindCarWinner 삭제

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -1,6 +1,6 @@
 package racingcar;
 
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cars {
@@ -15,8 +15,51 @@ public class Cars {
         return cars;
     }
 
-
     public int size() {
         return cars.size();
+    }
+
+    private void move(RandomNumber randomNumber) {
+        for (int i = 0; i < cars.size(); i++) {
+            int number = randomNumber.getRandomNumber();
+            cars.get(i).movePoint(number);
+        }
+    }
+
+    public void carRace(RandomNumber randomNumber, int raceCount) {
+        for (int i = 0; i < raceCount; i++) {
+            move(randomNumber);
+        }
+    }
+
+    public List<Car> findWinner() {
+        //최대위치인 차만 고르면됨
+        int maxPosition = getMaxPosition();
+        List<Car> winners = new ArrayList<>();
+        for (int i = 0; i < cars.size(); i++) {
+            addWinnerIfMatch(cars.get(i).getCarPosition(), maxPosition, winners, i);
+        }
+        return winners;
+    }
+
+    private void addWinnerIfMatch(int CarPosition, int maxPosition, List<Car> winners, int i) {
+        if (CarPosition == maxPosition) {
+            winners.add(cars.get(i));
+        }
+    }
+
+    private int getMaxPosition() {//다른데서 쓸일 없으니까 여기서만 쓰니까 private
+        int maxPosition = 0;
+        for (int i = 0; i < cars.size(); i++) { //이게 최대위치 구한거
+            maxPosition = compareMax(maxPosition, cars.get(i).getCarPosition());
+        }
+        return maxPosition;
+    }
+
+    private int compareMax(int maxPosition, int carPosition) {
+        if (maxPosition < carPosition) {
+            return carPosition;
+        }
+        return maxPosition;
     }
 }
