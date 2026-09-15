e4cc438d7fced1ab73064f283707c6b67cf1dbe1
yejoo0310 <yejoo0310@naver.com>
2026-09-15 10:17:56 +0900

    refactor: 자동차 컬렉션 관리 책임을 Cars로 분리

---
+++ src/main/java/Cars.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    private final List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = new ArrayList<>(cars);
+    }
+
+    public void move(NumberGenerator numberGenerator) {
+        for (Car car : cars) {
+            car.move(numberGenerator.generate());
+        }
+    }
+
+    private int findMaxPosition() {
+        int maxPosition = 0;
+
+        for (Car car : cars) {
+            maxPosition = Math.max(maxPosition, car.getPosition());
+        }
+
+        return maxPosition;
+    }
+
+    public List<Car> findWinners() {
+        List<Car> winners = new ArrayList<>();
+        int maxPosition = findMaxPosition();
+
+        for (Car car : cars) {
+            addWinner(car, maxPosition, winners);
+        }
+
+        return winners;
+    }
+
+    private void addWinner(Car car, int maxPosition, List<Car> winners) {
+        if (car.getPosition() == maxPosition) {
+            winners.add(car);
+        }
+    }
+}
