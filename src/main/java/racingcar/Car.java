c271a58bad40ff57cbc324e81c3a5f269a1f0981
guhyung5645 <leeknivoc@naver.com>
2026-09-15 15:28:34 +0900

    feat: carName 받을때 null or blank일 경우 예외처리

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,7 +3,7 @@ package racingcar;
 public class Car {
     private static final int FORWARD_CONDITION = 4;
     //static은 클랫 전체가 공유하는 변수
-    
+
     private final String carName;
     //final이 자바스크립트의 const와 유사?
     //private는 이제 직접 수정못하게하는? 외부에서 직접 접근 불가하게 하는것?
@@ -11,6 +11,9 @@ public class Car {
     private int carPosition = 0;
 
     public Car(String carName) {
+        if (carName == null || carName.isBlank()) {
+            throw new IllegalArgumentException("자동차 이름은 비어있을 수 없습니다.");
+        }
         this.carName = carName;
     }
 
