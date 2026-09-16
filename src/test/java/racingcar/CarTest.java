6b3d73cc6373764cf83e48f94f2a06050393b904
guhyung5645 <leeknivoc@naver.com>
2026-09-17 02:08:49 +0900

    test: 테스트 클래스/메서드 이름을 대상 클래스와 매칭함(1,2 단계 마지막 리뷰 반영사항) - CarMovingTest -> CarTest, FindCarWinnerTest -> CarsTest 로 파일명을 테스트 대상 클래스와 맞춤

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -6,24 +6,23 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class CarMovingTest {
+public class CarTest {
 
     @DisplayName("4이상이면 전진")
     @ParameterizedTest //얘는 이재 밑에를 반복해서 테스트 실행할꺼다. 라는 것을 선언? 알리는 것
     @ValueSource(ints = {4, 5, 6, 7, 8, 9}) //이 값들을 가지고 반복해서 실행한다.
-    public void moveForwardTest(int number) {
+    public void moveForwardWhenNumberIsFourOrMore(int number) {
         Car car = new Car("Test");
 
         car.moveIfPossible(number);
 
         assertThat(car.getCarPosition()).isEqualTo(1);
     }
-
-    //@Test
+    
     @DisplayName("3이하면 그대로")
     @ParameterizedTest //얘는 이재 밑에를 반복해서 테스트 실행할꺼다. 라는 것을 선언? 알리는 것
     @ValueSource(ints = {0, 1, 2, 3}) //이 값들을 가지고 반복해서 실행한다.
-    public void notMoveForwardTest(int number) {
+    public void stayWhenNumberIsThreeOrLess(int number) {
         Car car = new Car("Test");
 
         car.moveIfPossible(number);
--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -1,17 +1,16 @@
 package racingcar;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-public class FindCarWinnerTest {
+public class CarsTest {
 
     @Test
     @DisplayName("가장 멀리 간 자동차가 우승자다")
-    void findWinnerTest() {
+    void returnsSingleWinnerWhenOneCarLeads() {
         Car a = new Car("A");
         Car b = new Car("B");
         a.moveIfPossible(4);
@@ -25,7 +24,7 @@ public class FindCarWinnerTest {
 
     @Test
     @DisplayName("공동 우승자가 있을 수 있다")
-    void findWinnerTests() {
+    void returnsMultipleWinnersWhenTied() {
         Car a = new Car("A");
         Car b = new Car("B");
         a.moveIfPossible(4);
