7532249cd4ab2d42390a8e77cce78f22b922c14b
손서진 <sonseojin702@gmail.com>
2026-09-18 19:21:46 +0900

    refactor: 게임 실행 힌트에 맞게 InputView 수정

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -16,7 +16,7 @@ public class InputView {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("시도할 회수는 몇회인가요?");
-        String input = scanner.nextLine();
-        return Integer.parseInt(input);
+        int  tryCount = scanner.nextInt();
+        return tryCount;
     }
 }
