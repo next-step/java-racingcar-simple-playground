68b9cf656b26158d2701e81d740f985a3eb40c7d
woosukkk <ekstor8544@naver.com>
2026-09-18 09:58:55 +0900

    feat: 입력 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -6,8 +6,25 @@ public class Application {
 
     public static void main (String[] args){
         Scanner scanner = new Scanner(System.in);
-        List<Car> cars = readCars(Scanner);
-        int rounds = readRounds(Scanner);
+        List<Car> cars = readCars(scanner);
+        int rounds = readRounds(scanner);
 
+    }
+
+
+    private static List<Car> readCars(Scanner scanner){
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String[] names = scanner.nextLine().split(",", -1);
+        List<Car> cars = new ArrayList<>();
+        for (String name : names) {
+            cars.add(new Car(name));
+        }
+        return cars;
+    }
+
+    private static int readRounds(Scanner scanner){
+        System.out.println("시도할 회수는 몇회인가요?");
+        return Integer.parseInt(scanner.nextLine());
+    }
 
 }
