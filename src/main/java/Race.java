a6920b964e553b09d707ecc928fad0375df17b26
ynaeun <yne325@naver.com>
2026-09-18 21:17:34 +0900

    feat: Race 결과 출력 기능 추가

--- src/main/java/Race.java
+++ src/main/java/Race.java
@@ -1,26 +1,44 @@
 import NumberGenerator.NumberGenerator;
-
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Race {
 
     private final Cars cars;
+    private final int COUNT;
 
-    public Race(Cars cars){
-        this.cars=cars;
+    public Race(String carNames, int n) {
+        cars = new Cars(List.of(carNames.split(",")));
+        COUNT = n;
     }
 
-    public void start(NumberGenerator numberGenerator){
-        cars.moveAll(numberGenerator);
+    public void start(NumberGenerator numberGenerator) {
+        System.out.println();
+        System.out.println("실행 결과");
+
+        for (int i = 0; i < COUNT; i++) {
+            cars.moveAll(numberGenerator);
+            getResult();
+        }
+        getWinners();
     }
 
-    public List<Car> getWinners(){
-        return cars.getWinner();
+    public List<Car> getWinners() {
+        List<Car> winners = cars.getWinner();
+
+        String result = winners.stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(", "));
+
+        System.out.println(result + "가 최종 우승했습니다.");
+
+        return winners;
     }
 
-    public void getResult(){
+
+    public void getResult() {
         cars.printAll();
+        System.out.println();
     }
 
 }
