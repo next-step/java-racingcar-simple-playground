0891c27f1bd91070d2cb07313469352036776e2d
woosukkk <ekstor8544@naver.com>
2026-09-18 10:33:55 +0900

    feat: 3단계 게임 실행 힌트 적용

--- src/main/java/RandomNumberGenerator.java
+++ src/main/java/RandomNumberGenerator.java
@@ -1,8 +1,11 @@
+import java.util.Random;
+
 public class RandomNumberGenerator implements NumberGenerator {
     private static final int RANDOM_UPPER_BOUND_EXCLUSIVE = 10;
+    private final Random random = new Random();
 
     @Override
     public int generate() {
-        return (int) (Math.random() * RANDOM_UPPER_BOUND_EXCLUSIVE);
+        return random.nextInt(RANDOM_UPPER_BOUND_EXCLUSIVE);
     }
 }
