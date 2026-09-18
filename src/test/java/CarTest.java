fa82e099ac1b4cf1dee12c8d9beb68a94000f1b9
woosukkk <ekstor8544@naver.com>
2026-09-18 11:04:53 +0900

    refactor: 요구 사항에 맞춰 테스트 변수 변경

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -11,7 +11,7 @@ public class CarTest {
     void movesWhenNumberIsFourOrMore() {
 
         //준비
-        Car car = new Car("Greedy");
+        Car car = new Car("Green");
 
         //실행
         car.move(MOVABLE_MIN);
@@ -23,7 +23,7 @@ public class CarTest {
     @Test
     void nonMovesWhenNuberIsUnderFour() {
         //준비
-        Car car = new Car("Greedy");
+        Car car = new Car("Green");
 
         //실행
         car.move(NON_MOVABLE_UNDER);
