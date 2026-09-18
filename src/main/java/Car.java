1ec9a6098c42414b5c7084e05b6afd1722ba80a0
손서진 <sonseojin702@gmail.com>
2026-09-18 22:04:32 +0900

    refactor: 이름이 5자 이하일 경우 예외처리

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -4,6 +4,14 @@ public class Car {
     private static final int MOVE_THRESHOLD = 4;
 
     public Car(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("이름은 비워둘 수 없습니다.");
+        }
+
+        if (name.length() < 5) {
+            throw new IllegalArgumentException("이름은 5자 이하여야 합니다.");
+        }
+
         this.name = name;
     }
 
