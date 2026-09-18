34b73778fbc4bfe1c5ebf42db610ca082778437d
yejoo0310 <yejoo0310@naver.com>
2026-09-19 03:55:41 +0900

    feat: 자동차 이름은 5자 이하만 허용

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,4 +1,5 @@
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 import org.junit.jupiter.api.Test;
 
@@ -19,5 +20,26 @@ public class CarTest {
         car.move(3);
 
         assertThat(car.getPosition()).isEqualTo(0);
+    }
+
+    @Test
+    public void 자동차_이름은_5자까지_가능하다() {
+        // given
+        String name = "car12";
+
+        // when, then
+        assertThatCode(() -> new Car(name))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    public void 자동차_이름이_5자를_넘으면_예외가_발생한다() {
+        // given
+        String name = "car123";
+
+        // when, then
+        assertThatCode(() -> new Car(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 5자 이하여야 합니다.");
     }
 }
