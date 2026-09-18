0a1529523190e27e4b42e42413faefda04c2032d
손서진 <sonseojin702@gmail.com>
2026-09-18 16:10:31 +0900

    refactor: 입력에 맞게 InputView 수정

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 public class InputView {
     public List<String> getCarName() {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("5자 이하의 이름을 입력하세요. 이름은 쉼표로 구분됩니다.");
         String carName = scanner.nextLine();
         String[] names = carName.split(",");
         return Arrays.asList(names);
@@ -12,6 +14,8 @@ public class InputView {
 
     public int getTryCount() {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("반복할 라운드 횟수를 입력하세요.");
         String input = scanner.nextLine();
         return Integer.parseInt(input);
     }
