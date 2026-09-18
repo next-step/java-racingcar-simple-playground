bd61d5bc849b8855027bb0dab4a140e1dd4e9ecf
woosukkk <ekstor8544@naver.com>
2026-09-18 09:52:52 +0900

    feat : 어플리케이션 스케너 추가

---
+++ src/main/java/Application.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Application {
+
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        List<Car> cars = readCars(Scanner);
+        int rounds = readRounds(Scanner);
+
+
+}
