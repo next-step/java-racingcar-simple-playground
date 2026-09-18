27c2e12793365bf7ede1dc38527018a6b98a3a94
yejoo0310 <yejoo0310@naver.com>
2026-09-19 05:32:00 +0900

    refactor: Application 실행 흐름을 메서드로 분리

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,4 +1,9 @@
-import domain.*;
+import domain.Car;
+import domain.Cars;
+import domain.NumberGenerator;
+import domain.RandomNumberGenerator;
+import domain.RacingGame;
+
 import view.InputView;
 import view.OutputView;
 
@@ -8,42 +13,47 @@ import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
-        // UI 객체들 생성
         Scanner scanner = new Scanner(System.in);
         InputView inputView = new InputView(scanner);
         OutputView outputView = new OutputView();
 
-        // 이름 입력
         String[] names = inputView.readCarNames();
+        Cars cars = createCars(names);
 
-        // domain.Car 객체 생성
+        int raceCount = inputView.readRaceCount();
+
+        NumberGenerator numberGenerator = new RandomNumberGenerator();
+        RacingGame racingGame = new RacingGame(cars, numberGenerator);
+
+        runRaceAndPrintResults(raceCount, racingGame, cars, outputView);
+
+        outputView.printWinners(cars.findWinners());
+    }
+
+    private static Cars createCars(String[] names) {
         List<Car> carList = new ArrayList<>();
         for (String name : names) {
             Car car = new Car(name);
             carList.add(car);
         }
 
-        // domain.Cars 객체 생성
         Cars cars = new Cars(carList);
 
-        // 경주 횟수 입력
-        int raceCount = inputView.readRaceCount();
+        return cars;
+    }
 
-        // domain.RacingGame 생성
-        NumberGenerator numberGenerator = new RandomNumberGenerator();
-        RacingGame racingGame = new RacingGame(cars, numberGenerator);
-
-        // 경주 횟수만큼 라운드 반복
+    private static void runRaceAndPrintResults(
+            int raceCount,
+            RacingGame racingGame,
+            Cars cars,
+            OutputView outputView
+    ) {
         outputView.printResultHeader();
         for (int i = 0; i < raceCount; i++) {
             racingGame.race();
             outputView.printCars(cars);
         }
 
-        // 최종 자동차들 상태 출력
         outputView.printCars(cars);
-
-        // 최종 우승자 출력
-        outputView.printWinners(cars.findWinners());
     }
 }
