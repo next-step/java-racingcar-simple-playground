d2f042acd7b88abd34bfb79ec3f4d795a8d40498
ynaeun <yne325@naver.com>
2026-09-19 01:09:42 +0900

    refactor: main 입력 출력 부분 수정

--- src/main/java/CarRaceMain.java
+++ src/main/java/CarRaceMain.java
@@ -1,18 +1,16 @@
-import NumberGenerator.RandomNumberGenerator;
-import java.util.Scanner;
+import domain.Race;
+import numberGenerator.RandomNumberGenerator;
+import view.InputView;
+import view.ResultView;
 
 public class CarRaceMain {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        final Race race;
+        final String carNames = InputView.getCarNames();
+        final int count = InputView.getTryCount();
 
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        String carNames = sc.nextLine();
-        System.out.println("시도할 회수는 몇회인가요?");
-        int count = sc.nextInt();
-
-        race = new Race(carNames, count);
+        final Race race = new Race(carNames, count);
         race.start(new RandomNumberGenerator());
 
+        ResultView.printWinners(race.getWinners());
     }
 }
