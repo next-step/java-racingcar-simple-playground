7ce36b3a55698aa4bd508d46ca7027839312845e
ynaeun <yne325@naver.com>
2026-09-19 14:47:59 +0900

    feat: 단위 테스트 추가

--- src/test/java/CarRaceTest.java
+++ src/test/java/CarRaceTest.java
@@ -1,29 +1,59 @@
-import NumberGenerator.FixNumberGenerator;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import domain.Cars;
+import domain.Race;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.*;
-
 public class CarRaceTest {
-    Race race;
-
 
     @Test
-    void startRace() {
+    @DisplayName("3 이하면 전진, 4 이상이면 움직인다.")
+    void moveTest() {
 
-        Cars cars= new Cars(List.of("A", "B", "C"));
+        Cars cars = new Cars(List.of("A", "B"));
 
-        race= new Race(cars);
+        cars.moveAll(new FixNumberGenerator(new int[]{3, 4}));
 
-        race.start(new FixNumberGenerator(new int[]{2,3,3}));
-        race.start(new FixNumberGenerator(new int[]{2,3,3}));
-        race.start(new FixNumberGenerator(new int[]{2,4,4}));
-        race.start(new FixNumberGenerator(new int[]{4,4,4}));
+        List<String> winners = cars.getWinner();
 
-        List<Car> winners = race.getWinners();
+        assertThat(winners).hasSize(1);
+        assertThat(winners).isEqualTo(List.of("B"));
+    }
+
+    @Test
+    @DisplayName("단독 우승자 테스트")
+    void getSingleWinnerTest() {
+
+        int tryCount = 3;
+        int[] fixedNumbers = new int[]{1, 2, 3, 2, 3, 4, 3, 4, 5};
+        Race race = new Race("A,B,C", new FixNumberGenerator(fixedNumbers));
+
+        for (int i = 0; i < tryCount; i++) {
+            race.start();
+        }
+
+        List<String> winners = race.getWinners();
+
+        assertThat(winners).containsExactly("C");
+    }
+
+    @Test
+    @DisplayName("공동 우승자 테스트")
+    void getCoWinnerTest() {
+
+        int COUNT = 3;
+        int[] fixedNumbers = new int[]{2, 4, 5, 3, 5, 9, 1, 4, 4};
+        Race race = new Race("A,B,C", new FixNumberGenerator(fixedNumbers));
+
+        for (int i = 0; i < COUNT; i++) {
+            race.start();
+        }
+
+        List<String> winners = race.getWinners();
 
         assertThat(winners).hasSize(2);
-        assertThat(winners.get(0).getDistance()).isEqualTo(2);
+        assertThat(winners).contains("B", "C");
     }
 }
