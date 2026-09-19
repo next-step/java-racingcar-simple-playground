599d968b5cd68745102c307e12f6bbf086159151
손서진 <sonseojin702@gmail.com>
2026-09-19 14:46:27 +0900

    fix: 예외처리 반복문 밖으로 뻄.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -18,10 +18,11 @@ public class Application {
         Cars cars = new Cars(names, carMovement);
         Race race = new Race(cars);
 
+        if (tryCount <= 0) {
+            throw new IllegalArgumentException("시도 횟수는 0보다 커야 합니다.");
+        }
+
         for (int i = 0; i < tryCount; i++) {
-            if (tryCount <= 0) {
-                throw new IllegalArgumentException("시도 횟수는 0보다 커야 합니다.");
-            }
             race.oneRound();
             outputView.printRound(cars.getCars());
         }
