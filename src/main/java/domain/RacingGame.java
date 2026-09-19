3f0bf5414abaf170acdd679f47ea53734c7e1afd
woosukkk <ekstor8544@naver.com>
2026-09-19 21:00:27 +0900

    feat: Cars 객체 추가해 역할 분리

--- src/main/java/domain/RacingGame.java
+++ src/main/java/domain/RacingGame.java
@@ -1,62 +1,26 @@
 package domain;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class RacingGame {
 
-    private static final int START_POSITION = 0;
-    private final List<Car> cars;
+    private final Cars cars;
     private final NumberGenerator numberGenerator;
 
-    public RacingGame(List<Car> cars, NumberGenerator numberGenerator) {
+    public RacingGame(
+            Cars cars,
+            NumberGenerator numberGenerator
+    ) {
+        this.cars = cars;
         this.numberGenerator = numberGenerator;
-        if (cars.isEmpty()) {
-            throw new IllegalArgumentException("자동차가 필요합니다.");
-        }
-
-        this.cars = new ArrayList<>(cars);
     }
 
     public void race(int rounds) {
-
-        if (rounds < 0) {
-            throw new IllegalArgumentException("횟수는 음수일 수 없습니다.");
-        }
+        validateRounds(rounds);
 
         for (int round = 0; round < rounds; round++) {
             raceOneRound();
         }
-    }
-
-    private void raceOneRound() {
-        for (Car car : cars) {
-            car.move(numberGenerator.generate());
-        }
-    }
-
-    public List<String> getWinners() {
-        int maxPosition = findMaxPosition();
-        List<String> winners = new ArrayList<>();
-        for (Car car : cars) {
-            addWinnersName(car, maxPosition, winners);
-        }
-        return winners;
-    }
-
-    private void addWinnersName(Car car, int maxPosition, List<String> winners) {
-        if (car.getPosition() == maxPosition) {
-            winners.add(car.getName());
-        }
-
-    }
-
-    private int findMaxPosition() {
-        int maxPosition = START_POSITION;
-        for (Car car : cars) {
-            maxPosition = Math.max(car.getPosition(), maxPosition);
-        }
-        return maxPosition;
     }
 
     public void race(int rounds, Runnable afterRound) {
@@ -68,11 +32,24 @@ public class RacingGame {
         }
     }
 
+
     private void validateRounds(int rounds) {
         if (rounds < 0) {
             throw new IllegalArgumentException(
                     "횟수는 음수일 수 없습니다."
             );
         }
+    }
+
+    private void raceOneRound() {
+        cars.move(numberGenerator);
+    }
+
+    public List<String> getWinners() {
+        return cars.getWinners();
+    }
+
+    public List<Car> getCars() {
+        return cars.getCars();
     }
 }
