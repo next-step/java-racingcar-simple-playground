3acb21a7b6b6d7710b9cda513a3c8511aa826547
guhyung5645 <leeknivoc@naver.com>
2026-09-19 20:55:47 +0900

    test: 우승자 판별 테스트에서 위치를 직접 지정하도록 수정

--- src/test/java/domain/CarsTest.java
+++ src/test/java/domain/CarsTest.java
@@ -13,11 +13,8 @@ public class CarsTest {
     @Test
     @DisplayName("가장 멀리 간 자동차가 우승자다")
     void returnsSingleWinnerWhenOneCarLeads() {
-        Car a = new Car("A");
-        Car b = new Car("B");
-        a.moveIfPossible(4);
-        a.moveIfPossible(5);
-        b.moveIfPossible(4);
+        Car a = new Car("A", 7);
+        Car b = new Car("B", 2);
 
         Cars cars = new Cars(List.of(a, b));
 
@@ -27,10 +24,8 @@ public class CarsTest {
     @Test
     @DisplayName("공동 우승자가 있을 수 있다")
     void returnsMultipleWinnersWhenTied() {
-        Car a = new Car("A");
-        Car b = new Car("B");
-        a.moveIfPossible(4);
-        b.moveIfPossible(4);
+        Car a = new Car("A", 3);
+        Car b = new Car("B", 3);
 
         Cars cars = new Cars(List.of(a, b));
 
@@ -42,8 +37,8 @@ public class CarsTest {
     @ValueSource(ints = {4, 5, 6, 7, 8, 9})
     @DisplayName("랜덤값이 4이상이면 모든 자동차가 전진한다")
     void allCarsMoveForward_whenRandomNumberIsFourOrMore(int number) {
-        Car a = new Car("A");
-        Car b = new Car("B");
+        Car a = new Car("A", 0);
+        Car b = new Car("B", 0);
         Cars cars = new Cars(List.of(a, b));
 
         cars.move(new FixedRandomNumber(number));
@@ -57,8 +52,8 @@ public class CarsTest {
     @ValueSource(ints = {0, 1, 2, 3})
     @DisplayName("랜덤값이 3이하면 모든 자동차가 정지한다")
     void allCarsStay_whenRandomNumberIsThreeOrLess(int number) {
-        Car a = new Car("A");
-        Car b = new Car("B");
+        Car a = new Car("A", 0);
+        Car b = new Car("B", 0);
         Cars cars = new Cars(List.of(a, b));
 
         cars.move(new FixedRandomNumber(number));
