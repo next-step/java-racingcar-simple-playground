8e272c5bb6eebd540a0dff9e2b8c6ca88cdae5ec
ynaeun <yne325@naver.com>
2026-09-19 14:11:22 +0900

    chore: view 이름 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -2,10 +2,18 @@ package view;
 
 import java.util.List;
 
-public class ResultView {
+public class OutputView {
+    public static void printResultMessage() {
+        printNewLine();
+        System.out.println("실행 결과");
+    }
+
+    public static void printNewLine() {
+        System.out.println();
+    }
+
     public static void printWinners(List<String> winners) {
         String result = String.join(",", winners);
         System.out.println(result + "가 최종 우승했습니다.");
-
     }
 }
