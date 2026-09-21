5d6f12e0981976124c3001904eaa072dd49653dc
손서진 <sonseojin702@gmail.com>
2026-09-22 00:48:43 +0900

    refactor: TryCount 객체에서 시도 횟수가 정수인지, 0보다 큰지 검증

--- src/main/java/domain/TryCount.java
+++ src/main/java/domain/TryCount.java
@@ -3,11 +3,24 @@ package domain;
 public class TryCount {
     private final int value;
 
-    public TryCount(int value) {
+    public TryCount(String input) {
+        int value = parseToInt(input);
+        validate(value);
+        this.value = value;
+    }
+
+    private int parseToInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도 횟수는 숫자여야 합니다.");
+        }
+    }
+
+    private void validate(int value) {
         if (value <= 0) {
             throw new IllegalArgumentException("시도 횟수는 0보다 커야 합니다.");
         }
-        this.value = value;
     }
 
     public int getValue() {
