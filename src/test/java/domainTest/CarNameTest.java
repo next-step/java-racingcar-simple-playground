9aa6824aee533cc2782e88a8d908e088ff8180a1
손서진 <sonseojin702@gmail.com>
2026-09-22 01:02:49 +0900

    refactor: Car 객체 사용 제거 및 경계값 테스트 추가

--- src/test/java/domainTest/CarNameTest.java
+++ src/test/java/domainTest/CarNameTest.java
@@ -7,14 +7,20 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
 public class CarNameTest {
+    @Test
+    @DisplayName("이름이 5자일 때 오류가 발생하지 않는다.")
+    void SafeWhenNameIsFiveWords() {
+        CarName carName = new CarName("ABCDE");
+        assertEquals("ABCDE", carName.getValue());
+    }
+
     @Test
     @DisplayName("이름이 5자 초과일 때 오류가 발생한다.")
     void errorWhenNameMoreFiveWords() {
         IllegalArgumentException exception = assertThrows(
                 IllegalArgumentException.class,
-                () -> new Car("ABCDEF")
+                () -> new CarName("ABCDEF")
         );
         assertEquals("이름은 5자 이하여야 합니다.", exception.getMessage());
     }
@@ -25,6 +31,15 @@ public class CarNameTest {
         IllegalArgumentException exception = assertThrows(
                 IllegalArgumentException.class,
                 () -> new CarName(""));
+        assertEquals("이름은 비어있을 수 없습니다.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("이름이 공백일 때 오류가 발생한다.")
+    void errorWhenNameBlank() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> new CarName(" "));
         assertEquals("이름은 비어있을 수 없습니다.", exception.getMessage());
     }
 }
