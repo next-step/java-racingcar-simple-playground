e0ef7047b67e500868e9af4b75720a434f34dca6
ynaeun <yne325@naver.com>
2026-09-21 15:16:55 +0900

    refactor: InputView 예외 처리 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,9 +1,11 @@
 package view;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputView {
     static Scanner sc = new Scanner(System.in);
+    private static final int MIN_TRY_COUNT = 1;
 
     public static String getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
@@ -13,7 +15,19 @@ public class InputView {
 
     public static int getTryCount() {
         System.out.println("시도할 회수는 몇회인가요?");
-        final int tryCount = sc.nextInt();
-        return tryCount;
+        try {
+            int tryCount = sc.nextInt();
+            validatePositive(tryCount); // 0이나 음수 검증
+            return tryCount;
+        } catch (InputMismatchException e) {
+            throw new IllegalArgumentException("시도 횟수는 숫자여야 합니다.");
+        }
+    }
+
+    private static void validatePositive(int count) {
+        if (count < MIN_TRY_COUNT) {
+            throw new IllegalArgumentException("시도 횟수는 1 이상이어야 합니다.");
+        }
+
     }
 }
