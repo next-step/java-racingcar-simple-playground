ada26bf245c2acc82ddd0b3abbc45bbacb4c6af0
손서진 <sonseojin702@gmail.com>
2026-09-22 00:47:53 +0900

    refactor: 입력을 nextLine()으로 통일하고, Scanner 하나만 생성.

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,20 +5,18 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class InputView {
+    Scanner scanner = new Scanner(System.in);
+    
     public List<String> getCarName() {
-        Scanner scanner = new Scanner(System.in);
-
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String carName = scanner.nextLine();
         String[] names = carName.split(",");
         return Arrays.asList(names);
     }
 
-    public int getTryCount() {
-        Scanner scanner = new Scanner(System.in);
+    public String getTryCount() {
         System.out.println("시도할 회수는 몇회인가요?");
-        int  tryCount = scanner.nextInt();
-
+        String tryCount = scanner.nextLine();
         return tryCount;
     }
 }
