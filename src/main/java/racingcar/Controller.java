445827983d066239db01edd4a6c8f78e003f4d71
rahwan10 <donggeon10@naver.com>
2026-09-22 14:32:58 +0900

    feat: public으로 바꾼 playRound로 race구현하고 라운드마다 자동차 위치 출력, 우승자 출력구현

--- src/main/java/racingcar/Controller.java
+++ src/main/java/racingcar/Controller.java
@@ -15,7 +15,14 @@ public class Controller {
         String namesInput=inputView.readCarNames();
         int countInput=inputView.readTryCount();
         List<Car> cars = createCars(namesInput);
+        CarRace carRace= new CarRace(cars, new RandomNumberGenerator());
+        for (int i=0;i<countInput;i++){
+            carRace.playRound();
+            outputView.printCarsPosition(cars);
+        }
 
+        List<Car> winners=carRace.findWinners();
+        outputView.printWinner(winners);
 
 
     }
