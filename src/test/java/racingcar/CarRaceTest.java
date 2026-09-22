cb4c3294eddda9b4d8dc63fd79ab6a96a6dfb27c
rahwan10 <donggeon10@naver.com>
2026-09-22 14:48:02 +0900

    refactor: 스택틱한 메서드를 CarsFactory 클래스로 리팩토링

--- src/test/java/racingcar/CarRaceTest.java
+++ src/test/java/racingcar/CarRaceTest.java
@@ -40,5 +40,21 @@ public class CarRaceTest {
         assertThat(winners).isEqualTo(List.of(car1));
     }
 
+    @DisplayName("모든차가 승리한다")
+    @Test
+    void findAllWinnerTest() {
+        Car car1 = new Car("동건");
+        Car car2 = new Car("규민");
+        Car car3 = new Car("재홍");
+        List<Car> cars = List.of(car1, car2, car3);
+        NumberGenerator numberGenerator = new TestNumberGenerator(List.of(5, 2, 1));
+        CarRace carRace = new CarRace(cars, ()->4);
+
+        carRace.race(1);
+        List<Car> winners = carRace.findWinners();
+
+        assertThat(winners).isEqualTo(List.of(car1,car2,car3));
+    }
+
 
 }
