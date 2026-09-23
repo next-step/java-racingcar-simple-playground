193209a9d4cef0131115e106410598b57544cd06
guhyung5645 <leeknivoc@naver.com>
2026-09-23 09:53:03 +0900

    refactor: Application의 흐름을 별도 컨트롤러로 분리

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,50 +1,9 @@
-import static domain.Cars.createCars;
-import static view.InputView.readCarName;
-import static view.InputView.readTryCount;
-import static view.ResultView.printFinalWinners;
-import static view.ResultView.printRound;
-
-import domain.Cars;
-import domain.DefaultRandomNumber;
-import domain.RacingGame;
+import controller.RacingController;
 
 public class Application {
 
     public static void main(String[] args) {
-        //자동차 이름 입력하여 생성시키기 --> 둘이 쪼개야할거같음
-
-        //자동차 이름 입력받기
-        String[] carNameArray = readCarName();
-        //자동차 생성시키기
-        Cars carsList = createCars(carNameArray);
-        //시도할 횟수 받기
-        int tryCount = readTryCount();
-        System.out.println();
-        //시도 횟수만큼 경주시키기 (단, 경주할떄마다 값 출력하기)
-        RacingGame racingGame = new RacingGame(carsList, new DefaultRandomNumber());
-        raceCars(racingGame, carsList, tryCount);
-        //최종 우승자 출력하기
-        printFinalWinners(racingGame.findWinners());
+        new RacingController().run();
     }
-
-    //자동차 이름 입력받기
-    //view.Input로 옮김
-
-    //시도 횟수만큼 경주시키기 (단, 경주할떄마다 값 출력하기)
-    private static void raceCars(RacingGame racingGame, Cars carsList, int tryCount) {
-        for (int i = 0; i < tryCount; i++) {
-            racingGame.carRaceOnce();
-            //경주할때마다 값 출력하기
-            printRound(carsList);
-        }
-    }
-
-    //라운드간 우승자 출력하기
-    //최종 우승자 출력하기
-    //view.Result로 옮김
-
 
 }
-
-
-
