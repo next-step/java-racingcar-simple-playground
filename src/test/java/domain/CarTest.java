6eddce0ac2386b1c9725d7f6ddecc78597abc8ff
calamellucy <sua050526@naver.com>
2026-09-24 16:46:45 +0900

    test: Car 테스트 메서드 이름 한글로 수정 및 테스트 추가

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -10,26 +10,47 @@ import org.junit.jupiter.api.Test;
 class CarTest {
 
     @Test
-    void moveCar() {
+    void 랜덤값이_3이면_멈춤() {
         Car car = new Car("Car1", 0);
-        FixedNumGenerator fixedNumber = new FixedNumGenerator(List.of(3, 4));
-        car.moveCar(fixedNumber);
-        assertTrue(car.isSamePosition(0));
+        FixedNumGenerator fixedNumber = new FixedNumGenerator(List.of(3));
 
-        car = new Car("Car2", 0);
         car.moveCar(fixedNumber);
+
+        assertTrue(car.isSamePosition(0));
+    }
+
+    @Test
+    void 랜덤값이_4이면_한칸_이동() {
+        Car car = new Car("Car1", 0);
+        FixedNumGenerator fixedNumber = new FixedNumGenerator(List.of(4));
+
+        car.moveCar(fixedNumber);
+
         assertTrue(car.isSamePosition(1));
     }
 
     @Test
-    void getLargerPosition() {
+    void 현재_위치보다_큰_위치가_오면_큰_위치_반환() {
         Car car = new Car("Car1", 0);
         assertEquals(1, car.getLargerPosition(1));
     }
 
     @Test
-    void isSamePosition() {
+    void 자동차의_위치들이_같은지_확인() {
+        Car car = new Car("Car1", 1);
+
+        assertTrue(car.isSamePosition(1));
+    }
+
+    @Test
+    void 자동차_이름_조회() {
         Car car = new Car("Car1", 0);
-        assertTrue(car.isSamePosition(0));
+        assertEquals("Car1", car.getName());
+    }
+
+    @Test
+    void 자동차_위치_조회() {
+        Car car = new Car("Car1", 2);
+        assertEquals(2, car.getPosition());
     }
 }
