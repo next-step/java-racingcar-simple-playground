a3eb9e5bd7445531b4948fdfebdc03b0b2cc1aa5
calamellucy <sua050526@naver.com>
2026-09-24 16:12:50 +0900

    fix: CarName 입력 검증 부분 수정

--- src/main/java/domain/CarName.java
+++ src/main/java/domain/CarName.java
@@ -4,8 +4,11 @@ public class CarName {
     private String name;
 
     public CarName(String value) {
-        if (value.length() > 5 | value.isEmpty()) {
+        if (value.length() > 5 || value.isEmpty()) {
             throw new IllegalArgumentException("5글자 이하의 이름을 입력하세요");
+        }
+        if (value.isBlank()) {
+            throw new IllegalArgumentException("자동차 이름으로 공백 불가능");
         }
         name = value;
     }
